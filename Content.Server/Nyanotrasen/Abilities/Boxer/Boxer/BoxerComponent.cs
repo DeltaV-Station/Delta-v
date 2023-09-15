8502f78bcc532cfe47c3b217ef377d8ba2d867e7
Fluffiest Floofers <thebluewulf@gmail.com>
2023-09-15 23:22:52 +0200

    DataDefinition partial

--- Content.Server/Nyanotrasen/Abilities/Boxer/Boxer/BoxerComponent.cs
+++ Content.Server/Nyanotrasen/Abilities/Boxer/Boxer/BoxerComponent.cs
@@ -1,23 +1,22 @@
 using Content.Shared.Damage;
 
-namespace Content.Server.Abilities.Boxer
+namespace Content.Server.Abilities.Boxer;
+
+/// <summary>
+/// Added to the boxer on spawn.
+/// </summary>
+[RegisterComponent]
+public sealed partial class BoxerComponent : Component
 {
+    [DataField("modifiers", required: true)]
+    public DamageModifierSet UnarmedModifiers = default!;
+
+    [DataField("rangeBonus")]
+    public float RangeBonus = 1.5f;
+
     /// <summary>
-    /// Added to the boxer on spawn.
+    /// Damage modifier with boxing glove stam damage.
     /// </summary>
-    [RegisterComponent]
-    public sealed class BoxerComponent : Component
-    {
-        [DataField("modifiers", required: true)]
-        public DamageModifierSet UnarmedModifiers = default!;
-
-        [DataField("rangeBonus")]
-        public float RangeBonus = 1.5f;
-
-        /// <summary>
-        /// Damage modifier with boxing glove stam damage.
-        /// </summary>
-        [DataField("boxingGlovesModifier")]
-        public float BoxingGlovesModifier = 1.75f;
-    }
+    [DataField("boxingGlovesModifier")]
+    public float BoxingGlovesModifier = 1.75f;
 }
--- Content.Server/Nyanotrasen/Abilities/Boxer/BoxingSystem.cs
+++ Content.Server/Nyanotrasen/Abilities/Boxer/BoxingSystem.cs
@@ -3,37 +3,36 @@ using Content.Shared.Weapons.Melee;
 using Content.Shared.Weapons.Melee.Events;
 using Robust.Shared.Containers;
 
-namespace Content.Server.Abilities.Boxer
+namespace Content.Server.Abilities.Boxer;
+
+public sealed partial class BoxingSystem : EntitySystem
 {
-    public sealed class BoxingSystem : EntitySystem
+    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+        base.Initialize();
+        SubscribeLocalEvent<BoxerComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<BoxerComponent, MeleeHitEvent>(OnMeleeHit);
+        SubscribeLocalEvent<BoxingGlovesComponent, StaminaMeleeHitEvent>(OnStamHit);
+    }
 
-        public override void Initialize()
-        {
-            base.Initialize();
-            SubscribeLocalEvent<BoxerComponent, ComponentInit>(OnInit);
-            SubscribeLocalEvent<BoxerComponent, MeleeHitEvent>(OnMeleeHit);
-            SubscribeLocalEvent<BoxingGlovesComponent, StaminaMeleeHitEvent>(OnStamHit);
-        }
+    private void OnInit(EntityUid uid, BoxerComponent component, ComponentInit args)
+    {
+        if (TryComp<MeleeWeaponComponent>(uid, out var meleeComp))
+            meleeComp.Range *= component.RangeBonus;
+    }
+    private void OnMeleeHit(EntityUid uid, BoxerComponent component, MeleeHitEvent args)
+    {
+        args.ModifiersList.Add(component.UnarmedModifiers);
+    }
 
-        private void OnInit(EntityUid uid, BoxerComponent component, ComponentInit args)
-        {
-            if (TryComp<MeleeWeaponComponent>(uid, out var meleeComp))
-                meleeComp.Range *= component.RangeBonus;
-        }
-        private void OnMeleeHit(EntityUid uid, BoxerComponent component, MeleeHitEvent args)
-        {
-            args.ModifiersList.Add(component.UnarmedModifiers);
-        }
+    private void OnStamHit(EntityUid uid, BoxingGlovesComponent component, StaminaMeleeHitEvent args)
+    {
+        if (!_containerSystem.TryGetContainingContainer(uid, out var equipee))
+            return;
 
-        private void OnStamHit(EntityUid uid, BoxingGlovesComponent component, StaminaMeleeHitEvent args)
-        {
-            if (!_containerSystem.TryGetContainingContainer(uid, out var equipee))
-                return;
-
-            if (TryComp<BoxerComponent>(equipee.Owner, out var boxer))
-                args.Multiplier *= boxer.BoxingGlovesModifier;
-        }
+        if (TryComp<BoxerComponent>(equipee.Owner, out var boxer))
+            args.Multiplier *= boxer.BoxingGlovesModifier;
     }
 }
