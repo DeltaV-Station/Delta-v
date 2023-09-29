43aca298f0362323fcf12405ab5eb20dcff1d76f
Adrian16199 <piaskowskiadrian0@gmail.com>
2023-09-29 15:59:41 +0200

    IT LIVEEEEEEEEEEEEEEEEEEES (Server side.)

--- Content.Client/Nyanotrasen/Kitchen/Components/DeepFriedComponent.cs
+++ Content.Client/Nyanotrasen/Kitchen/Components/DeepFriedComponent.cs
@@ -3,8 +3,8 @@ using Content.Shared.Kitchen.Components;
 namespace Content.Client.Kitchen.Components
 {
     [RegisterComponent]
-    [ComponentReference(typeof(SharedDeepFriedComponent))]
-    public sealed class DeepFriedComponent : SharedDeepFriedComponent
+ // [ComponentReference(typeof(SharedDeepFriedComponent))]
+    public sealed partial class DeepFriedComponent : SharedDeepFriedComponent
     {
     }
 }
--- Content.Client/Nyanotrasen/Kitchen/Components/DeepFryerComponent.cs
+++ Content.Client/Nyanotrasen/Kitchen/Components/DeepFryerComponent.cs
@@ -3,8 +3,8 @@ using Content.Shared.Kitchen.Components;
 namespace Content.Client.Kitchen.Components
 {
     [RegisterComponent]
-    [ComponentReference(typeof(SharedDeepFryerComponent))]
-    public sealed class DeepFryerComponent : SharedDeepFryerComponent
+//  [ComponentReference(typeof(SharedDeepFryerComponent))]
+    public sealed partial class DeepFryerComponent : SharedDeepFryerComponent
     {
     }
 }
