9e0f6c2945120fbab4dc4df1fe0f7425034a989f
Fluffiest Floofers <thebluewulf@gmail.com>
2023-09-29 20:38:11 +0200

    init I need to grab food

---
+++ Content.Client/Nyanotrasen/Books/BooksSystem.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Books;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.Books;
+
+public sealed partial class BooksSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<OpenURLEvent>(OnOpenURL);
+    }
+
+    private void OnOpenURL(OpenURLEvent args)
+    {
+        var uriOpener = IoCManager.Resolve<IUriOpener>();
+        uriOpener.OpenUri(args.URL);
+    }
+}
---
+++ Content.Server/Nyanotrasen/Books/HyperlinkBookComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.Books;
+
+[RegisterComponent]
+public sealed partial class HyperlinkBookComponent : Component
+{
+    [DataField("url")]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public string URL = string.Empty;
+}
