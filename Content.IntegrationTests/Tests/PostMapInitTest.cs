b878f8815745e07a2ba4560283c55efd69f41ec1
Colin-Tel <113523727+Colin-Tel@users.noreply.github.com>
2023-10-06 19:30:14 -0500

    adjusts test
    also obligatory post-"I'm done mapping" map edits

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -63,6 +63,7 @@ namespace Content.IntegrationTests.Tests
             "Marathon",
             "Kettle",
             "MeteorArena"
+            "Edge" //DeltaV
         };
 
         /// <summary>
