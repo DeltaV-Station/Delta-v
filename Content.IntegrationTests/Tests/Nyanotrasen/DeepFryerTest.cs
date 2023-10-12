692ca8189e9b266893922b7abdd2fcd089c4f83e
PHCodes <47927305+PHCodes@users.noreply.github.com>
2023-10-12 16:04:46 -0400

    Create DeepFryerTest.cs

---
+++ Content.IntegrationTests/Tests/Nyanotrasen/DeepFryerTest.cs
@@ -0,0 +1,60 @@
+#nullable enable annotations
+using Content.Server.Kitchen.Components;
+using Content.Server.Kitchen.EntitySystems;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Reflection;
+
+namespace Content.IntegrationTests.Tests.DeepFryer
+{
+    [TestFixture]
+    [TestOf(typeof(DeepFryerComponent))]
+    public sealed class DeepFryerTest
+    {
+        [Reflect(false)]
+        private sealed class DeepFryerTestSystem : EntitySystem
+        {
+            public override void Initialize()
+            {
+                base.Initialize();
+            }
+        }
+
+        [TestPrototypes]
+        private const string Prototypes = @"
+- type: entity
+  name: DeepFryerDummy
+  id: DeepFryerDummy
+  components:
+  - type: DeepFryer
+    entryDelay: 0
+    draggedEntryDelay: 0
+    flushTime: 0
+  - type: Anchorable
+  - type: ApcPowerReceiver
+  - type: Physics
+    bodyType: Static
+  - type: Fixtures
+    fixtures:
+      fix1:
+        shape:
+          !type:PhysShapeCircle
+          radius: 0.35
+";
+
+        [Test]
+        public async Task Test()
+        {
+            await using var pair = await PoolManager.GetServerClient();
+            var server = pair.Server;
+
+            var testMap = await pair.CreateTestMap();
+
+            EntityUid unitUid = default;
+
+            var entityManager = server.ResolveDependency<IEntityManager>();
+            var xformSystem = entityManager.System<SharedTransformSystem>();
+            var deepFryerSystem = entityManager.System<DeepFryerSystem>();
+            await pair.CleanReturnAsync();
+        }
+    }
+}
