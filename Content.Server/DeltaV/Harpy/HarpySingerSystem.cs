f39e35e69cfced50de2f1976b1837ba8020553ab
evilexecutive <evilexecutive@gmail.com>
2023-10-16 19:00:39 -0400

    Simpler fix

--- Content.Server/DeltaV/Harpy/HarpySingerSystem.cs
+++ Content.Server/DeltaV/Harpy/HarpySingerSystem.cs
@@ -1,41 +1,28 @@
-using Content.Server.Mobs;
 using Content.Server.Instruments;
-using Content.Shared.Instruments;
-using Content.Shared.Actions;
 using Content.Shared.Mobs;
-using Content.Shared.DeltaV.Harpy;
+using Content.Shared.Mobs.Systems;
+using Robust.Server.GameObjects;
 
 namespace Content.Server.DeltaV.Harpy
 {
-    public class HarpySingerSystem : EntitySystem
+    public sealed class HarpySingerSystem : EntitySystem
     {
         [Dependency] private readonly InstrumentSystem _instrument = default!;
+        [Dependency] private readonly MobStateSystem _mobState = default!;
         public override void Initialize()
         {
             base.Initialize();
 
-            SubscribeLocalEvent<InstrumentComponent, MobStateChangedEvent>(OnRemoveInstrumentAbility);
-            SubscribeLocalEvent<HarpySingerComponent, MobStateChangedEvent>(OnAddInstrumentAbility);
+            SubscribeLocalEvent<InstrumentComponent, MobStateChangedEvent>(HarpyStopSinging);
         }
-        //Immediately stops a harpy from singing if they're critted or killed, and prevents them from starting a new song
-        private void OnRemoveInstrumentAbility(EntityUid uid, InstrumentComponent component, MobStateChangedEvent args)
-        {
-            if (args.NewMobState != MobState.Dead || args.NewMobState != MobState.Critical)
-            {
-                RemComp<InstrumentComponent>(uid);
-                return;
-            }
-        }
-        //Allows a harpy to sing again after being revived.
-        private void OnAddInstrumentAbility(EntityUid uid, HarpySingerComponent component, MobStateChangedEvent args)
-        {
-            if (args.NewMobState != MobState.Alive)
-                return;
+        //Immediately closes the Midi UI window if a Singer is incapacitated
+        private void HarpyStopSinging(EntityUid uid, InstrumentComponent component, MobStateChangedEvent args)
 
-            var instrument = EnsureComp<InstrumentComponent>(uid);
-            EnsureComp<InstrumentComponent>(uid);
-           _instrument.SetInstrumentProgram(instrument, 52, 0);
+        {
+            if (HasComp<ActiveInstrumentComponent>(uid) && TryComp<ActorComponent>(uid, out var actor) && _mobState.IsIncapacitated(uid))
+            {
+                _instrument.ToggleInstrumentUi(uid, actor.PlayerSession);
+            }
         }
     }
 }
-
