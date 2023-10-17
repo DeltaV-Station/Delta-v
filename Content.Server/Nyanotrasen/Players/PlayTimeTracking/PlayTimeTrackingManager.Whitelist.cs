34b14cac42b98b7c6b31ed45c9d224b82e5c740e
Debug <sidneymaatman@gmail.com>
2023-10-17 16:45:27 +0200

    Remove unused function

--- Content.Server/Nyanotrasen/Players/PlayTimeTracking/PlayTimeTrackingManager.Whitelist.cs
+++ Content.Server/Nyanotrasen/Players/PlayTimeTracking/PlayTimeTrackingManager.Whitelist.cs
@@ -6,18 +6,6 @@ namespace Content.Server.Players.PlayTimeTracking;
 
 public sealed partial class PlayTimeTrackingManager
 {
-    public async void SendWhitelist(IPlayerSession playerSession)
-    {
-        var whitelist = await _db.GetWhitelistStatusAsync(playerSession.UserId);
-
-        var msg = new MsgWhitelist
-        {
-            Whitelisted = whitelist
-        };
-
-        _net.ServerSendMessage(msg, playerSession.ConnectedClient);
-    }
-
     public void SendWhitelistCached(IPlayerSession playerSession)
     {
         var whitelist = playerSession.ContentData()?.Whitelisted ?? false;
