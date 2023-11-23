dd116f2b04f48d3dad97dc4e57a56a3ed1234e7d
Jeff <>
2023-11-23 15:14:07 -0600

    Revert "Typo/file not saved"
    This reverts commit 190363ead8db415e1d0e06e27b884cbcf835a398.

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -74,7 +74,7 @@ public sealed partial class IdCardConsoleComponent : Component
         "Paramedic", // DeltaV - Add Paramedic access
         "Psychologist", // DeltaV - Add Psychologist access
         "Quartermaster",
-        "Reporter", // DeltaV - Add Reporter access
+        "Reporter", //  DeltaV - Add Reporter access
         "Research",
         "ResearchDirector",
         "Salvage",
