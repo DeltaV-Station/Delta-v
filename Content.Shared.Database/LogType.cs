580996c1724c0af3e44bdbbb79d471a8f27ad876
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2023-11-27 04:08:30 +0100

    Basic rate limiting for chat messages (#21907)

--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -89,6 +89,11 @@ public enum LogType
     ItemConfigure = 84,
     DeviceLinking = 85,
     Tile = 86,
+    /// <summary>
+    /// A client has sent too many chat messages recently and is temporarily blocked from sending more.
+    /// </summary>
+    ChatRateLimited = 87,
+
     BagOfHolding = 420, //Nyano - Summary: adds bag of holding. 
     Psionics = 421, //Nyano - Summary: ads psionic as a log type.
 }
