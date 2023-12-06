271af1730d6b1cae42a7ebdb6dfa880c2428ea65
Bakke <luringens@protonmail.com>
2023-12-06 16:54:05 +0100

    Prevent shooting while inside a duffelbag (#510)
    Anyone shoved into a duffelbag gets an ItemComponent. This denies anyone
    with an ItemComponent from shooting a gun.
    
    (cherry picked from commit a29c36d9bcbc5d004dfc446a7cc17970fce97681)

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -10,6 +10,7 @@ using Content.Shared.Examine;
 using Content.Shared.Gravity;
 using Content.Shared.Hands;
 using Content.Shared.Hands.Components;
+using Content.Shared.Item; // Delta-V: Felinids in duffelbags can't shoot.
 using Content.Shared.Popups;
 using Content.Shared.Projectiles;
 using Content.Shared.Tag;
@@ -129,7 +130,8 @@ public abstract partial class SharedGunSystem : EntitySystem
 
         if (user == null ||
             !_combatMode.IsInCombatMode(user) ||
-            !TryGetGun(user.Value, out var ent, out var gun))
+            !TryGetGun(user.Value, out var ent, out var gun) ||
+            HasComp<ItemComponent>(user)) // Delta-V: Felinids in duffelbags can't shoot.
         {
             return;
         }
