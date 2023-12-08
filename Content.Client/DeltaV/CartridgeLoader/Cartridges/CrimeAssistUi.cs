e8d01ea8fb7f98e8e291b5383e4b2b2376aca06f
Tim <timfalken@hotmail.com>
2023-12-08 20:39:20 +0100

    Move page to its own file

--- Content.Client/DeltaV/CartridgeLoader/Cartridges/CrimeAssistUi.cs
+++ Content.Client/DeltaV/CartridgeLoader/Cartridges/CrimeAssistUi.cs
@@ -32,36 +32,4 @@ public sealed partial class CrimeAssistUi : UIFragment
     public override void UpdateState(BoundUserInterfaceState state)
     {
     }
-
-    [Prototype("crimeAssistPage")]
-    public sealed partial class CrimeAssistPage : IPrototype
-    {
-        [ViewVariables]
-        [IdDataField]
-        public string ID { get; private set; } = "";
-
-        [DataField("onStart")]
-        public string? OnStart { get; private set; }
-
-        [DataField("locKey")]
-        public string? LocKey { get; private set; }
-
-        [DataField("onYes")]
-        public string? OnYes { get; private set; }
-
-        [DataField("onNo")]
-        public string? OnNo { get; private set; }
-
-        [DataField("locKeyTitle")]
-        public string? LocKeyTitle { get; private set; }
-
-        [DataField("locKeyDescription")]
-        public string? LocKeyDescription { get; private set; }
-
-        [DataField("locKeySeverity")]
-        public string? LocKeySeverity { get; private set; }
-
-        [DataField("locKeyPunishment")]
-        public string? LocKeyPunishment { get; private set; }
-    }
 }
