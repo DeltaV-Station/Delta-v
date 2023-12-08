754b4a4aa1456f167fe06566708266700b9ad91e
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2023-12-08 13:50:41 -0500

    Experimental Anomaly Vessel (#22233)
    * experimental anomaly vessel
    
    * sprote changes
    
    (cherry picked from commit e7e7974a24d03e3da788cd55ff4987d278148927)

--- Content.Server/Anomaly/AnomalySystem.Vessel.cs
+++ Content.Server/Anomaly/AnomalySystem.Vessel.cs
@@ -21,7 +21,6 @@ public sealed partial class AnomalySystem
     {
         SubscribeLocalEvent<AnomalyVesselComponent, ComponentShutdown>(OnVesselShutdown);
         SubscribeLocalEvent<AnomalyVesselComponent, MapInitEvent>(OnVesselMapInit);
-        SubscribeLocalEvent<AnomalyVesselComponent, RefreshPartsEvent>(OnRefreshParts);
         SubscribeLocalEvent<AnomalyVesselComponent, UpgradeExamineEvent>(OnUpgradeExamine);
         SubscribeLocalEvent<AnomalyVesselComponent, InteractUsingEvent>(OnVesselInteractUsing);
         SubscribeLocalEvent<AnomalyVesselComponent, ExaminedEvent>(OnExamined);
@@ -69,12 +68,6 @@ public sealed partial class AnomalySystem
         UpdateVesselAppearance(uid,  component);
     }
 
-    private void OnRefreshParts(EntityUid uid, AnomalyVesselComponent component, RefreshPartsEvent args)
-    {
-        var modifierRating = args.PartRatings[component.MachinePartPointModifier] - 1;
-        component.PointMultiplier = MathF.Pow(component.PartRatingPointModifier, modifierRating);
-    }
-
     private void OnUpgradeExamine(EntityUid uid, AnomalyVesselComponent component, UpgradeExamineEvent args)
     {
         args.AddPercentageUpgrade("anomaly-vessel-component-upgrade-output", component.PointMultiplier);
@@ -102,6 +95,7 @@ public sealed partial class AnomalySystem
 
         component.Anomaly = scanner.ScannedAnomaly;
         anomalyComponent.ConnectedVessel = uid;
+        _radiation.SetSourceEnabled(uid, true);
         UpdateVesselAppearance(uid,  component);
         Popup.PopupEntity(Loc.GetString("anomaly-vessel-component-anomaly-assigned"), uid);
     }
@@ -129,6 +123,7 @@ public sealed partial class AnomalySystem
 
             component.Anomaly = null;
             UpdateVesselAppearance(ent,  component);
+            _radiation.SetSourceEnabled(ent, false);
 
             if (!args.Supercritical)
                 continue;
