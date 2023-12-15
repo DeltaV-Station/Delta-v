edf74ff68bf1afc6dfe6d75b588de32330efbec1
leo <136020119+leonardo-dabepis@users.noreply.github.com>
2023-12-15 22:33:48 +1000

    little glimmer guy

---
+++ Content.Server/DeltaV/StationEvents/Components/GlimmerMiteRuleComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server.StationEvents.Events;
+
+namespace Content.Server.StationEvents.Components;
+
+[RegisterComponent, Access(typeof(GlimmerMiteRule))]
+public sealed partial class GlimmerMiteRuleComponent : Component
+{
+}
