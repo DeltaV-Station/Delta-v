ec512329773aaf7dd0b7aab5b2748350bbbd260e
Debug <49997488+DebugOk@users.noreply.github.com>
2024-01-15 00:48:30 +0100

    Spare ID safe (#671)
    * Initial commit
    
    * Unused access level
    
    * Update meta.json
    
    * Update SharedBiscuitComponent.cs
    
    * Unneeded DataField and VVAccess
    
    Editing it in VV does nothing anyways, it wont magically unlock the item slot
    
    * Big smart
    
    * Add safe sprite
    
    Thanks @TadJohnson00
    
    Co-Authored-By: Tad "Taddy" Johnson <120885811+TadJohnson00@users.noreply.github.com>
    
    * Prevent faxing slips
    
    * Custom background for corperate slip
    
    * Localize crack verb
    
    * Update paperslips.yml
    
    * Update paperslips.yml
    
    * Remove default comments
    
    * Update paperslips.yml
    
    * Pro
    
    * Remove default id card
    
    * Update Resources/Prototypes/DeltaV/Entities/Objects/Specific/Command/safe.yml
    
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: Debug <49997488+DebugOk@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Debug <49997488+DebugOk@users.noreply.github.com>
    Co-authored-by: Tad "Taddy" Johnson <120885811+TadJohnson00@users.noreply.github.com>
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

---
+++ Content.Server/DeltaV/Biscuit/BiscuitComponent.cs
@@ -0,0 +1,10 @@
+﻿using Content.Shared.DeltaV.Biscuit;
+
+namespace Content.Server.DeltaV.Biscuit;
+
+[RegisterComponent]
+public sealed partial class BiscuitComponent : SharedBiscuitComponent
+{
+    [DataField]
+    public bool Cracked { get; set; }
+}
