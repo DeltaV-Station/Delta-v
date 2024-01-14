df4efe56676c49504da834a1fff6c719ec8c100e
VMSolidus <evilexecutive@gmail.com>
2024-01-14 17:21:51 -0500

    Update PirateRadioSpawnRule.cs

--- Content.Server/DeltaV/StationEvents/Events/PirateRadioSpawnRule.cs
+++ Content.Server/DeltaV/StationEvents/Events/PirateRadioSpawnRule.cs
@@ -1,11 +1,23 @@
 using Robust.Server.GameObjects;
 using Robust.Server.Maps;
+using Robust.Server.Player;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Configuration;
+using Robust.Shared.Enums;
 using Robust.Shared.Map;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Utility;
 using Content.Server.GameTicking;
 using Content.Server.GameTicking.Rules;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.StationEvents.Components;
 using Content.Server.RoundEnd;
+using Content.Server.Station.Components;
+using System.Linq;
+using System.Numerics;
 
 namespace Content.Server.StationEvents.Events;
 
@@ -13,20 +25,32 @@ public sealed class PirateRadioSpawnRule : StationEventSystem<PirateRadioSpawnRu
 {
     [Dependency] private readonly IMapManager _mapManager = default!;
     [Dependency] private readonly MapLoaderSystem _map = default!;
-    [Dependency] private readonly GameTicker _gameTicker = default!;
-    [Dependency] private readonly TraitorRuleSystem _TraitorRuleSystem = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
 
     protected override void Started(EntityUid uid, PirateRadioSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
         base.Started(uid, component, gameRule, args);
-		
-        var shuttleMap = _mapManager.CreateMap();
-        var options = new MapLoadOptions
-        {
-            LoadMap = true,
-        };
 
-        _map.TryLoad(shuttleMap, component.PirateRadioShuttlePath, out _, options);
+        var xformQuery = GetEntityQuery<TransformComponent>();
+
+        var aabbs = EntityQuery<StationDataComponent>().SelectMany(x =>
+                x.Grids.Select(x =>
+                    xformQuery.GetComponent(x).WorldMatrix.TransformBox(_mapManager.GetGridComp(x).LocalAABB)))
+            .ToArray();
+        if (aabbs.Length < 1) return;
+        var aabb = aabbs[0];
+
+        for (var i = 1; i < aabbs.Length; i++)
+        {
+            aabb.Union(aabbs[i]);
+        }
+        var a = MathF.Max(aabb.Height / 2f, aabb.Width / 2f) * 20f;
+        var randomoffset = _random.NextVector2(a, a * 2.5f);
+        _map.TryLoad(GameTicker.DefaultMap, component.PirateRadioShuttlePath, out _, new MapLoadOptions
+        {
+            Offset = aabb.Center + randomoffset,
+            LoadMap = false,
+        });
     }
 
     protected override void Ended(EntityUid uid, PirateRadioSpawnRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
