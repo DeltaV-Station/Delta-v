069daf62a76b1484debad3523ac268085f2dce41
Debug <49997488+DebugOk@users.noreply.github.com>
2024-01-14 14:35:44 +0100

    Initial commit

---
+++ Content.Server/DeltaV/Biscuit/BiscuitComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.DeltaV.Biscuit;
+
+namespace Content.Server.DeltaV.Biscuit;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class BiscuitComponent : SharedBiscuitComponent
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("cracked")]
+    public bool Cracked { get; set; }
+}
---
+++ Content.Shared/DeltaV/Biscuit/SharedBiscuitComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.DeltaV.Biscuit;
+
+[Serializable, NetSerializable]
+public enum BiscuitStatus : byte
+{
+    Cracked
+}
