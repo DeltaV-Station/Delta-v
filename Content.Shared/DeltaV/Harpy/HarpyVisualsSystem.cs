c5eba53a975cde1e0f11484bde4cf36b05a44b07
VMSolidus <evilexecutive@gmail.com>
2024-01-15 23:09:12 -0500

    Runs smoother from Shared

---
+++ Content.Shared/DeltaV/Harpy/HarpyVisualsSystem.cs
@@ -0,0 +1,39 @@
+using Content.Shared.DeltaV.Harpy;
+using Content.Shared.Inventory.Events;
+using Content.Shared.Tag;
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.DeltaV.Harpy;
+
+public sealed class HarpyVisualsSystem : EntitySystem
+{
+    [Dependency] private readonly TagSystem _tagSystem = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
+
+    [ValidatePrototypeId<TagPrototype>]
+    private const string HarpyWingsTag = "HidesHarpyWings";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<HarpySingerComponent, DidEquipEvent>(OnDidEquipEvent);
+        SubscribeLocalEvent<HarpySingerComponent, DidUnequipEvent>(OnDidUnequipEvent);
+    }
+
+    private void OnDidEquipEvent(EntityUid uid, HarpySingerComponent component, DidEquipEvent args)
+    {
+        if (args.Slot == "outerClothing" && _tagSystem.HasTag(args.Equipment, HarpyWingsTag))
+        {
+            _appearanceSystem.SetData(uid, HardsuitWings.Worn, true);
+        }
+    }
+
+    private void OnDidUnequipEvent(EntityUid uid, HarpySingerComponent component, DidUnequipEvent args)
+    {
+        if (args.Slot == "outerClothing" && _tagSystem.HasTag(args.Equipment, HarpyWingsTag))
+        {
+            _appearanceSystem.SetData(uid, HardsuitWings.Worn, false);
+        }
+    }
+}
