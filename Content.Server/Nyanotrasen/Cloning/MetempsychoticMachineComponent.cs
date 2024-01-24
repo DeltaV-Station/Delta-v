8dd1b9110921aebf1585a1297d334081c1e6c0bd
VMSolidus <evilexecutive@gmail.com>
2024-01-23 21:53:11 -0500

    Update Content.Server/Nyanotrasen/Cloning/MetempsychoticMachineComponent.cs
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>

--- Content.Server/Nyanotrasen/Cloning/MetempsychoticMachineComponent.cs
+++ Content.Server/Nyanotrasen/Cloning/MetempsychoticMachineComponent.cs
@@ -1,4 +1,4 @@
-namespace Content.Server.Cloning
+namespace Content.Server.Nyanotrasen.Cloning
 {
     [RegisterComponent]
     public sealed partial class MetempsychoticMachineComponent : Component
