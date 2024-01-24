6eb6fbc880bbdcddc6cb290083ffbc7e4451d1b3
VMSolidus <evilexecutive@gmail.com>
2024-01-23 21:52:57 -0500

    Update Content.Server/Nyanotrasen/Cloning/MetempsychosisKarmaComponent.cs
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>

--- Content.Server/Nyanotrasen/Cloning/MetempsychosisKarmaComponent.cs
+++ Content.Server/Nyanotrasen/Cloning/MetempsychosisKarmaComponent.cs
@@ -1,4 +1,4 @@
-namespace Content.Server.Cloning
+namespace Content.Server.Nyanotrasen.Cloning
 {
     /// <summary>
     /// This tracks how many times you have already been cloned and lowers your chance of getting a humanoid each time.
