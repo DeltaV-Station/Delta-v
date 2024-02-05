0bb0f7b37940053db83ee1176b28281c7fbe9bab
VMSolidus <evilexecutive@gmail.com>
2024-02-04 21:36:06 -0500

    Code optimizations

--- Content.Client/DeltaV/Overlays/UltraVisionSystem.cs
+++ Content.Client/DeltaV/Overlays/UltraVisionSystem.cs
@@ -1,13 +1,10 @@
 using Content.Shared.Abilities;
-using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
-using Robust.Client.Player;
 
 namespace Content.Client.DeltaV.Overlays;
 
 public sealed partial class UltraVisionSystem : EntitySystem
 {
-    [Dependency] private readonly IPlayerManager _player = default!;
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
 
     private UltraVisionOverlay _overlay = default!;
@@ -19,28 +16,16 @@ public sealed partial class UltraVisionSystem : EntitySystem
         SubscribeLocalEvent<UltraVisionComponent, ComponentInit>(OnUltraVisionInit);
         SubscribeLocalEvent<UltraVisionComponent, ComponentShutdown>(OnUltraVisionShutdown);
 
-        _player.LocalPlayerAttached += OnAttachedChanged;
-        _player.LocalPlayerDetached += OnAttachedChanged;
-
         _overlay = new();
-    }
-
-    private void OnAttachedChanged(EntityUid uid)
-    {
-        _overlayMan.AddOverlay(_overlay);
     }
 
     private void OnUltraVisionInit(EntityUid uid, UltraVisionComponent component, ComponentInit args)
     {
-        if (_player.LocalPlayer?.ControlledEntity == uid)
-            _overlayMan.AddOverlay(_overlay);
+        _overlayMan.AddOverlay(_overlay);
     }
 
     private void OnUltraVisionShutdown(EntityUid uid, UltraVisionComponent component, ComponentShutdown args)
     {
-        if (_player.LocalPlayer?.ControlledEntity == uid)
-        {
-            _overlayMan.RemoveOverlay(_overlay);
-        }
+        _overlayMan.RemoveOverlay(_overlay);
     }
 }
