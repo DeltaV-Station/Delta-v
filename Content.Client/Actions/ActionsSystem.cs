9d39233f18aaa098ea8903b7e8e8f41c649bf7cd
Debug <49997488+DebugOk@users.noreply.github.com>
2024-02-19 19:56:25 +0100

    Allow loadacts to take a file

--- Content.Client/Actions/ActionsSystem.cs
+++ Content.Client/Actions/ActionsSystem.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Linq;
 using Content.Shared.Actions;
+using Content.Shared.Mapping;
 using JetBrains.Annotations;
 using Robust.Client.Player;
 using Robust.Shared.ContentPack;
@@ -294,6 +295,70 @@ namespace Content.Client.Actions
 
                 if (map.TryGet<ValueDataNode>("name", out var nameNode))
                     _metaData.SetEntityName(actionId, nameNode.Value);
+
+                if (!map.TryGet("assignments", out var assignmentNode))
+                    continue;
+
+                var nodeAssignments = _serialization.Read<List<(byte Hotbar, byte Slot)>>(assignmentNode, notNullableOverride: true);
+
+                foreach (var index in nodeAssignments)
+                {
+                    var assignment = new SlotAssignment(index.Hotbar, index.Slot, actionId);
+                    assignments.Add(assignment);
+                }
+            }
+
+            AssignSlot?.Invoke(assignments);
+        }
+
+        /// <summary>
+        ///     Load actions and their toolbar assignments from a file.
+        ///     DeltaV - Load from an existing yaml stream instead
+        /// </summary>
+        public void LoadActionAssignments(YamlStream stream)
+        {
+            if (_playerManager.LocalEntity is not { } user)
+                return;
+
+            if (stream.Documents[0].RootNode.ToDataNode() is not SequenceDataNode sequence)
+                return;
+
+            ClearAssignments?.Invoke();
+
+            var assignments = new List<SlotAssignment>();
+            var existingActions = GetClientActions();
+            var existingActionsList = existingActions.ToList();
+
+            foreach (var entry in sequence.Sequence)
+            {
+                if (entry is not MappingDataNode map)
+                    continue;
+
+                if (!map.TryGet("action", out var actionNode))
+                    continue;
+
+                if (!map.TryGet<ValueDataNode>("name", out var nameNode))
+                    continue;
+
+                var action = _serialization.Read<BaseActionComponent>(actionNode, notNullableOverride: true);
+
+                // Prevent spawning actions multiple times
+                var existing = existingActionsList.FirstOrNull(a =>
+                    Name(a.Id) == nameNode.Value);
+
+                EntityUid actionId;
+                if (existing == null)
+                {
+                    actionId = Spawn(null);
+                    AddComp(actionId, action);
+                    _metaData.SetEntityName(actionId, nameNode.Value);
+                    DirtyEntity(actionId);
+                    AddActionDirect(user, actionId);
+                }
+                else
+                {
+                    actionId = existing.Value.Id;
+                }
 
                 if (!map.TryGet("assignments", out var assignmentNode))
                     continue;
