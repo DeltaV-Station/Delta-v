918825ba9b118b96717b1c722b1b56d1de96fa58
Debug <49997488+DebugOk@users.noreply.github.com>
2024-03-01 00:02:08 +0100

    Prevent deconstruction of the EPU
    Surely this wont break anything...
    Cant wait for https://github.com/space-wizards/space-station-14/pull/24654 to be merged

---
+++ Content.Server/DeltaV/Construction/Components/PreventDeconstructionComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Server.DeltaV.Construction.Components;
+
+[RegisterComponent]
+public sealed partial class PreventDeconstructionComponent : Component
+{
+    [DataField]
+    public bool RemoveWirePanel;
+}
---
+++ Content.Server/DeltaV/Construction/PreventDeconstructionSystem.cs
@@ -0,0 +1,20 @@
+﻿using Content.Server.Construction.Components;
+using Content.Server.DeltaV.Construction.Components;
+using Content.Shared.Wires;
+
+namespace Content.Server.DeltaV.Construction;
+
+public sealed class PreventDeconstructionSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<PreventDeconstructionComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(EntityUid uid, PreventDeconstructionComponent component, MapInitEvent args)
+    {
+        RemComp<ConstructionComponent>(uid);
+        if (component.RemoveWirePanel)
+            RemComp<WiresPanelComponent>(uid);
+    }
+}
