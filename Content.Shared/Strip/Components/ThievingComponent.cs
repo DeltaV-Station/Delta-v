2ff6a67be4514bc0c361d151d9ea6136f89a0497
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2024-04-17 21:40:51 +1000

    Strip UI can now check for Thieving Component to see hidden items

--- Content.Shared/Strip/Components/ThievingComponent.cs
+++ Content.Shared/Strip/Components/ThievingComponent.cs
@@ -1,3 +1,5 @@
+using SixLabors.ImageSharp;
+
 namespace Content.Shared.Strip.Components;
 
 /// <summary>
@@ -9,14 +11,21 @@ public sealed partial class ThievingComponent : Component
     /// <summary>
     /// How much the strip time should be shortened by
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("stripTimeReduction")]
+    [ViewVariables(VVAccess.ReadWrite), DataField("stripTimeReduction")]
     public TimeSpan StripTimeReduction = TimeSpan.FromSeconds(0.5f);
+
+    [ViewVariables(VVAccess.ReadWrite), DataField("stripTimeMultiplier")]
+    public float StripTimeMultiplier = 1f;
 
     /// <summary>
     /// Should it notify the user if they're stripping a pocket?
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("stealthy")]
+    [ViewVariables(VVAccess.ReadWrite), DataField("stealthy")]
     public bool Stealthy;
+
+    /// <summary>
+    ///  Should the user be able to see hidden items? (i.e pockets)
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField("ignoreStripHidden")]
+    public bool IgnoreStripHidden = false;
 }
