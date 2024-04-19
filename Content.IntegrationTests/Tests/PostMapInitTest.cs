afbbb7549ade27cd737468b1876b0e41e5361d43
deltanedas <@deltanedas:kde.org>
2024-04-19 04:44:19 +0100

    add to test

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -58,6 +58,7 @@ namespace Content.IntegrationTests.Tests
             "TheHive", //DeltaV
             "Hammurabi", //DeltaV
             "Lighthouse", //DeltaV
+            "Saucer", //DeltaV
 			"Submarine" //DeltaV
         };
 
