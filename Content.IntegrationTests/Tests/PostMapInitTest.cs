8809b2d5ea636bb2f8ead6bda1b619dd1bc1b3db
Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
2024-04-22 14:24:03 +0100

    Update PostMapInitTest.cs
    Signed-off-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -57,7 +57,7 @@ namespace Content.IntegrationTests.Tests
             "Asterisk", //DeltaV
             "TheHive", //DeltaV
             "Hammurabi", //DeltaV
-			"Angle" //DeltaV
+			"Angle", //DeltaV
             "Lighthouse", //DeltaV
 			"Submarine" //DeltaV
         };
