bcf9e6c8db79743da9177807972b43b85594efd1
endr_animet <mingachev07@bk.ru>
2024-05-15 18:59:01 +0400

    fix

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -25,39 +25,6 @@ namespace Content.Shared.Roles
 
         public string GetGear(string slot)
         {
-            if (profile != null)
-            {
-                if (slot == "jumpsuit" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(InnerClothingSkirt)
-                    || slot == "jumpsuit" && profile.Species == "Harpy" && !string.IsNullOrEmpty(InnerClothingSkirt)) //DeltaV adds this line to prevent Harpies from starting with jumpsuits
-                    return InnerClothingSkirt;
-                if (slot == "back" && profile.Backpack == BackpackPreference.Satchel && !string.IsNullOrEmpty(Satchel))
-                    return Satchel;
-                if (slot == "back" && profile.Backpack == BackpackPreference.Duffelbag && !string.IsNullOrEmpty(Duffelbag))
-                    return Duffelbag;
-
-                // Plasmaman things, high priority
-                if (slot == "outerClothing" && profile.Species == "Plasmaman")
-                    return "ClothingOuterHardsuitEnvirosuit";
-                if (slot == "head" && profile.Species == "Plasmaman")
-                    return "ClothingHeadHelmetHardsuitEnvirosuitDelete";
-                if (slot == "suitstorage" && profile.Species == "Plasmaman")
-                    return "PlasmaTankFilled";
-                if (slot == "mask" && profile.Species == "Plasmaman")
-                    return "ClothingMaskBreath";
-
-                // Trait things, very low priority (for now)
-                if (slot == "eyes") {
-                    // Awful solution, do something shorter if possible :)
-                    var booleanthing = false;
-
-                    foreach (var trait in profile.TraitPreferences)
-                    {
-                        if (trait == "Nearsighted") booleanthing = true;
-                    }
-
-                    if (booleanthing == true) return "ClothingEyesGlasses";
-                }
-            }
             return Equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
         }
     }
