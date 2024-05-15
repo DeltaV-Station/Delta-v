33f3e96490663b4d521f50e0d726130de5af5002
deltanedas <@deltanedas:kde.org>
2024-05-15 20:50:04 +0100

    move paradox code around and update it

---
+++ Content.Server/DeltaV/StationEvents/Components/ParadoxClonerRuleComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Server.StationEvents.Components;
+
+/// <summary>
+/// Creates a paradox anomaly of a random person when taken by a player.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ParadoxClonerRuleComponent : Component;
