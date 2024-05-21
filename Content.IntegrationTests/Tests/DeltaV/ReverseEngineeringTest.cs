7432332e616d568a6632be8ac034213a8a50aed2
deltanedas <@deltanedas:kde.org>
2024-05-21 08:11:07 +0100

    add test for missing recipes

---
+++ Content.IntegrationTests/Tests/DeltaV/ReverseEngineeringTest.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using Content.Shared.Lathe;
+using Content.Shared.Research.Prototypes;
+using Content.Shared.ReverseEngineering;
+using Robust.Shared.Prototypes;
+
+namespace Content.IntegrationTests.Tests.DeltaV;
+
+[TestFixture]
+public sealed class ReverseEngineeringTest
+{
+    [Test]
+    public async Task AllReverseEngineeredPrintableTest()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
+
+        await server.WaitAssertion(() =>
+        {
+            var lathes = new List<LatheComponent>();
+            var reverseEngineered = new List<ProtoId<LatheRecipePrototype>>();
+            foreach (var proto in protoManager.EnumeratePrototypes<EntityPrototype>())
+            {
+                if (proto.Abstract)
+                    continue;
+
+                if (pair.IsTestPrototype(proto))
+                    continue;
+
+                if (proto.TryGetComponent<LatheComponent>(out var lathe))
+                    lathes.Add(lathe);
+
+                if (proto.TryGetComponent<ReverseEngineeringComponent>(out var rev))
+                    reverseEngineered.AddRange(rev.Recipes);
+            }
+
+            var latheRecipes = new HashSet<ProtoId<LatheRecipePrototype>>();
+            foreach (var lathe in lathes)
+            {
+                if (lathe.DynamicRecipes == null)
+                    continue;
+
+                foreach (var recipe in lathe.DynamicRecipes)
+                {
+                    latheRecipes.Add(recipe);
+                }
+            }
+
+            Assert.Multiple(() =>
+            {
+                foreach (var recipe in reverseEngineered)
+                {
+                    Assert.That(latheRecipes, Does.Contain(recipe), $"Reverse engineered recipe \"{recipe}\" cannot be unlocked on any lathe.");
+                }
+            });
+        });
+
+        await pair.CleanReturnAsync();
+    }
+}
