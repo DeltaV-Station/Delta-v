810376f291010a06bc53bd84172f547e7eb8aeb5
pissdemon <165581243+pissdemon@users.noreply.github.com>
2024-05-27 00:04:21 +0000

    Syndie radio implant: fix doc string

--- Content.Shared/DeltaV/Implants/Radio/RadioImplantComponent.cs
+++ Content.Shared/DeltaV/Implants/Radio/RadioImplantComponent.cs
@@ -3,7 +3,7 @@
 namespace Content.Shared.DeltaV.Implants.Radio;
 
 /// <summary>
-/// This is used for radio implants.
+/// This is for radio implants. Might be Syndie, might not be Syndie, but either way, it's an implant.
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(SharedRadioImplantSystem))]
 public sealed partial class RadioImplantComponent : Component
@@ -13,6 +13,7 @@ public sealed partial class RadioImplantComponent : Component
     /// </summary>
     [DataField, AutoNetworkedField]
     public EntityUid? Implantee { get; set; }
+
     /// <summary>
     /// The channels this implant can talk on.
     /// </summary>
