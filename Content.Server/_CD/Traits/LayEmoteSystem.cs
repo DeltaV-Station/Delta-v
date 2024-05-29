0a0ad2d941d6bdc937e8473de35e3499ed0010ce
Adrian16199 <144424013+Adrian16199@users.noreply.github.com>
2024-05-29 19:33:17 +0200

    Update Content.Server/_CD/Traits/LayEmoteSystem.cs
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: Adrian16199 <144424013+Adrian16199@users.noreply.github.com>

--- Content.Server/_CD/Traits/LayEmoteSystem.cs
+++ Content.Server/_CD/Traits/LayEmoteSystem.cs
@@ -76,7 +76,7 @@ public sealed class LayEmoteSystem : EntitySystem
     private void OnRefreshMovementSpeedModifiers(EntityUid uid, LayEmoteComponent component, RefreshMovementSpeedModifiersEvent args)
     {
         if (component.Laying)
-            args.ModifySpeed(0.1f, 0.1f); // Delta V- crawl, baby, crawl!
+            args.ModifySpeed(component.CrawlSpeed, component.CrawlSpeed); // Delta V- crawl, baby, crawl!
 
         if (!component.Laying)
             args.ModifySpeed(1f, 1f);
