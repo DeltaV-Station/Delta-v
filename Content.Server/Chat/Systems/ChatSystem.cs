11f6eba9011e673d446d2639bc4381cd12f4e280
MilenVolf <63782763+MilenVolf@users.noreply.github.com>
2024-06-01 23:34:58 +0300

    Station event component and system tweaks (#28331)
    * Make anomaly, artifact and gifts events announcement sound optional
    
    * Requested changes + Added new "GameRuleAfterAddedEvent" for StationEventSystem
    
    We need to call "Add" in "StationEventSystem" after others GameRule's in case if we need to change StationEventComponent variables.
    
    * Fix margins
    
    * Makes use of GameRuleComponent.Delay and remove station system handling of delays plus small cleanup
    
    * Fix merge
    
    ---------
    
    Co-authored-by: AJCM <AJCM@tutanota.com>

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -841,7 +841,7 @@ public sealed partial class ChatSystem : SharedChatSystem
                 recipients.Add(player, new ICChatRecipientData(-1, true));
         }
 
-        RaiseLocalEvent(new ExpandICChatRecipientstEvent(source, voiceGetRange, recipients));
+        RaiseLocalEvent(new ExpandICChatRecipientsEvent(source, voiceGetRange, recipients));
         return recipients;
     }
 
@@ -886,7 +886,7 @@ public sealed partial class ChatSystem : SharedChatSystem
 ///     This event is raised before chat messages are sent out to clients. This enables some systems to send the chat
 ///     messages to otherwise out-of view entities (e.g. for multiple viewports from cameras).
 /// </summary>
-public record ExpandICChatRecipientstEvent(EntityUid Source, float VoiceRange, Dictionary<ICommonSession, ChatSystem.ICChatRecipientData> Recipients)
+public record ExpandICChatRecipientsEvent(EntityUid Source, float VoiceRange, Dictionary<ICommonSession, ChatSystem.ICChatRecipientData> Recipients)
 {
 }
 
