fac8b63e0543978d5653831f8b602a8e7991cc39
deltanedas <@deltanedas:kde.org>
2024-06-13 00:39:13 +0100

    fixes

--- Content.Server/Roles/ListeningPostRoleComponent.cs
+++ Content.Server/Roles/ListeningPostRoleComponent.cs
@@ -5,5 +5,5 @@ namespace Content.Server.Roles;
 /// <summary>
 /// DeltaV - listening post ops have their own role
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, ExclusiveAntagonist]
 public sealed partial class ListeningPostRoleComponent : AntagonistRoleComponent;
