e6bafb2a64e6d62a1f0ea82411b3fd965f8f4124
Colin-Tel <113523727+Colin-Tel@users.noreply.github.com>
2024-06-12 12:30:02 -0400

    Adjust Hunger rates (#1302)
    * Upped hunger rates
    
    * vulp hunger and diona thirst
    
    * Revert "Upped hunger rates"
    
    This reverts commit da6bf536872e8decd61d1c6b6020041ae1a41d25.
    
    * changed hungercomponent
    
    * Update diona.yml
    
    adjusted diona hunger
    
    Signed-off-by: Colin-Tel <113523727+Colin-Tel@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Colin-Tel <113523727+Colin-Tel@users.noreply.github.com>
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>

--- Content.Shared/Nutrition/Components/HungerComponent.cs
+++ Content.Shared/Nutrition/Components/HungerComponent.cs
@@ -24,7 +24,7 @@ public sealed partial class HungerComponent : Component
     /// The base amount at which <see cref="CurrentHunger"/> decays.
     /// </summary>
     [DataField("baseDecayRate"), ViewVariables(VVAccess.ReadWrite)]
-    public float BaseDecayRate = 0.01666666666f;
+    public float BaseDecayRate = 0.05f; //DeltaV: changed from 0.01666666666f
 
     /// <summary>
     /// The actual amount at which <see cref="CurrentHunger"/> decays.
