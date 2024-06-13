db8bc652f186caa563bc35d8043b8f4804175382
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-06-12 23:38:43 -0700

    Fix playtime on player tab getting set to Unknown (#28876)
    Update play time on admin panel player tab after db loads

--- Content.Server/Players/PlayTimeTracking/PlayTimeTrackingManager.cs
+++ Content.Server/Players/PlayTimeTracking/PlayTimeTrackingManager.cs
@@ -82,6 +82,8 @@ public sealed partial class PlayTimeTrackingManager : ISharedPlaytimeManager, IP
 
     public event CalcPlayTimeTrackersCallback? CalcTrackers;
 
+    public event Action<ICommonSession>? SessionPlayTimeUpdated;
+
     public void Initialize()
     {
         _sawmill = Logger.GetSawmill("play_time");
@@ -225,6 +227,7 @@ public sealed partial class PlayTimeTrackingManager : ISharedPlaytimeManager, IP
         };
 
         _net.ServerSendMessage(msg, pSession.Channel);
+        SessionPlayTimeUpdated?.Invoke(pSession);
     }
 
     /// <summary>
