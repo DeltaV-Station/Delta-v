efe55e90980e2236af50a3421f21eac8a0cfe7ab
deltanedas <@deltanedas:kde.org>
2024-06-13 21:13:04 +0100

    add to test :trollface:

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -55,6 +55,7 @@ namespace Content.IntegrationTests.Tests
             "Tortuga", //DeltaV
             "Arena", //DeltaV
             "Asterisk", //DeltaV
+            "Glacier", //DeltaV
             "TheHive", //DeltaV
             "Hammurabi", //DeltaV
             "Lighthouse", //DeltaV
