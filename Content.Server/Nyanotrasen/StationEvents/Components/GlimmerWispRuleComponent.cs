047a4931ba74568313dc811b044a2b716415b693
deltanedas <@deltanedas:kde.org>
2024-06-14 10:29:44 +0100

    remove glimmer wisp rule and make GlimmerMobRule much more flexible + better

--- Content.Server/Nyanotrasen/StationEvents/Components/GlimmerWispRuleComponent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Server.Nyanotrasen.StationEvents.Events;
-using Content.Server.StationEvents.Events;
-
-namespace Content.Server.StationEvents.Components;
-
-[RegisterComponent, Access(typeof(GlimmerWispRule))]
-public sealed partial class GlimmerWispRuleComponent : Component
-{
-}
