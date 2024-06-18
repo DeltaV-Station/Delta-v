3dfe86be427fd99ded937be081396f807ce08c55
Bellwether <null>
2024-06-18 11:00:15 -0700

    Attempting to slay a rogue byte order mark

--- Content.Shared/Security/SecurityStatus.cs
+++ Content.Shared/Security/SecurityStatus.cs
@@ -9,7 +9,7 @@ namespace Content.Shared.Security;
 /// Detained - the person is detained by security
 /// Paroled - the person is on parole
 /// Discharged - the person has been released from prison
-/// # DeltaV - Subpoenaed - the person has been subpoenaed by the Justice Department 
+/// # DeltaV - Subpoenaed - the person has been subpoenaed by the Justice Dept. 
 /// </summary>
 public enum SecurityStatus : byte
 {
