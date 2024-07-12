5d7463a9ed57c9038a9395b42faf0eba8f50ef5f
portfiend <109661617+portfiend@users.noreply.github.com>
2024-07-12 18:22:18 -0400

    add a 1 second cooldown between phrases

--- Content.Server/DeltaV/AACTablet/AACTabletComponent.cs
+++ Content.Server/DeltaV/AACTablet/AACTabletComponent.cs
@@ -3,5 +3,12 @@ namespace Content.Server.DeltaV.AACTablet;
 [RegisterComponent]
 public sealed partial class AACTabletComponent : Component
 {
+    // Minimum time between each phrase, to prevent spam
+    [DataField]
+    public TimeSpan Cooldown = TimeSpan.FromSeconds(1);
 
+    // Time that the next phrase can be sent.
+    [DataField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan NextPhrase;
 }
--- Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
+++ Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
@@ -2,6 +2,7 @@ using Content.Server.Chat.Systems;
 using Content.Shared.DeltaV.AACTablet;
 using Content.Shared.DeltaV.QuickPhrase;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Server.DeltaV.AACTablet;
 
@@ -10,6 +11,7 @@ public sealed class AACTabletSystem : EntitySystem
     [Dependency] private readonly ChatSystem _chat = default!;
     [Dependency] private readonly ILocalizationManager _loc = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] protected readonly IGameTiming Timing = default!;
 
     public override void Initialize()
     {
@@ -19,8 +21,13 @@ public sealed class AACTabletSystem : EntitySystem
 
     private void OnSendPhrase(EntityUid uid, AACTabletComponent component, AACTabletSendPhraseMessage message)
     {
-        var phrase = _prototypeManager.Index<QuickPhrasePrototype>(message.PhraseID);
+        if (component.NextPhrase > Timing.CurTime)
+            return;
 
+        var phrase = _prototypeManager.Index<QuickPhrasePrototype>(message.PhraseID);
         _chat.TrySendInGameICMessage(uid, _loc.GetString(phrase.Text), InGameICChatType.Speak, false);
+
+        var curTime = Timing.CurTime;
+        component.NextPhrase = curTime + component.Cooldown;
     }
 }
