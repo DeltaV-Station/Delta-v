8cdb97bfea0a3980e80fca7fbbdab822c221a273
BioMass <155424652+Tetronamecronicus@users.noreply.github.com>
2024-07-13 21:30:05 +0300

    singl

--- Content.Shared/Singularity/Components/SingularityDistortionComponent.cs
+++ Content.Shared/Singularity/Components/SingularityDistortionComponent.cs
@@ -11,6 +11,9 @@ namespace Content.Shared.Singularity.Components
         [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
         public float Intensity = 31.25f;
 
+        [DataField("energy"), AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
+        public float Energy = 150f;
+
         [DataField, AutoNetworkedField, ViewVariables(VVAccess.ReadWrite)]
         public float FalloffPower = MathF.Sqrt(2f);
     }
