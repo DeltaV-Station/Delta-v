d7b823f15c6a8a17ed5ff99cfd96647d968d4087
portfiend <109661617+portfiend@users.noreply.github.com>
2024-07-21 18:21:12 -0400

    add: server mouthstoragesystem

---
+++ Content.Server/DeltaV/Storage/EntitySystems/MouthStorageSystem.cs
@@ -0,0 +1,6 @@
+using Content.Shared.DeltaV.Storage.EntitySystems;
+
+namespace Content.Server.DeltaV.Storage.EntitySystems;
+
+public sealed class MouthStorageSystem : SharedMouthStorageSystem
+{ }
