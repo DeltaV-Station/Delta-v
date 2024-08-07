b6f054399438938208f807e5709fecf2ef60ee63
Timemaster99 <57200767+Timemaster99@users.noreply.github.com>
2024-08-07 00:53:32 -0400

    IPCs are now alerted when another person tries to toggle their lock

--- Content.Server/_EstacaoPirata/BatteryLocking/BatterySlotRequiresLockSystem.cs
+++ Content.Server/_EstacaoPirata/BatteryLocking/BatterySlotRequiresLockSystem.cs
@@ -1,5 +1,8 @@
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Lock;
+using Content.Shared.Popups;
+using Content.Shared.SimpleStation14.Silicon.Components; // DeltaV
+using Content.Shared.IdentityManagement; // DeltaV
 
 namespace Content.Server.SimpleStation14.BatteryLocking;
 
@@ -7,12 +10,14 @@ public sealed class BatterySlotRequiresLockSystem : EntitySystem
 
 {
     [Dependency] private readonly ItemSlotsSystem _itemSlotsSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!; // DeltaV
 
     /// <inheritdoc/>
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<BatterySlotRequiresLockComponent, LockToggledEvent>(LockToggled);
+        SubscribeLocalEvent<BatterySlotRequiresLockComponent, LockToggleAttemptEvent>(LockToggleAttempted); // DeltaV
 
     }
     private void LockToggled(EntityUid uid, BatterySlotRequiresLockComponent component, LockToggledEvent args)
@@ -23,4 +28,12 @@ public sealed class BatterySlotRequiresLockSystem : EntitySystem
             return;
         _itemSlotsSystem.SetLock(uid, slot, lockComp.Locked, itemslots);
     }
+
+    // DeltaV - Alert IPCs when they are being unlocked
+    private void LockToggleAttempted(EntityUid uid, BatterySlotRequiresLockComponent component, LockToggleAttemptEvent args)
+    {
+        if (args.User != uid && TryComp<SiliconComponent>(uid, out var siliconComp))
+            _popupSystem.PopupEntity(Loc.GetString("batteryslotrequireslock-component-alert-owner", ("user", Identity.Entity(args.User, EntityManager))), uid, uid, PopupType.Large);
+    }
+    // End of DeltaV Code
 }
