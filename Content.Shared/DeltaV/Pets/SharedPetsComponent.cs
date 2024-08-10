ccd3a3326f88b6dc2ee1c2dd44b1caa5f89b410e
Timemaster99 <57200767+Timemaster99@users.noreply.github.com>
2024-08-10 13:37:29 -0400

    Added extra state to damage visualizer for carpy

---
+++ Content.Shared/DeltaV/Pets/SharedPetsComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.DeltaV.Pets
+{
+    [Serializable, NetSerializable]
+    public enum PetVisuals : byte
+    {
+        Clothing
+    }
+}
