49979f554c68c9547c0f5c1cf76e040e5c776a93
deltanedas <@deltanedas:kde.org>
2024-08-10 14:30:20 +0100

    give prisoner roundstart criminal record

---
+++ Content.Server/DeltaV/Prisoner/PrisonerRecordSystem.cs
@@ -0,0 +1,63 @@
+using Content.Server.CriminalRecords.Systems;
+using Content.Server.StationEvents.Components;
+using Content.Server.StationRecords.Systems;
+using Content.Shared.CriminalRecords;
+using Content.Shared.Roles;
+using Content.Shared.Security;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server.DeltaV.Prisoner;
+
+/// <summary>
+/// Gives prisoners a roundstart randomized criminal record and detained status.
+/// </summary>
+public sealed class PrisonerRecordSystem : EntitySystem
+{
+    [Dependency] private readonly CriminalRecordsSystem _criminalRecords = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly StationRecordsSystem _records = default!;
+
+    /// <summary>
+    /// Only give this job a criminal record.
+    /// </summary>
+    public ProtoId<JobPrototype> Prisoner = "Prisoner";
+
+    public RandomCrimes Crimes = new()
+    {
+        Dataset = "FugitiveCrimes",
+        Crimes = new(1, 5),
+        Counts = new(1, 20)
+    };
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        // make sure this is ran after criminal records system so it exists
+        SubscribeLocalEvent<AfterGeneralRecordCreatedEvent>(OnGeneralRecordCreated, after: [typeof(CriminalRecordsSystem)]);
+    }
+
+    private void OnGeneralRecordCreated(AfterGeneralRecordCreatedEvent args)
+    {
+        if (args.Record.JobPrototype != Prisoner)
+            return;
+
+        if (!_records.TryGetRecord<CriminalRecord>(args.Key, out var criminal))
+        {
+            Log.Error($"No criminal record found for {args.Key} somehow!");
+            return;
+        }
+
+        // Prisoners spawn in perma so start off detained
+        _criminalRecords.OverwriteStatus(args.Key, criminal, SecurityStatus.Detained, null);
+        var start = TimeSpan.Zero;
+        criminal.History.Add(new CrimeHistory(start, Loc.GetString("criminal-records-prisoner-record-header")));
+        foreach (var (crime, count) in Crimes.Pick(_proto, _random))
+        {
+            criminal.History.Add(new CrimeHistory(start, Loc.GetString("fugitive-report-crime", ("crime", crime), ("count", count))));
+        }
+        _records.Synchronize(args.Key);
+    }
+}
--- Resources/Locale/en-US/deltav/criminal-records/criminal-records.ftl
+++ Resources/Locale/en-US/deltav/criminal-records/criminal-records.ftl
@@ -3,3 +3,7 @@
 criminal-records-status-subpoenaed = Subpoenaed
 criminal-records-console-subpoenaed = {$name} has been issued a subpoena by the Justice Department and must report to Court immediately.
 criminal-records-console-not-subpoenaed = {$name} is no longer subpoenaed by the Justice Department.
+
+## Prisoner roundstart criminal record
+
+criminal-records-prisoner-record-header = Sentenced to extended confinement for these previous crimes:
