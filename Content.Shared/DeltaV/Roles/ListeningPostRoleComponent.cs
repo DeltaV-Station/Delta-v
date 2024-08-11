e4892c4272a1ce973f4e4d51712781121e96f7b8
deltanedas <@deltanedas:kde.org>
2024-08-11 15:47:24 +0100

    add paradox role, better role handling

--- Content.Shared/DeltaV/Roles/ListeningPostRoleComponent.cs
+++ Content.Shared/DeltaV/Roles/ListeningPostRoleComponent.cs
@@ -1,9 +1,6 @@
 using Content.Shared.Roles;
 
-namespace Content.Server.Roles;
+namespace Content.Shared.DeltaV.Roles;
 
-/// <summary>
-/// DeltaV - listening post ops have their own role
-/// </summary>
 [RegisterComponent, ExclusiveAntagonist]
 public sealed partial class ListeningPostRoleComponent : AntagonistRoleComponent;
---
+++ Resources/Locale/en-US/deltav/nutrition/components/food-sequence.ftl
@@ -0,0 +1,5 @@
+food-sequence-burger-content-grilled-cheese = grilled cheese
+food-sequence-burger-content-scrambled-eggs = scrambled egg
+food-sequence-burger-content-crystal-thistle = crystal
+food-sequence-burger-content-ghost-pepper = ghost pepper
+food-sequence-burger-content-revenant = revenant
