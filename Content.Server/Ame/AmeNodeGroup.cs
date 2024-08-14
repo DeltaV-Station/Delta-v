de826555a51f5cc24f8f27deb4f82256a481f8cf
Milon <plmilonpl@gmail.com>
2024-08-14 19:39:19 +0200

    we're SO BACK

--- Content.Server/Ame/AmeNodeGroup.cs
+++ Content.Server/Ame/AmeNodeGroup.cs
@@ -173,7 +173,7 @@ public sealed class AmeNodeGroup : BaseNodeGroup
 
         // The adjustment for cores make it so that a 1 core AME at 2 injections is better than a 2 core AME at 2 injections.
         // However, for the relative amounts for each (1 core at 2 and 2 core at 4), more cores has more output.
-        return 200000f * MathF.Log10(fuel * fuel) * MathF.Pow(0.75f, cores - 1);
+        return 200000f * MathF.Log10(MathF.Pow(fuel, 1.9f)) * (0.4f * (fuel / cores));
     }
 
     public int GetTotalStability()
