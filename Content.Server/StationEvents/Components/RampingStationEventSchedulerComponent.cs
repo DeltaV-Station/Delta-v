1d80ddbaf106974e072fc936c5596de33359df2d
IProduceWidgets <107586145+IProduceWidgets@users.noreply.github.com>
2024-08-15 15:33:17 -0400

    Event scheduler setting tweaks (#31018)
    tweaks

--- Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
+++ Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
@@ -3,7 +3,21 @@
 [RegisterComponent, Access(typeof(RampingStationEventSchedulerSystem))]
 public sealed partial class RampingStationEventSchedulerComponent : Component
 {
-    [DataField("endTime"), ViewVariables(VVAccess.ReadWrite)]
+    /// <summary>
+    ///     Average ending chaos modifier for the ramping event scheduler. Higher means faster.
+    ///     Max chaos chosen for a round will deviate from this
+    /// </summary>
+    [DataField]
+    public float AverageChaos = 12f;
+
+    /// <summary>
+    ///     Average time (in minutes) for when the ramping event scheduler should stop increasing the chaos modifier.
+    ///     Close to how long you expect a round to last, so you'll probably have to tweak this on downstreams.
+    /// </summary>
+    [DataField]
+    public float AverageEndTime = 90f;
+
+    [DataField]
     public float EndTime;
 
     [DataField("maxChaos"), ViewVariables(VVAccess.ReadWrite)]
