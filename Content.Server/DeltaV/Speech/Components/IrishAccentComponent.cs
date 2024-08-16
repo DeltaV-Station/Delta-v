0f36d740c22628e1140aef273b362a589c356cbf
SsalamethVersaach <thebluettdle@gmail.com>
2024-08-16 15:12:10 -0400

    Irish Accent now recognized.

--- Content.Server/DeltaV/Speech/Components/IrishAccentComponent.cs
+++ Content.Server/DeltaV/Speech/Components/IrishAccentComponent.cs
@@ -1,4 +1,6 @@
-namespace Content.Server.Speech.Components;
+using Content.Server.DeltaV.Speech.EntitySystems;
+
+namespace Content.Server.DeltaV.Speech.Components;
 
 [RegisterComponent]
 [Access(typeof(IrishAccentSystem))]
