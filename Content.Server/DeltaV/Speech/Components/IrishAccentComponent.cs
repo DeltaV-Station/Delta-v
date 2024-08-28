d262b0c39af202abcf3c6aeee87a6ab7130d0a5b
SsalamethVersaach <thebluettdle@gmail.com>
2024-08-28 17:00:40 -0400

    Cleanup with some comments and explanation

--- Content.Server/DeltaV/Speech/Components/IrishAccentComponent.cs
+++ Content.Server/DeltaV/Speech/Components/IrishAccentComponent.cs
@@ -2,6 +2,7 @@ using Content.Server.DeltaV.Speech.EntitySystems;
 
 namespace Content.Server.DeltaV.Speech.Components;
 
+// Takes the ES and assigns the system and component to each other
 [RegisterComponent]
 [Access(typeof(IrishAccentSystem))]
 public sealed partial class IrishAccentComponent : Component
