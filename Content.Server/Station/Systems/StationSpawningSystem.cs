d58bdd65ec73c28b40cca45ad3c179831ab8388f
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-08-28 10:57:12 +1000

    Station AI (#30944)
    * Station AI overlay
    
    * implement
    
    * Bunch of ports
    
    * Fix a heap of bugs and basic scouting
    
    * helldivers
    
    * Shuffle interactions a bit
    
    * navmap stuff
    
    * Revert "navmap stuff"
    
    This reverts commit d1f89dd4be83233e22cf5dd062b2581f3c6da062.
    
    * AI wires implemented
    
    * Fix examines
    
    * Optimise the overlay significantly
    
    * Back to old static
    
    * BUI radial working
    
    * lots of work
    
    * Saving work
    
    * thanks fork
    
    * alright
    
    * pc
    
    * AI upload console
    
    * AI upload
    
    * stuff
    
    * Fix copy-paste shitcode
    
    * AI actions
    
    * navmap work
    
    * Fixes
    
    * first impressions
    
    * a
    
    * reh
    
    * Revert "navmap work"
    
    This reverts commit 6f63fea6e9245e189f368f97be3e32e9b210580e.
    
    * OD
    
    * radar
    
    * weh
    
    * Fix examines
    
    * scoop mine eyes
    
    * fixes
    
    * reh
    
    * Optimise
    
    * Final round of optimisations
    
    * Fixes
    
    * fixes

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -18,6 +18,7 @@ using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.PDA;
 using Content.Shared.Preferences;
 using Content.Shared.Preferences.Loadouts;
+using Content.Shared.Preferences.Loadouts.Effects;
 using Content.Shared.Random;
 using Content.Shared.Random.Helpers;
 using Content.Shared.Roles;
@@ -153,6 +154,22 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
         EntityUid? entity = null)
     {
         _prototypeManager.TryIndex(job?.Prototype ?? string.Empty, out var prototype);
+        RoleLoadout? loadout = null;
+
+        // Need to get the loadout up-front to handle names if we use an entity spawn override.
+        var jobLoadout = LoadoutSystem.GetJobPrototype(prototype?.ID);
+
+        if (_prototypeManager.TryIndex(jobLoadout, out RoleLoadoutPrototype? roleProto))
+        {
+            profile?.Loadouts.TryGetValue(jobLoadout, out loadout);
+
+            // Set to default if not present
+            if (loadout == null)
+            {
+                loadout = new RoleLoadout(jobLoadout);
+                loadout.SetDefault(profile, _actors.GetSession(entity), _prototypeManager);
+            }
+        }
 
         // If we're not spawning a humanoid, we're gonna exit early without doing all the humanoid stuff.
         if (prototype?.JobEntity != null)
@@ -160,6 +177,13 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
             DebugTools.Assert(entity is null);
             var jobEntity = EntityManager.SpawnEntity(prototype.JobEntity, coordinates);
             MakeSentientCommand.MakeSentient(jobEntity, EntityManager);
+
+            // Make sure custom names get handled, what is gameticker control flow whoopy.
+            if (loadout != null)
+            {
+                EquipRoleName(jobEntity, loadout, roleProto!);
+            }
+
             DoJobSpecials(job, jobEntity);
             _identity.QueueIdentityUpdate(jobEntity);
             return jobEntity;
@@ -191,21 +215,9 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
             profile = HumanoidCharacterProfile.RandomWithSpecies(speciesId);
         }
 
-        var jobLoadout = LoadoutSystem.GetJobPrototype(prototype?.ID);
-
-        if (_prototypeManager.TryIndex(jobLoadout, out RoleLoadoutPrototype? roleProto))
+        if (loadout != null)
         {
-            RoleLoadout? loadout = null;
-            profile?.Loadouts.TryGetValue(jobLoadout, out loadout);
-
-            // Set to default if not present
-            if (loadout == null)
-            {
-                loadout = new RoleLoadout(jobLoadout);
-                loadout.SetDefault(profile, _actors.GetSession(entity), _prototypeManager);
-            }
-
-            EquipRoleLoadout(entity.Value, loadout, roleProto);
+            EquipRoleLoadout(entity.Value, loadout, roleProto!);
         }
 
         if (prototype?.StartingGear != null)
