57dc43f2dab9b79f0d806df36e742f9781910ac0
endr_animet <mingachev07@bk.ru>
2024-09-17 21:40:32 +0400

    New CentComm Station
    уэ... Я это в соло замаппил

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -37,7 +37,7 @@ namespace Content.IntegrationTests.Tests
 
         private static readonly string[] Grids =
         {
-            "/Maps/Ebengrad/centcomm.yml",
+            "/Maps/Stray/centcomm.yml",
             "/Maps/Shuttles/cargo.yml",
             "/Maps/Shuttles/emergency.yml",
             "/Maps/Shuttles/infiltrator.yml",
