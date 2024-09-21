334bc57589132798f9336e51a3da129ee9598d28
Milon <plmilonpl@gmail.com>
2024-09-21 19:23:21 +0200

    fix more shit from my doing of random shit

--- Content.Shared/Overlays/ShowHealthBarsComponent.cs
+++ Content.Shared/Overlays/ShowHealthBarsComponent.cs
@@ -10,14 +10,12 @@ namespace Content.Shared.Overlays;
 /// This component allows you to see health bars above damageable mobs.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState] // DeltaV - This should probably get reworked to use ProtoId but upstream
 public sealed partial class ShowHealthBarsComponent : Component
 {
     /// <summary>
     /// Displays health bars of the damage containers.
     /// </summary>
     [DataField("damageContainers", customTypeSerializer: typeof(PrototypeIdListSerializer<DamageContainerPrototype>))]
-    [AutoNetworkedField] // DeltaV
     public List<string> DamageContainers = new();
 
     [DataField]
--- Content.Shared/Overlays/ShowHealthIconsComponent.cs
+++ Content.Shared/Overlays/ShowHealthIconsComponent.cs
@@ -8,13 +8,11 @@ namespace Content.Shared.Overlays;
 /// This component allows you to see health status icons above damageable mobs.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState] // DeltaV - This should probably get reworked to use ProtoId but upstream
 public sealed partial class ShowHealthIconsComponent : Component
 {
     /// <summary>
     /// Displays health status icons of the damage containers.
     /// </summary>
     [DataField("damageContainers", customTypeSerializer: typeof(PrototypeIdListSerializer<DamageContainerPrototype>))]
-    [AutoNetworkedField] // DeltaV
     public List<string> DamageContainers = new();
 }
