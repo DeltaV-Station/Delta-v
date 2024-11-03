e63ae2bba3d6499e2b0584f3782f7c6d60d73f3e
Beck <beck314159@hotmail.com>
2024-11-03 08:33:25 -0800

    pro delta changes

--- Content.Server/DeltaV/Implants/SyrinxImplantSystem.cs
+++ Content.Server/DeltaV/Implants/SyrinxImplantSystem.cs
@@ -1,16 +1,10 @@
 using Content.Server.VoiceMask;
 using Content.Shared.Implants;
-using Content.Shared.Tag;
 
 namespace Content.Server.Implants;
 
 public sealed class SubdermalBionicSyrinxImplantSystem : EntitySystem
 {
-    [Dependency] private readonly TagSystem _tag = default!;
-
-    [ValidatePrototypeId<TagPrototype>]
-    private const string BionicSyrinxImplant = "BionicSyrinxImplant";
-
     public override void Initialize()
     {
         base.Initialize();
@@ -18,13 +12,12 @@ public sealed class SubdermalBionicSyrinxImplantSystem : EntitySystem
         SubscribeLocalEvent<VoiceMaskComponent, ImplantImplantedEvent>(OnInsert);
     }
 
-    private void OnInsert(EntityUid uid, VoiceMaskComponent component, ImplantImplantedEvent args)
+    private void OnInsert(Entity<VoiceMaskComponent> ent, ref ImplantImplantedEvent args)
     {
-        if (!args.Implanted.HasValue ||
-            !_tag.HasTag(args.Implant, BionicSyrinxImplant))
+        if (!args.Implanted.HasValue)
             return;
 
         // Update the name so it's the entities default name. You can't take it off like a voice mask so it's important!
-        component.VoiceMaskName = Name(args.Implanted.Value);
+        ent.Comp.VoiceMaskName = Name(args.Implanted.Value);
     }
 }
--- Content.Shared/Implants/SharedSubdermalImplantSystem.cs
+++ Content.Shared/Implants/SharedSubdermalImplantSystem.cs
@@ -1,6 +1,6 @@
 using System.Linq;
 using Content.Shared.Actions;
-using Content.Shared.Chat;
+using Content.Shared.Chat; // Delta-v
 using Content.Shared.Implants.Components;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
@@ -30,7 +30,7 @@ public abstract class SharedSubdermalImplantSystem : EntitySystem
         SubscribeLocalEvent<ImplantedComponent, MobStateChangedEvent>(RelayToImplantEvent);
         SubscribeLocalEvent<ImplantedComponent, AfterInteractUsingEvent>(RelayToImplantEvent);
         SubscribeLocalEvent<ImplantedComponent, SuicideEvent>(RelayToImplantEvent);
-        SubscribeLocalEvent<ImplantedComponent, TransformSpeakerNameEvent>(RelayToImplantEvent);
+        SubscribeLocalEvent<ImplantedComponent, TransformSpeakerNameEvent>(RelayToImplantEvent); // Delta-v
     }
 
     private void OnInsert(EntityUid uid, SubdermalImplantComponent component, EntGotInsertedIntoContainerMessage args)
