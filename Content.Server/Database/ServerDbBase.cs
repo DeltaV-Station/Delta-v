b0568faa23d799fdeb921d5c985884cf4eed0e89
deltanedas <@deltanedas:kde.org>
2024-11-05 08:42:19 +0000

    port borg preferred name from goob

--- Content.Server/Database/ServerDbBase.cs
+++ Content.Server/Database/ServerDbBase.cs
@@ -243,6 +243,8 @@ namespace Content.Server.Database
                 profile.CharacterName,
                 profile.FlavorText,
                 profile.Species,
+                // #Goobstation - Borg Preferred Name
+                profile.BorgName,
                 profile.Age,
                 sex,
                 gender,
@@ -278,6 +280,8 @@ namespace Content.Server.Database
 
             profile.CharacterName = humanoid.Name;
             profile.FlavorText = humanoid.FlavorText;
+            // #Goobstation - Borg Preferred Name
+            profile.BorgName = humanoid.BorgName;
             profile.Species = humanoid.Species;
             profile.Age = humanoid.Age;
             profile.Sex = humanoid.Sex.ToString();
