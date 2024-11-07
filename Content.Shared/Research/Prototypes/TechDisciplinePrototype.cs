70779979220382c14aeaa31d8633fdd69d1666b6
Stop-Signs <stopsign221@gmail.com>
2024-11-07 12:39:14 -0600

    Comments it out

--- Content.Shared/Research/Prototypes/TechDisciplinePrototype.cs
+++ Content.Shared/Research/Prototypes/TechDisciplinePrototype.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.Shared.Research.Prototypes;
@@ -43,6 +43,6 @@ public sealed partial class TechDisciplinePrototype : IPrototype
     /// <summary>
     /// Purchasing this tier of technology causes a server to become "locked" to this discipline.
     /// </summary>
-    [DataField("lockoutTier")]
-    public int LockoutTier = 3;
+    #[DataField("lockoutTier")]
+    #public int LockoutTier = 3;
 }
