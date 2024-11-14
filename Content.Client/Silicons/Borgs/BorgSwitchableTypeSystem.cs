4aecc673aeca8573c198511cac990579cf56239a
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-11-14 18:08:35 +0100

    Borg type switching. (#32586)
    * Borg type switching.
    
    This allows borgs (new spawn or constructed) to select their chassis type on creation, like in SS13. This removes the need for the many different chassis types, and means round-start borgs can actually play the game immediately instead of waiting for science to unlock everything.
    
    New borgs have an additional action that allows them to select their type. This opens a nice window with basic information about the borgs and a select button. Once a type has been selected it is permanent for that borg chassis.
    
    These borg types also immediately start the borg with specific modules, so they do not need to be printed. Additional modules can still be inserted for upgrades, though this is now less critical. The built-in modules cannot be removed, but are shown in the UI.
    
    The modules that each borg type starts with:
    
    * Generic: tools
    * Engineering: advanced tools, construction, RCD, cable
    * Salvage: Grappling gun, appraisal, mining
    * Janitor: cleaning, light replacer
    * Medical: treatment
    * Service: music, service, clowning
    
    Specialized borgs have 3 additional module slots available on top of the ones listed above, generic borgs have 5.
    
    Borg types are specified in a new BorgTypePrototype. These prototypes specify all information about the borg type. It is assigned to the borg entity through a mix of client side, server, and shared code. Some of the involved components were made networked, others are just ensured they're set on both sides of the wire.
    
    The most gnarly change is the inventory template prototype, which needs to change purely to modify the borg hat offset. I managed to bodge this in with an API that *probably* won't explode for specifically for this use case, but it's still not the most clean of API designs.
    
    Parts for specific borg chassis have been removed (so much deleted YAML) and specialized borg modules that are in the base set of a type have been removed from the exosuit fab as there's no point to printing those.
    
    The ability to "downgrade" a borg so it can select a new chassis, like in SS13, is something that would be nice, but was not high enough priority for me to block the feature on. I did keep it in mind with some of the code, so it may be possible in the future.
    
    There is no fancy animation when selecting borg types like in SS13, because I didn't think it was high priority, and it would add a lot of complex code.
    
    * Fix sandbox failure due to collection expression.
    
    * Module tweak
    
    Fix salvage borg modules still having research/lathe recipes
    
    Engie borg has regular tool module, not advanced.
    
    * Fix inventory system breakage
    
    * Fix migrations
    
    Some things were missing
    
    * Guidebook rewordings & review
    
    * MinWidth on confirm selection button

---
+++ Content.Client/Silicons/Borgs/BorgSwitchableTypeSystem.cs
@@ -0,0 +1,88 @@
+﻿using Content.Shared.Movement.Components;
+using Content.Shared.Silicons.Borgs;
+using Content.Shared.Silicons.Borgs.Components;
+using Robust.Client.GameObjects;
+using Robust.Client.ResourceManagement; // DeltaV
+using static Robust.Shared.Serialization.TypeSerializers.Implementations.SpriteSpecifierSerializer; // DeltaV
+
+namespace Content.Client.Silicons.Borgs;
+
+/// <summary>
+/// Client side logic for borg type switching. Sets up primarily client-side visual information.
+/// </summary>
+/// <seealso cref="SharedBorgSwitchableTypeSystem"/>
+/// <seealso cref="BorgSwitchableTypeComponent"/>
+public sealed class BorgSwitchableTypeSystem : SharedBorgSwitchableTypeSystem
+{
+    [Dependency] private readonly BorgSystem _borgSystem = default!;
+    [Dependency] private readonly AppearanceSystem _appearance = default!;
+    [Dependency] private readonly IResourceCache _resourceCache = default!; // DeltaV
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BorgSwitchableTypeComponent, AfterAutoHandleStateEvent>(AfterStateHandler);
+        SubscribeLocalEvent<BorgSwitchableTypeComponent, ComponentStartup>(OnComponentStartup);
+    }
+
+    private void OnComponentStartup(Entity<BorgSwitchableTypeComponent> ent, ref ComponentStartup args)
+    {
+        UpdateEntityAppearance(ent);
+    }
+
+    private void AfterStateHandler(Entity<BorgSwitchableTypeComponent> ent, ref AfterAutoHandleStateEvent args)
+    {
+        UpdateEntityAppearance(ent);
+    }
+
+    protected override void UpdateEntityAppearance(
+        Entity<BorgSwitchableTypeComponent> entity,
+        BorgTypePrototype prototype)
+    {
+        if (TryComp(entity, out SpriteComponent? sprite))
+        {
+            // DeltaV: Begin sprite path stuff
+            if (prototype.SpritePath is {} path && _resourceCache.TryGetResource(TextureRoot / path, out RSIResource? resource))
+                sprite.BaseRSI = resource.RSI;
+            // DeltaV: End sprite path stuff
+            sprite.LayerSetState(BorgVisualLayers.Body, prototype.SpriteBodyState);
+            sprite.LayerSetState(BorgVisualLayers.LightStatus, prototype.SpriteToggleLightState);
+        }
+
+        if (TryComp(entity, out BorgChassisComponent? chassis))
+        {
+            _borgSystem.SetMindStates(
+                (entity.Owner, chassis),
+                prototype.SpriteHasMindState,
+                prototype.SpriteNoMindState);
+
+            if (TryComp(entity, out AppearanceComponent? appearance))
+            {
+                // Queue update so state changes apply.
+                _appearance.QueueUpdate(entity, appearance);
+            }
+        }
+
+        if (prototype.SpriteBodyMovementState is { } movementState)
+        {
+            var spriteMovement = EnsureComp<SpriteMovementComponent>(entity);
+            spriteMovement.NoMovementLayers.Clear();
+            spriteMovement.NoMovementLayers["movement"] = new PrototypeLayerData
+            {
+                State = prototype.SpriteBodyState,
+            };
+            spriteMovement.MovementLayers.Clear();
+            spriteMovement.MovementLayers["movement"] = new PrototypeLayerData
+            {
+                State = movementState,
+            };
+        }
+        else
+        {
+            RemComp<SpriteMovementComponent>(entity);
+        }
+
+        base.UpdateEntityAppearance(entity, prototype);
+    }
+}
