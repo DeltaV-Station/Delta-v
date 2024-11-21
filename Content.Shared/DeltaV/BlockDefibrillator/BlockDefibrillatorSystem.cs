4ccbf82abd7f47c3302a7ba5c04d868619ae6400
Beck <beck314159@hotmail.com>
2024-11-20 21:04:50 -0800

    First commit

---
+++ Content.Shared/DeltaV/BlockDefibrillator/BlockDefibrillatorSystem.cs
@@ -0,0 +1,21 @@
+﻿using Content.Shared.Popups;
+
+namespace Content.Shared.DeltaV.BlockDefibrillator;
+
+public sealed class BlockDefibrillatorSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BlockDefibrillatorComponent, TargetBeforeDefibrillatorZapsEvent>(OnBeforeDefibrillatorZapsEvent);
+    }
+
+    private void OnBeforeDefibrillatorZapsEvent(Entity<BlockDefibrillatorComponent> entity, TargetBeforeDefibrillatorZapsEvent args)
+    {
+        args.Cancel();
+        _popupSystem.PopupClient(entity.Comp.OnBlockPopupMessage, args.User);
+    }
+}
