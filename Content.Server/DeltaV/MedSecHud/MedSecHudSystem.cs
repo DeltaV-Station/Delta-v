b056ef90ad9f8bf6da4bfc20ddbb20d8a983a16f
Milon <plmilonpl@gmail.com>
2024-11-23 10:22:06 +0100

    what the fuck was milon doing

--- Content.Server/DeltaV/MedSecHud/MedSecHudSystem.cs
+++ Content.Server/DeltaV/MedSecHud/MedSecHudSystem.cs
@@ -1,62 +1,42 @@
-using Content.Shared.Actions;
 using Content.Shared.DeltaV.MedSecHud;
 using Content.Shared.Clothing;
-using Content.Shared.Overlays;
-using Robust.Shared.GameStates;
 
 namespace Content.Server.DeltaV.MedSecHud
 {
     public sealed class MedSecHudSystem : EntitySystem
     {
-        [Dependency] private readonly SharedActionsSystem _actions = default!;
-        [Dependency] private readonly IEntityManager _entities = default!;
-
         public override void Initialize()
         {
             base.Initialize();
 
             SubscribeLocalEvent<MedSecHudComponent, ClothingGotEquippedEvent>(OnEquip);
-            SubscribeLocalEvent<MedSecHudComponent, ClothingGotUnequippedEvent>(OnUnequip);
             SubscribeLocalEvent<MedSecHudComponent, ToggleMedSecHudEvent>(OnToggle);
         }
 
-        private void OnEquip(EntityUid uid, MedSecHudComponent component, ClothingGotEquippedEvent args)
+        private void OnEquip(Entity<MedSecHudComponent> ent, ref ClothingGotEquippedEvent args)
         {
-            _actions.AddAction(args.Wearer, ref component.ActionEntity, component.ActionId, uid);
-            UpdateVisuals(uid, component);
+            UpdateVisuals(ent);
         }
 
-        private void OnUnequip(EntityUid uid, MedSecHudComponent component, ClothingGotUnequippedEvent args)
+        private void OnToggle(Entity<MedSecHudComponent> ent, ref ToggleMedSecHudEvent args)
         {
-            _actions.RemoveAction(args.Wearer, component.ActionEntity);
+            ent.Comp.MedicalMode = !ent.Comp.MedicalMode;
+            UpdateVisuals(ent);
         }
 
-        private void OnToggle(EntityUid uid, MedSecHudComponent component, ToggleMedSecHudEvent args)
+        private void UpdateVisuals(Entity<MedSecHudComponent> ent)
         {
-
-            component.MedicalMode = !component.MedicalMode;
-            UpdateVisuals(uid, component);
-        }
-
-        private void UpdateVisuals(EntityUid uid, MedSecHudComponent component)
-        {
-            if (component.MedicalMode)
+            if (ent.Comp.MedicalMode)
             {
-                EnsureComp<ShowHealthBarsComponent>(uid);
-                EnsureComp<ShowHealthIconsComponent>(uid);
-                RemComp<ShowJobIconsComponent>(uid);
-                RemComp<ShowMindShieldIconsComponent>(uid);
-                RemComp<ShowCriminalRecordIconsComponent>(uid);
+                EntityManager.AddComponents(ent, ent.Comp.AddComponents);
+                EntityManager.RemoveComponents(ent, ent.Comp.RemoveComponents);
             }
             else
             {
-                EnsureComp<ShowJobIconsComponent>(uid);
-                EnsureComp<ShowMindShieldIconsComponent>(uid);
-                EnsureComp<ShowCriminalRecordIconsComponent>(uid);
-                RemComp<ShowHealthIconsComponent>(uid);
-                RemComp<ShowHealthBarsComponent>(uid);
+                EntityManager.RemoveComponents(ent, ent.Comp.AddComponents); // just to confuse everyone
+                EntityManager.AddComponents(ent, ent.Comp.RemoveComponents);
             }
-            Dirty(uid, component);
+            Dirty(ent);
         }
     }
 }
--- Content.Shared/DeltaV/MedSecHud/MedSecHudComponent.cs
+++ Content.Shared/DeltaV/MedSecHud/MedSecHudComponent.cs
@@ -1,22 +1,30 @@
 using Content.Shared.Actions;
-using Robust.Shared.Prototypes;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.DeltaV.MedSecHud
+namespace Content.Shared.DeltaV.MedSecHud;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MedSecHudComponent : Component, ISerializationHooks
 {
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class MedSecHudComponent : Component, ISerializationHooks
-    {
-        [DataField]
-        public bool MedicalMode = true;
+    [DataField]
+    public bool MedicalMode = true;
 
-        [DataField]
-        public string ActionId = "ActionToggleMedSecHud";
+    [DataField]
+    public EntProtoId<InstantActionComponent> ActionId = "ActionToggleMedSecHud";
 
-        [DataField]
-        public EntityUid? ActionEntity;
-    }
+    /// <summary>
+    ///     Components to add if MedicalMode is set to true.
+    /// </summary>
+    [DataField]
+    public ComponentRegistry AddComponents = new();
+
+    /// <summary>
+    ///     Components to remove if MedicalMode is set to true.
+    /// </summary>
+    [DataField]
+    public ComponentRegistry RemoveComponents = new();
 }
 
-public sealed partial class ToggleMedSecHudEvent : InstantActionEvent { }
+public sealed partial class ToggleMedSecHudEvent : InstantActionEvent;
