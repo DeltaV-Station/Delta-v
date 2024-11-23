edc2cbafe10dea2c6f08ffd1d4e2aa6bd3969e82
Milon <plmilonpl@gmail.com>
2024-11-23 10:27:19 +0100

    time to wait 3 more months

--- Content.Server/DeltaV/MedSecHud/MedSecHudSystem.cs
+++ Content.Server/DeltaV/MedSecHud/MedSecHudSystem.cs
@@ -1,42 +1,43 @@
 using Content.Shared.DeltaV.MedSecHud;
 using Content.Shared.Clothing;
 
-namespace Content.Server.DeltaV.MedSecHud
+namespace Content.Server.DeltaV.MedSecHud;
+
+public sealed class MedSecHudSystem : EntitySystem
 {
-    public sealed class MedSecHudSystem : EntitySystem
+    public override void Initialize()
     {
-        public override void Initialize()
-        {
-            base.Initialize();
+        base.Initialize();
 
-            SubscribeLocalEvent<MedSecHudComponent, ClothingGotEquippedEvent>(OnEquip);
-            SubscribeLocalEvent<MedSecHudComponent, ToggleMedSecHudEvent>(OnToggle);
+        SubscribeLocalEvent<MedSecHudComponent, ClothingGotEquippedEvent>(OnEquip);
+        SubscribeLocalEvent<MedSecHudComponent, ToggleMedSecHudEvent>(OnToggle);
+    }
+
+    private void OnEquip(Entity<MedSecHudComponent> ent, ref ClothingGotEquippedEvent args)
+    {
+        UpdateVisuals(ent);
+    }
+
+    private void OnToggle(Entity<MedSecHudComponent> ent, ref ToggleMedSecHudEvent args)
+    {
+        ent.Comp.MedicalMode = !ent.Comp.MedicalMode;
+        UpdateVisuals(ent);
+    }
+
+    private void UpdateVisuals(Entity<MedSecHudComponent> ent)
+    {
+        if (ent.Comp.MedicalMode)
+        {
+            EntityManager.AddComponents(ent, ent.Comp.AddComponents);
+            EntityManager.RemoveComponents(ent, ent.Comp.RemoveComponents);
+        }
+        else
+        {
+            EntityManager.RemoveComponents(ent, ent.Comp.AddComponents); // just to confuse everyone
+            EntityManager.AddComponents(ent, ent.Comp.RemoveComponents);
         }
 
-        private void OnEquip(Entity<MedSecHudComponent> ent, ref ClothingGotEquippedEvent args)
-        {
-            UpdateVisuals(ent);
-        }
-
-        private void OnToggle(Entity<MedSecHudComponent> ent, ref ToggleMedSecHudEvent args)
-        {
-            ent.Comp.MedicalMode = !ent.Comp.MedicalMode;
-            UpdateVisuals(ent);
-        }
-
-        private void UpdateVisuals(Entity<MedSecHudComponent> ent)
-        {
-            if (ent.Comp.MedicalMode)
-            {
-                EntityManager.AddComponents(ent, ent.Comp.AddComponents);
-                EntityManager.RemoveComponents(ent, ent.Comp.RemoveComponents);
-            }
-            else
-            {
-                EntityManager.RemoveComponents(ent, ent.Comp.AddComponents); // just to confuse everyone
-                EntityManager.AddComponents(ent, ent.Comp.RemoveComponents);
-            }
-            Dirty(ent);
-        }
+        Dirty(ent);
     }
 }
+
