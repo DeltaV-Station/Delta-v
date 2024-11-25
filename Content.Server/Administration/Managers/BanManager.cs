5b3ceb6ea50d9b44d5493ea69a477d8c1b6751a8
OCO_Omega <42233446+OCOtheOmega@users.noreply.github.com>
2024-11-25 19:47:34 +0300

    Revert "Merge branch 'pr/144'"
    This reverts commit e6b42847f3228ee674430ef8a3cf0adf06f6f080, reversing
    changes made to c29b1b9d61998e9786b95648ffbc9ccc8ca57a84.

--- Content.Server/Administration/Managers/BanManager.cs
+++ Content.Server/Administration/Managers/BanManager.cs
@@ -88,8 +88,7 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
 
         var netChannel = player.Channel;
         ImmutableArray<byte>? hwId = netChannel.UserData.HWId.Length == 0 ? null : netChannel.UserData.HWId;
-        var modernHwids = netChannel.UserData.ModernHWIds;
-        var roleBans = await _db.GetServerRoleBansAsync(netChannel.RemoteEndPoint.Address, player.UserId, hwId, modernHwids, false);
+        var roleBans = await _db.GetServerRoleBansAsync(netChannel.RemoteEndPoint.Address, player.UserId, hwId, false);
 
         var userRoleBans = new List<ServerRoleBanDef>();
         foreach (var ban in roleBans)
@@ -156,7 +155,7 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
     }
 
     #region Server Bans
-    public async void CreateServerBan(NetUserId? target, string? targetUsername, NetUserId? banningAdmin, (IPAddress, int)? addressRange, ImmutableTypedHwid? hwid, uint? minutes, NoteSeverity severity, string reason, ServerBanExemptFlags exemptFlags = ServerBanExemptFlags.None)
+        public async void CreateServerBan(NetUserId? target, string? targetUsername, NetUserId? banningAdmin, (IPAddress, int)? addressRange, ImmutableArray<byte>? hwid, uint? minutes, NoteSeverity severity, string reason, ServerBanExemptFlags exemptFlags = ServerBanExemptFlags.None)
     {
         DateTimeOffset? expires = null;
         if (minutes > 0)
@@ -191,7 +190,9 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
         var addressRangeString = addressRange != null
             ? $"{addressRange.Value.Item1}/{addressRange.Value.Item2}"
             : "null";
-        var hwidString = hwid?.ToString() ?? "null";
+        var hwidString = hwid != null
+            ? string.Concat(hwid.Value.Select(x => x.ToString("x2")))
+            : "null";
         var expiresString = expires == null ? Loc.GetString("server-ban-string-never") : $"{expires}";
         var TimeString = minutes == null ? Loc.GetString("server-ban-string-infinity") : $"{TimeSpan.FromMinutes(minutes.Value)}";
 
@@ -235,7 +236,6 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
             UserId = player.UserId,
             Address = player.Channel.RemoteEndPoint.Address,
             HWId = player.Channel.UserData.HWId,
-            ModernHWIds = player.Channel.UserData.ModernHWIds,
             // It's possible for the player to not have cached data loading yet due to coincidental timing.
             // If this is the case, we assume they have all flags to avoid false-positives.
             ExemptFlags = _cachedBanExemptions.GetValueOrDefault(player, ServerBanExemptFlags.All),
@@ -256,7 +256,7 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
     #region Job Bans
     // If you are trying to remove timeOfBan, please don't. It's there because the note system groups role bans by time, reason and banning admin.
     // Removing it will clutter the note list. Please also make sure that department bans are applied to roles with the same DateTimeOffset.
-    public async void CreateRoleBan(NetUserId? target, string? targetUsername, NetUserId? banningAdmin, (IPAddress, int)? addressRange, ImmutableTypedHwid? hwid, string role, uint? minutes, NoteSeverity severity, string reason, DateTimeOffset timeOfBan)
+    public async void CreateRoleBan(NetUserId? target, string? targetUsername, NetUserId? banningAdmin, (IPAddress, int)? addressRange, ImmutableArray<byte>? hwid, string role, uint? minutes, NoteSeverity severity, string reason, DateTimeOffset timeOfBan)
     {
         if (!_prototypeManager.TryIndex(role, out JobPrototype? _))
         {
