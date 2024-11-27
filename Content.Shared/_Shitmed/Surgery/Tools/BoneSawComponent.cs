dc703f6adb8cc033e88c41ab5fcafd43ad1bd0df
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-11-27 20:46:37 +0000

    make any sharp item a ghetto surgery tool (#990)
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Shared/_Shitmed/Surgery/Tools/BoneSawComponent.cs
+++ Content.Shared/_Shitmed/Surgery/Tools/BoneSawComponent.cs
@@ -2,11 +2,11 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared._Shitmed.Medical.Surgery.Tools;
 
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class BoneSawComponent : Component, ISurgeryToolComponent
 {
     public string ToolName => "a bone saw";
     public bool? Used { get; set; } = null;
-    [DataField]
+    [DataField, AutoNetworkedField]
     public float Speed { get; set; } = 1f;
 }
--- Content.Shared/_Shitmed/Surgery/Tools/ScalpelComponent.cs
+++ Content.Shared/_Shitmed/Surgery/Tools/ScalpelComponent.cs
@@ -2,11 +2,11 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared._Shitmed.Medical.Surgery.Tools;
 
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class ScalpelComponent : Component, ISurgeryToolComponent
 {
     public string ToolName => "a scalpel";
     public bool? Used { get; set; } = null;
-    [DataField]
+    [DataField, AutoNetworkedField]
     public float Speed { get; set; } = 1f;
 }
