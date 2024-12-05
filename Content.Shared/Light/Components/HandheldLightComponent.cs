b14a177487333a837acd302960c76da566c312e1
AserCreator <aser.ionov15@gmail.com>
2024-12-05 13:10:18 +0300

    new work

--- Content.Shared/Light/Components/HandheldLightComponent.cs
+++ Content.Shared/Light/Components/HandheldLightComponent.cs
@@ -14,7 +14,7 @@ public sealed partial class HandheldLightComponent : Component
 
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("wattage")]
-    public float Wattage { get; set; } = .8f;
+    public float Wattage { get; set; } = .2f; // NES14-Changes, жрите меньше энергии
 
     [DataField("turnOnSound")]
     public SoundSpecifier TurnOnSound = new SoundPathSpecifier("/Audio/Items/flashlight_on.ogg");
