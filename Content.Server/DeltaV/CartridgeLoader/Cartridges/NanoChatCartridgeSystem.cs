a307aa9012eeaf4431c190a3954f4ba93cd20e37
Milon <plmilonpl@gmail.com>
2024-12-07 03:21:16 +0100

    FUCK

--- Content.Server/DeltaV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
+++ Content.Server/DeltaV/CartridgeLoader/Cartridges/NanoChatCartridgeSystem.cs
@@ -23,8 +23,8 @@ public sealed class NanoChatCartridgeSystem : EntitySystem
     [Dependency] private readonly IAdminLogManager _adminLogger = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IPrototypeManager _prototype = default!;
-    [Dependency] private readonly StationSystem _station = default!;
     [Dependency] private readonly SharedNanoChatSystem _nanoChat = default!;
+    [Dependency] private readonly StationSystem _station = default!;
 
     // Messages in notifications get cut off after this point
     // no point in storing it on the comp
