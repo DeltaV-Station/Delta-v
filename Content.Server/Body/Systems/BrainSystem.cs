c9825240bb081c2fe8d4d013919d6b5b5120b6de
Emily9031 <182209267+Emily9031@users.noreply.github.com>
2024-12-08 01:29:41 -0700

    Comments

--- Content.Server/Body/Systems/BrainSystem.cs
+++ Content.Server/Body/Systems/BrainSystem.cs
@@ -1,13 +1,15 @@
 using Content.Server.Body.Components;
 using Content.Server.Ghost.Components;
-using Content.Server.Traits.Assorted;
 using Content.Shared.Body.Components;
 using Content.Shared.Body.Events;
-using Content.Shared.Examine;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
 using Content.Shared.Pointing;
+// DeltaV Start
+using Content.Shared.Examine;
+using Content.Server.Traits.Assorted;
 using Robust.Shared.Utility;
+// DeltaV End
 
 namespace Content.Server.Body.Systems
 {
@@ -22,7 +24,7 @@ namespace Content.Server.Body.Systems
             SubscribeLocalEvent<BrainComponent, OrganAddedToBodyEvent>((uid, _, args) => HandleMind(args.Body, uid));
             SubscribeLocalEvent<BrainComponent, OrganRemovedFromBodyEvent>((uid, _, args) => HandleMind(uid, args.OldBody));
             SubscribeLocalEvent<BrainComponent, PointAttemptEvent>(OnPointAttempt);
-            SubscribeLocalEvent<UnborgableComponent, ExaminedEvent>(OnExamined);
+            SubscribeLocalEvent<UnborgableComponent, ExaminedEvent>(OnExamined); // DeltaV
         }
 
         private void HandleMind(EntityUid newEntity, EntityUid oldEntity)
@@ -37,7 +39,7 @@ namespace Content.Server.Body.Systems
             if (HasComp<BodyComponent>(newEntity))
                 ghostOnMove.MustBeDead = true;
 
-            if (HasComp<UnborgableComponent>(oldEntity))
+            if (HasComp<UnborgableComponent>(oldEntity)) // DeltaV
                 EnsureComp<UnborgableComponent>(newEntity);
 
             if (!_mindSystem.TryGetMind(oldEntity, out var mindId, out var mind))
@@ -46,7 +48,7 @@ namespace Content.Server.Body.Systems
             _mindSystem.TransferTo(mindId, newEntity, mind: mind);
         }
 
-        private void OnExamined(Entity<UnborgableComponent> ent, ref ExaminedEvent args)
+        private void OnExamined(Entity<UnborgableComponent> ent, ref ExaminedEvent args) //DeltaV
         {
             var msg = new FormattedMessage();
             msg.AddMarkupPermissive("[color=red]This brain is damaged beyond use.[/color]");
