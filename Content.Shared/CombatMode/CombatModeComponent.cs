62f1bf866d48c0882552fcc7761747b543f3b285
AserCreator <aser.ionov15@gmail.com>
2024-12-13 20:27:42 +0300

    работа со светом

--- Content.Shared/CombatMode/CombatModeComponent.cs
+++ Content.Shared/CombatMode/CombatModeComponent.cs
@@ -29,7 +29,7 @@ namespace Content.Shared.CombatMode
         public SoundSpecifier DisarmSuccessSound = new SoundPathSpecifier("/Audio/Effects/thudswoosh.ogg");
 
         [DataField("disarmFailChance")]
-        public float BaseDisarmFailChance = 0.75f;
+        public float BaseDisarmFailChance = 0.1f; // NES14-Changes, приколы с боевкой
 
         #endregion
 
@@ -48,5 +48,7 @@ namespace Content.Shared.CombatMode
         /// </summary>
         [DataField, AutoNetworkedField]
         public bool ToggleMouseRotator = true;
+        [DataField, AutoNetworkedField]
+        public bool SmoothRotation = true; // NES14-Changes, фонарик на 360
     }
 }
