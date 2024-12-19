6ccaa5cfcda90a8bf5c6e78652cb6b17a91fb67e
widgetbeck <beckparrott@gmail.com>
2024-12-18 21:17:25 -0500

    drones are Done now
    (cherry picked from commit 77c944183d91cc0d6437654887ce2f4cf386cc5c)

--- Content.Server/_Impstation/Drone/Components/DroneComponent.cs
+++ Content.Server/_Impstation/Drone/Components/DroneComponent.cs
@@ -10,6 +10,6 @@ namespace Content.Server._Impstation.Drone.Components
 		
 		public TimeSpan NextProximityAlert = new();
 		
-		public bool IsItemBlacklisted = false;
+		public EntityUid NearestEnt = default!;
     }
 }
