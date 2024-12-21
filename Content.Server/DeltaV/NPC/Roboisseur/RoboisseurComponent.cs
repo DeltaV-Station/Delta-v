1d133f28162361975a88ed6dacc54dfdb6d4a488
Stop-Signs <stopsign221@gmail.com>
2024-12-21 12:04:19 -0600

    Fucking weed
    Signed-off-by: Stop-Signs <stopsign221@gmail.com>

--- Content.Server/DeltaV/NPC/Roboisseur/RoboisseurComponent.cs
+++ Content.Server/DeltaV/NPC/Roboisseur/RoboisseurComponent.cs
@@ -299,6 +299,8 @@ namespace Content.Server.Roboisseur.Roboisseur
             "FoodSoupMiso",
             "FoodSoupTomatoBlue",
             "FoodBoritoPie",
+            "LeavesCannabisRainbowDried",
+            "LeavesCannabisDried",
             "FoodCakeBrain",
             "FoodBurgerBrain"
         };
