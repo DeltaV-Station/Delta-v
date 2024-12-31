95c280599897f22a018c5a276c60b476b389a60f
Beck <beck314159@hotmail.com>
2024-12-31 11:41:56 -0800

    Added round number support

--- Content.Server/DeltaV/FeedbackPopup/FeedbackPopupManager.cs
+++ Content.Server/DeltaV/FeedbackPopup/FeedbackPopupManager.cs
@@ -3,6 +3,7 @@ using Robust.Shared.Configuration;
 using Robust.Shared.Network;
 using Content.Shared.DeltaV.CCVars;
 using Content.Shared.DeltaV.FeedbackOverwatch;
+using Content.Shared.GameTicking;
 
 namespace Content.Server.DeltaV.FeedbackPopup;
 
@@ -14,6 +15,9 @@ public sealed class FeedbackPopupManager
     [Dependency] private readonly IConfigurationManager _cfg = default!;
     [Dependency] private readonly DiscordWebhook _discord = default!;
     [Dependency] private readonly IServerNetManager _netManager = default!;
+    [Dependency] private readonly IEntityManager _entity = default!;
+
+    private SharedGameTicker? _ticker;
 
     private ISawmill _sawmill = default!;
 
@@ -37,15 +41,19 @@ public sealed class FeedbackPopupManager
 
     private void RecieveFeedbackResponse(FeedbackResponseMessage message)
     {
+        // Post inject doesn't work for the entity manager (Entity manager is only initialized after this system is initialized)
+        _ticker ??= _entity.System<SharedGameTicker>();
+
+        var t = _ticker.RoundId;
         if (string.IsNullOrWhiteSpace(_webhookUrl))
             return;
 
-        SendDiscordWebhookMessage(CreateMessage(message.FeedbackName, message.FeedbackMessage, message.MsgChannel.UserName));
+        SendDiscordWebhookMessage(CreateMessage(message.FeedbackName, message.FeedbackMessage, _ticker.RoundId, message.MsgChannel.UserName));
     }
 
-    private string CreateMessage(string feedbackName, string feedback, string username)
+    private string CreateMessage(string feedbackName, string feedback, int roundNumber, string username)
     {
-        var header = Loc.GetString("feedbackpopup-discord-format-header", ("playerName", username));
+        var header = Loc.GetString("feedbackpopup-discord-format-header", ("$roundNumber", roundNumber), ("playerName", username));
         var info = Loc.GetString("feedbackpopup-discord-format-info", ("feedbackName", feedbackName));
         var spacer = Loc.GetString("feedbackpopup-discord-format-spacer");
         var feedbackbody = Loc.GetString("feedbackpopup-discord-format-feedbackbody", ("feedback", feedback));
--- Resources/Locale/en-US/deltav/feedbackpopup/feedbackpopup.ftl
+++ Resources/Locale/en-US/deltav/feedbackpopup/feedbackpopup.ftl
@@ -3,7 +3,7 @@ feedbackpopup-submit-feedback-button = Submit feedback
 feedbackpopup-disclaimer = Your SS14 username and other in game information will be relayed to the discord.
 
 # Discord relay formatting
-feedbackpopup-discord-format-header = ```[Player name: {$playerName}]```
+feedbackpopup-discord-format-header = ```[Round number: {$roundNumber}, Player name: {$playerName}]```
 feedbackpopup-discord-format-info = Feedback name: **{$feedbackName}**
 feedbackpopup-discord-format-spacer = ---- Feedback start ----
 feedbackpopup-discord-format-feedbackbody = {$feedback}
