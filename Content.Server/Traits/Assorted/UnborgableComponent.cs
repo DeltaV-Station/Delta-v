a95b7526da7d94fb788afabee7ed6da64c0d11e3
deltanedas <@deltanedas:kde.org>
2025-01-01 14:36:51 +0000

    remove old component

--- Content.Server/Traits/Assorted/UnborgableComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server.Traits.Assorted;
-
-/// <summary>
-/// This is used for the unborgable trait.
-/// </summary>
-[RegisterComponent]
-public sealed partial class UnborgableComponent : Component;
