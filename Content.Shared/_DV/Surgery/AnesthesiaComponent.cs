01269be2606a7bfd60742d38930397c473cada35
Janet Blackquill <uhhadd@gmail.com>
2025-01-10 13:05:06 -0500

    Add Anesthesia to NO2, Nocturine, and ChloralHydrate

--- Content.Shared/_DV/Surgery/AnesthesiaComponent.cs
+++ Content.Shared/_DV/Surgery/AnesthesiaComponent.cs
@@ -1,12 +1,9 @@
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared._DV.Surgery;
 
 /// <summary>
 ///     Exists for use as a status effect. Allows surgical operations to not cause immense pain.
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentPause]
-public sealed partial class AnesthesiaComponent : Component
-{
-}
+[RegisterComponent, NetworkedComponent]
+public sealed partial class AnesthesiaComponent : Component;
