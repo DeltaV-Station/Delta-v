1e928b1e242aea24fd8252035b2e7a4d57299030
mqole <113324899+mqole@users.noreply.github.com>
2025-01-25 19:44:53 +1100

    Merge pull request #1359 from Darkmajia/supermatter-final-maybe
    further supermatter adjustments

--- Content.Shared/Projectiles/SharedProjectileSystem.cs
+++ Content.Shared/Projectiles/SharedProjectileSystem.cs
@@ -14,6 +14,7 @@ using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Serialization;
 using Robust.Shared.Utility;
+using Content.Shared._EinsteinEngines.Supermatter.Components;
 
 namespace Content.Shared.Projectiles;
 
