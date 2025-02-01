bbe4039ceccba1e12255960b8d13d2aa80760831
Dvir <dvirf01@gmail.com>
2025-02-01 02:33:24 +0200

    Fix Harpy (Whatstone)
    https://github.com/new-frontiers-14/frontier-station-14/pull/2785

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -170,16 +170,31 @@ public partial class ChatSystem
         if (!_wordEmoteDict.TryGetValue(actionTrimmedLower, out var emotes)) // DeltaV, renames to emotes
             return;
 
-        foreach (var emote in emotes) // DeltaV - Multiple emotes for the same trigger
+        // Frontier: return first usable emote - comment out Delta version
+        bool validEmote = false;
+        foreach (var emote in emotes)
         {
             if (!AllowedToUseEmote(uid, emote))
-                return;
+                continue;
+
+            InvokeEmoteEvent(uid, emote);
+            validEmote = true;
         }
 
-        foreach (var emote in emotes) // DeltaV - Multiple emotes for the same trigger
-        {
-            InvokeEmoteEvent(uid, emote);
-        }
+        if (!validEmote)
+            return;
+
+        // foreach (var emote in emotes) // DeltaV - Multiple emotes for the same trigger
+        // {
+        //     if (!AllowedToUseEmote(uid, emote))
+        //         return;
+        // }
+
+        // foreach (var emote in emotes) // DeltaV - Multiple emotes for the same trigger
+        // {
+        //     InvokeEmoteEvent(uid, emote);
+        // }
+        // End Frontier
 
         static string TrimPunctuation(string textInput)
         {
