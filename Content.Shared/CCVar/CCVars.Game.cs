98caa8c42bdbb5c7eb61abdc1aa06eea6b31b773
Tobias Berger <toby@tobot.dev>
2025-02-05 13:42:01 +0100

    Change default value of game.secret_weight
    Creates semi-frequent confusion in contributor-help channel on discord,
    so might as well change it

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -348,7 +348,7 @@ public sealed partial class CCVars
         ///     The prototype to use for secret weights.
         /// </summary>
         public static readonly CVarDef<string> SecretWeightPrototype =
-            CVarDef.Create("game.secret_weight_prototype", "Secret", CVar.SERVERONLY);
+            CVarDef.Create("game.secret_weight_prototype", "SecretDeltaV", CVar.SERVERONLY); // DeltaV (duh) - Change default secret weight prototype for easier testing
 
         /// <summary>
         ///     The id of the sound collection to randomly choose a sound from and play when the round ends.
