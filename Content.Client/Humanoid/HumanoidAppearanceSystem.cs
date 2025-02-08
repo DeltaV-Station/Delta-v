b5bc34cda7b2a3fc624f67bfc624c5b1e13dc0a3
hivehum <ketchupfaced@gmail.com>
2025-02-07 19:05:20 -0500

    markings bundle

--- Content.Client/Humanoid/HumanoidAppearanceSystem.cs
+++ Content.Client/Humanoid/HumanoidAppearanceSystem.cs
@@ -380,8 +380,14 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
                 _sprite.LayerMapSet((entity.Owner, sprite), layerId, layer);
                 _sprite.LayerSetSprite((entity.Owner, sprite), layerId, rsi);
             }
-
-            _sprite.LayerSetVisible((entity.Owner, sprite), layerId, visible);
+            // impstation edit begin - check if there's a shader defined in the markingPrototype's shader datafield, and if there is...
+            if (markingPrototype.Shader != null)
+            {
+                // use spriteComponent's layersetshader function to set the layer's shader to that which is specified.
+                sprite.LayerSetShader(layerId, markingPrototype.Shader);
+            }
+            // impstation edit end
+            sprite.LayerSetVisible(layerId, visible);
 
             if (!visible || setting == null) // this is kinda implied
                 continue;
