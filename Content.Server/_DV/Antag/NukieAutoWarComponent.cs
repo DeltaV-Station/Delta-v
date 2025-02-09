d8466b278ae40a9fa2a6f3a0da037deeb531ba8e
Stop-Signs <stopsign221@gmail.com>
2025-02-09 11:26:10 -0600

    Return of warops

---
+++ Content.Server/_DV/Antag/NukieAutoWarComponent.cs
@@ -0,0 +1,14 @@
+using Content.Shared._DV.Antag;
+using Robust.Shared.Analyzers;
+
+namespace Content.Server._DV.Antag;
+
+[RegisterComponent, Access(typeof(NukieOperationSystem)), AutoGenerateComponentPause]
+public sealed partial class NukieAutoWarComponent : Component
+{
+    /// <summary>
+    ///     Automatically calls war after set time.
+    /// </summary>
+    [DataField, AutoPausedField]
+    public TimeSpan AutoWarCallTime;
+}
