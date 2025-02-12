40a7150c8c0b31b6c400e124fca16992116bc549
Sol <ewokgotswag222@gmail.com>
2025-02-12 04:09:21 -0500

    Fixed enough to launch

--- Content.Shared/_DV/Abilities/Kitsune/KitsuneComponent.cs
+++ Content.Shared/_DV/Abilities/Kitsune/KitsuneComponent.cs
@@ -1,29 +1,4 @@
-using Content.Shared.Actions.Events;
-using Content.Shared.Abilities.Psionics;
+namespace Content.Shared._DV.Abilities.Kitsune;
 
-namespace Content.Shared.Abilities.Kitsune;
-
-public sealed class KitsuneSystem : EntitySystem
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-        {
-            base.Initialize();
-
-            SubscribeLocalEvent<PsionicComponent, CreateFoxfireActionEvent>(OnCreateFoxfire);
-        }
-    }
-
-    private void OnCreateFoxfire(EntityUid uid, PsionicComponent component, CreatefoxfireActionEvent args)
-    {
-        if (HasComp<PsionicInsulationComponent>(uid))
-            return;
-
-        var fireEnt = Spawn(args.FoxfirePrototype, Transform(uid).Coordinates);
-        var fireComp = EnsureComp<FoxFireComponent>(fireEnt);
-        fireComp.Owner = uid;
-
-        args.Handled = true;
-    }
-}
+[RegisterComponent]
+public sealed partial class KitsuneComponent : Component;
