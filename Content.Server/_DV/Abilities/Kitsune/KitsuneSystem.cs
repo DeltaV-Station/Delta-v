12a40fb65d832d5742e6a8502e0513594973b2c2
Sol <ewokgotswag222@gmail.com>
2025-02-12 04:09:21 -0500

    Fixed enough to launch

--- Content.Server/_DV/Abilities/Kitsune/KitsuneSystem.cs
+++ Content.Server/_DV/Abilities/Kitsune/KitsuneSystem.cs
@@ -1,9 +1,9 @@
-using System.Drawing.Printing;
+using Content.Shared._DV.Abilities.Kitsune;
+using Content.Shared._DV.Actions.Events;
+using Content.Shared.Abilities.Psionics;
 using Content.Shared.Actions;
-using Content.Shared.Actions.Events;
-using YamlDotNet.Core.Tokens;
 
-namespace Content.Server.Abilities.Kitsune;
+namespace Content.Server._DV.Abilities.Kitsune;
 
 public sealed class KitsuneSystem : EntitySystem
 {
@@ -19,25 +19,27 @@ public sealed class KitsuneSystem : EntitySystem
 
         SubscribeLocalEvent<FoxFireComponent, ComponentShutdown>(OnFoxFireShutdown);
 
-        SubscribeLocalEvent<KitsuneComponent, FoxFireDestroyedEvent>(OnFoxFireDestroyed);
+        //SubscribeLocalEvent<KitsuneComponent, FoxFireDestroyedEvent>(OnFoxFireDestroyed);
+
+        SubscribeLocalEvent<PsionicComponent, CreateFoxfireActionEvent>(OnCreateFoxfire);
     }
 
     private void OnStartup(EntityUid uid, KitsuneComponent component, ComponentStartup args)
     {
         Log.Error("Kitsune broke");
 
-        component.FoxfireAction = _actions.AddAction(uid, component.FoxfireActionId);
+        //component.FoxfireAction = _actions.AddAction(uid, component.FoxfireActionId);
     }
 
     private void OnCreateFoxfire(EntityUid uid, KitsuneComponent component, CreateFoxfireActionEvent args)
     {
         Log.Error("Fire still broke");
-        if (_actions.GetCharges(component.FoxfireAction) is not int charges || charges < 1)
-            return;
+        //if (_actions.GetCharges(component.FoxfireAction) is not int charges || charges < 1)
+        //    return;
 
-        var fireEnt = Spawn(component.FoxfirePrototype, Transform(uid).Coordinates);
-        var fireComp = EnsureComp<FoxFireComponent>(fireEnt);
-        fireComp.Owner = uid;
+        //var fireEnt = Spawn(component.FoxfirePrototype, Transform(uid).Coordinates);
+        //var fireComp = EnsureComp<FoxFireComponent>(fireEnt);
+        //fireComp.Owner = uid;
 
         args.Handled = true;
 
@@ -49,14 +51,27 @@ public sealed class KitsuneSystem : EntitySystem
         Log.Error("Fire won't break");
         if (component.Owner is null)
             return;
-        RaiseLocalEvent<FoxFireDestroyedEvent>(component.Owner.Value, new());
+        //RaiseLocalEvent<FoxFireDestroyedEvent>(component.Owner.Value, new());
+        //TODO: Add FoxFiredDestroyedEvent
     }
-
+/*
     private void OnFoxFireDestroyed(EntityUid uid, KitsuneComponent component, FoxFireDestroyedEvent args)
     {
         Log.Error("Fire didn't break");
         _actions.AddCharges(component.FoxfireAction, 1);
         _actions.SetEnabled(component.FoxfireAction, true);
 
+    }
+*/
+    private void OnCreateFoxfire(EntityUid uid, PsionicComponent component, CreateFoxfireActionEvent args)
+    {
+        if (HasComp<PsionicInsulationComponent>(uid))
+            return;
+
+        var fireEnt = Spawn(args.FoxfirePrototype, Transform(uid).Coordinates);
+        var fireComp = EnsureComp<FoxFireComponent>(fireEnt);
+        fireComp.Owner = uid;
+
+        args.Handled = true;
     }
 }
--- Content.Shared/_DV/Actions/Events/CreateFoxfireEvent.cs
+++ Content.Shared/_DV/Actions/Events/CreateFoxfireEvent.cs
@@ -1,14 +1,15 @@
-using Robust.Shared.Protoypes;
+using Content.Shared.Actions;
+using Robust.Shared.Prototypes;
 
-namespace Content.Shared.Actions.Events;
+namespace Content.Shared._DV.Actions.Events;
 
 public sealed partial class CreateFoxfireActionEvent : InstantActionEvent
 {
-    # The foxfire prototype to use
+    // The foxfire prototype to use
     [DataField]
     public EntProtoId FoxfirePrototype = "Foxfire";
 
-    [DateField]
+    [DataField]
     public EntProtoId FoxfireActionId = "FoxfireAction";
 
     public EntityUid? FoxfireAction;
