afb99ac2e478e92453f0b649400328aa535a0591
Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
2025-02-24 23:04:54 +0300

    Cleanup: Use ``MapSystem.DeleteMap`` instead of ``IMapManager.DeleteMap`` in ``CargoTest`` (#35467)
    * Cleanup
    
    * Update
    
    * Fix

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -6,10 +6,8 @@ using Content.Server.Cargo.Systems;
 using Content.Server.Nutrition.Components;
 using Content.Server.Nutrition.EntitySystems;
 using Content.Shared.Cargo.Prototypes;
-using Content.Shared.IdentityManagement;
 using Content.Shared.Prototypes;
 using Content.Shared.Stacks;
-using Content.Shared.Tag;
 using Content.Shared.Whitelist;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
@@ -69,7 +67,7 @@ public sealed class CargoTest
         var testMap = await pair.CreateTestMap();
 
         var entManager = server.ResolveDependency<IEntityManager>();
-        var mapManager = server.ResolveDependency<IMapManager>();
+        var mapSystem = server.System<SharedMapSystem>();
         var protoManager = server.ResolveDependency<IPrototypeManager>();
         var cargo = entManager.System<CargoSystem>();
 
@@ -96,7 +94,7 @@ public sealed class CargoTest
                 }
             });
 
-            mapManager.DeleteMap(mapId);
+            mapSystem.DeleteMap(mapId);
         });
 
         await pair.CleanReturnAsync();
@@ -155,6 +153,7 @@ public sealed class CargoTest
         var testMap = await pair.CreateTestMap();
 
         var entManager = server.ResolveDependency<IEntityManager>();
+        var mapSystem = server.System<SharedMapSystem>();
         var mapManager = server.ResolveDependency<IMapManager>();
         var protoManager = server.ResolveDependency<IPrototypeManager>();
         var componentFactory = server.ResolveDependency<IComponentFactory>();
@@ -211,7 +210,7 @@ public sealed class CargoTest
 
                 entManager.DeleteEntity(ent);
             }
-            mapManager.DeleteMap(mapId);
+            mapSystem.DeleteMap(mapId);
         });
 
         await pair.CleanReturnAsync();
