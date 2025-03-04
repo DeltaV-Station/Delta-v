ffeec8f481a7a311811f5cdc32e0e2d01e620969
deltanedas <@deltanedas:kde.org>
2025-03-04 12:07:39 +0000

    centcomm isnt a grid

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -39,7 +39,7 @@ namespace Content.IntegrationTests.Tests
 
         private static readonly string[] Grids =
         {
-            "/Maps/centcomm.yml",
+            //"/Maps/centcomm.yml", // DeltaV - centcomm is a map not grid
             AdminTestArenaSystem.ArenaMapPath
         };
 
