892c145320fcdd09312f3afed305ed76e744b5aa
deltanedas <@deltanedas:kde.org>
2025-03-08 19:53:14 +0000

    invert ion storm shuttle law

--- Content.Server/Silicons/Laws/IonStormSystem.cs
+++ Content.Server/Silicons/Laws/IonStormSystem.cs
@@ -231,7 +231,7 @@ public sealed class IonStormSystem : EntitySystem
         return _robustRandom.Next(0, 35) switch
         {
             0  => Loc.GetString("ion-storm-law-on-station", ("joined", joined), ("subjects", triple)),
-            1  => Loc.GetString("ion-storm-law-no-shuttle", ("joined", joined), ("subjects", triple)),
+            1  => Loc.GetString("ion-storm-law-must-evac", ("joined", joined), ("subjects", triple)), // DeltaV - replace no-shuttle with must-evac
             2  => Loc.GetString("ion-storm-law-crew-are", ("who", crewAll), ("joined", joined), ("subjects", objectsThreats)),
             3  => Loc.GetString("ion-storm-law-subjects-harmful", ("adjective", adjective), ("subjects", triple)),
             4  => Loc.GetString("ion-storm-law-must-harmful", ("must", must)),
