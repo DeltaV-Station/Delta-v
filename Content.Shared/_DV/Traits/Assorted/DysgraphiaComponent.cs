5a01251272b80723cd8a364ac3fedcad15bffa6d
Madison Rye Progress <makyo@drab-makyo.com>
2025-03-09 19:39:22 -0700

    Simplify to just add BlockWritingComponent

--- Content.Shared/_DV/Traits/Assorted/DysgraphiaComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._DV.Traits.Assorted;
-
-/// <summary>
-/// This is used for the dysgraphia trait.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class DysgraphiaComponent : Component;
