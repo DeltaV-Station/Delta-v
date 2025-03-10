81c872f43122166a676272e85f322625b340330f
Madison Rye Progress <makyo@drab-makyo.com>
2025-03-09 18:59:16 -0700

    Remove unused field in component

--- Content.Shared/_DV/Traits/Assorted/DysgraphiaComponent.cs
+++ Content.Shared/_DV/Traits/Assorted/DysgraphiaComponent.cs
@@ -6,10 +6,4 @@ namespace Content.Shared.Traits.Assorted;
 /// This is used for the dysgraphia trait.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class DysgraphiaComponent : Component
-{
-    /// <summary>
-    /// What message should be displayed when the character fails to write?
-    /// </summary>
-    public LocId FailWriteMessage = "paper-component-dysgraphia";
-}
+public sealed partial class DysgraphiaComponent : Component;
