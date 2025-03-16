9e02466867f38a143a4668c65c6fad2013561fcb
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-03-16 20:56:06 +0100

    Rework ParticleHolder to new ParticleEnergy type,

--- Content.Shared/_DV/Noospherics/ParticleTypes.cs
+++ Content.Shared/_DV/Noospherics/ParticleTypes.cs
@@ -1,4 +1,3 @@
-
 namespace Content.Shared._DV.Noospherics;
 
 public enum ParticleType
@@ -9,17 +8,69 @@ public enum ParticleType
     Omega,
 }
 
-public sealed class NoosphericParticlesHolder<T>
+public sealed class NoosphericParticleEnergy
 {
-    private readonly Dictionary<ParticleType, T> _particles = [];
+    private Dictionary<ParticleType, float> _particles = [];
 
-    public T this[ParticleType type]
+    public float this[ParticleType type]
     {
         get => _particles[type];
         set => _particles[type] = value;
     }
 
-    public NoosphericParticlesHolder(T initialValue)
+    public IEnumerator<KeyValuePair<ParticleType, float>> GetEnumerator()
+    {
+        return _particles.GetEnumerator();
+    }
+
+    public void AddToAll(float value)
+    {
+        foreach (var item in _particles)
+        {
+            _particles[item.Key] += value;
+        }
+    }
+
+    public float Average()
+    {
+        var total = 0f;
+        foreach (var item in _particles)
+        {
+            total += item.Value;
+        }
+
+        return total;
+    }
+
+    public static NoosphericParticleEnergy operator +(
+        NoosphericParticleEnergy lhs,
+        NoosphericParticleEnergy rhs)
+    {
+        var result = lhs;
+        foreach (var item in result._particles)
+        {
+            result._particles[item.Key] += rhs._particles[item.Key];
+        }
+
+        return result;
+    }
+
+    public static NoosphericParticleEnergy operator *(
+        NoosphericParticleEnergy energy,
+        float value)
+    {
+        var result = energy;
+        foreach (var item in result._particles)
+        {
+            result._particles[item.Key] *= value;
+        }
+
+        return result;
+    }
+
+    public NoosphericParticleEnergy() : this(0f) { }
+
+    public NoosphericParticleEnergy(float initialValue)
     {
         _particles = new()
         {
