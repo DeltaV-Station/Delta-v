ad54b5da49a09ba63bb27fefbfa0951efab880b3
deltanedas <@deltanedas:kde.org>
2025-03-17 23:24:27 +0000

    add missing surgery step documentation

--- Content.Shared/_Shitmed/Surgery/Conditions/SurgeryOrganSlotConditionComponent.cs
+++ Content.Shared/_Shitmed/Surgery/Conditions/SurgeryOrganSlotConditionComponent.cs
@@ -2,6 +2,9 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared._Shitmed.Medical.Surgery.Conditions;
 
+/// <summary>
+/// Requires that an organ slot does (not) exist on the target part for a surgery to be possible.
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class SurgeryOrganSlotConditionComponent : Component
 {
