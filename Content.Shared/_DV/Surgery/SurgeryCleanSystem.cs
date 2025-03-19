914a209d61d4f351c1e28f9364eba175c53447f8
deltanedas <39013340+deltanedas@users.noreply.github.com>
2025-03-19 01:31:01 +0000

    fix not being able to clean autodoc (#3272)
    * contamination dna prediction + cleanup
    
    * fix autodoc not being able to be cleaned
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Shared/_DV/Surgery/SurgeryCleanSystem.cs
+++ Content.Shared/_DV/Surgery/SurgeryCleanSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.DoAfter;
 using Content.Shared.Examine;
+using Content.Shared.Forensics;
 using Content.Shared.Interaction;
 using Content.Shared.Popups;
 using Content.Shared.Verbs;
@@ -8,7 +9,7 @@ using Content.Shared.FixedPoint;
 
 namespace Content.Shared._DV.Surgery;
 
-public abstract class SharedSurgeryCleanSystem : EntitySystem
+public sealed class SurgeryCleanSystem : EntitySystem
 {
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
@@ -24,6 +25,7 @@ public abstract class SharedSurgeryCleanSystem : EntitySystem
         SubscribeLocalEvent<SurgeryCleansDirtComponent, GetVerbsEvent<UtilityVerb>>(OnUtilityVerb);
 
         SubscribeLocalEvent<SurgeryDirtinessComponent, SurgeryCleanedEvent>(OnCleanDirt);
+        SubscribeLocalEvent<SurgeryCrossContaminationComponent, SurgeryCleanedEvent>(OnCleanDNA);
     }
 
     private void OnUtilityVerb(Entity<SurgeryCleansDirtComponent> ent, ref GetVerbsEvent<UtilityVerb> args)
@@ -51,6 +53,7 @@ public abstract class SharedSurgeryCleanSystem : EntitySystem
     {
         var stage = (int)Math.Ceiling(ent.Comp.Dirtiness.Double() / 20.0);
 
+        // no separate examine for DNAs as they are both cleaned at the same time.
         var description = stage switch {
             0 => "surgery-cleanliness-0",
             1 => "surgery-cleanliness-1",
@@ -63,7 +66,12 @@ public abstract class SharedSurgeryCleanSystem : EntitySystem
         args.PushMarkup(Loc.GetString(description));
     }
 
-    public abstract bool RequiresCleaning(EntityUid target);
+    public bool RequiresCleaning(EntityUid target)
+    {
+        var isDirty = (TryComp<SurgeryDirtinessComponent>(target, out var dirtiness) && dirtiness.Dirtiness > 0);
+        var isContaminated = (TryComp<SurgeryCrossContaminationComponent>(target, out var contamination) && contamination.DNAs.Count > 0);
+        return isDirty || isContaminated;
+    }
 
     private void OnAfterInteract(Entity<SurgeryCleansDirtComponent> ent, ref AfterInteractEvent args)
     {
@@ -97,14 +105,18 @@ public abstract class SharedSurgeryCleanSystem : EntitySystem
         return true;
     }
 
-    protected virtual void FinishCleaning(Entity<SurgeryCleansDirtComponent> ent, ref SurgeryCleanDirtDoAfterEvent args)
+    private void FinishCleaning(Entity<SurgeryCleansDirtComponent> ent, ref SurgeryCleanDirtDoAfterEvent args)
     {
-        if (args.Handled || args.Cancelled || args.Args.Target == null)
+        if (args.Handled || args.Cancelled || args.Args.Target is not {} target)
             return;
 
-        DoClean(ent, args.Args.Target.Value);
+        DoClean(ent, target);
 
-        args.Repeat = RequiresCleaning(args.Args.Target.Value);
+        args.Repeat = RequiresCleaning(target);
+
+        // daisychain to forensics because if you sterilise something youve almost definitely scrubbed all dna and fibers off of it
+        var daisyChainEvent = new CleanForensicsDoAfterEvent() { DoAfter = args.DoAfter };
+        RaiseLocalEvent(ent, daisyChainEvent);
     }
 
     public void DoClean(Entity<SurgeryCleansDirtComponent> cleaner, EntityUid target)
@@ -118,4 +130,55 @@ public abstract class SharedSurgeryCleanSystem : EntitySystem
         ent.Comp.Dirtiness = FixedPoint2.Max(FixedPoint2.Zero, ent.Comp.Dirtiness - args.DirtAmount);
         Dirty(ent);
     }
+
+    private void OnCleanDNA(Entity<SurgeryCrossContaminationComponent> ent, ref SurgeryCleanedEvent args)
+    {
+        var i = 0;
+        var count = args.DnaAmount;
+        ent.Comp.DNAs.RemoveWhere(item => i++ < count);
+        Dirty(ent);
+    }
+
+    #region Public API
+
+    /// <summary>
+    /// Get the dirtiness level for a tool/user, adding SurgeryDirtiness if it doesn't exist already.
+    /// </summary>
+    public FixedPoint2 Dirtiness(EntityUid uid)
+    {
+        return EnsureComp<SurgeryDirtinessComponent>(uid).Dirtiness;
+    }
+
+    /// <summary>
+    /// Get all DNA strings contaminating a tool/user, adding SurgeryCrossContamination if it doesn't exist already.
+    /// </summary>
+    public HashSet<string> CrossContaminants(EntityUid uid)
+    {
+        return EnsureComp<SurgeryCrossContaminationComponent>(uid).DNAs;
+    }
+
+    /// <summary>
+    /// Add dirt to a tool/user.
+    /// </summary>
+    public void AddDirt(EntityUid uid, FixedPoint2 amount)
+    {
+        var comp = EnsureComp<SurgeryDirtinessComponent>(uid);
+        comp.Dirtiness += amount;
+        Dirty(uid, comp);
+    }
+
+    /// <summary>
+    /// Contaminate a tool/user with DNA.
+    /// </summary>
+    public void AddDna(EntityUid uid, string? dna)
+    {
+        if (dna == null)
+            return;
+
+        var comp = EnsureComp<SurgeryCrossContaminationComponent>(uid);
+        comp.DNAs.Add(dna);
+        Dirty(uid, comp);
+    }
+
+    #endregion
 }
