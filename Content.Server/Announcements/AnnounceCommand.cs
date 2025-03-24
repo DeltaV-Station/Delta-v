093d57d0a1d2443d508b8d8d7d776d31cb6a1dd8
Monotheonist <182929384+Monotheonist@users.noreply.github.com>
2025-03-23 22:11:31 -0300

    Impstation fixing it ops

--- Content.Server/Announcements/AnnounceCommand.cs
+++ Content.Server/Announcements/AnnounceCommand.cs
@@ -1,11 +1,11 @@
-using System.Linq; // Impstation Random Announcer System
+using System.Linq
 using Content.Server.Administration;
-using Content.Server._EE.Announcements.Systems; // Impstation Random Announcer System
+using Content.Server.Chat.Systems; 
 using Content.Shared.Administration;
-using Content.Shared._EE.Announcements.Prototypes; // Impstation Random Announcer System
+using Robust.Shared.Audio;
 using Robust.Shared.Console;
-using Robust.Shared.Player; // Impstation Random Announcer System
-using Robust.Shared.Prototypes; // Impstation Random Announcer System
+using Robust.Shared.ContentPack;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.Announcements;
 
@@ -22,79 +22,36 @@ public sealed class AnnounceCommand : LocalizedEntityCommands
 
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-
-        public string Command => "announce";
-        public string Description => "Send an in-game announcement.";
-        public string Help => $"{Command} <sender> <message> <sound> <announcer>"; // Impstation Random Announcer System: Adds announcer
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        switch (args.Length)
         {
-            var announcer = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<AnnouncerSystem>(); // Start Impstation Random Announcer System
-            var proto = IoCManager.Resolve<IPrototypeManager>();
-
-            switch (args.Length)
-            {
-                case 0:
-                    shell.WriteError("Not enough arguments! Need at least 1.");
-                    return;
-                case 1:
-                    announcer.SendAnnouncement(announcer.GetAnnouncementId("CommandReport"), Filter.Broadcast(),
-                        args[0], "Central Command", Color.Gold);
-                    break;
-                case 2:
-                    announcer.SendAnnouncement(announcer.GetAnnouncementId("CommandReport"), Filter.Broadcast(),
-                        args[1], args[0], Color.Gold);
-                    break;
-                case 3:
-                    announcer.SendAnnouncement(announcer.GetAnnouncementId(args[2]), Filter.Broadcast(), args[1],
-                        args[0], Color.Gold);
-                    break;
-                case 4:
-                    if (!proto.TryIndex(args[3], out AnnouncerPrototype? prototype))
-                    {
-                        shell.WriteError($"No announcer prototype with ID {args[3]} found!");
-                        return;
-                    }
-                    announcer.SendAnnouncement(args[2], Filter.Broadcast(), args[1], args[0], Color.Gold, null,
-                        prototype);
-                    break;
-            }
-
-            shell.WriteLine("Sent!");
+            case 0:
+                shell.WriteError(Loc.GetString("shell-need-minimum-one-argument"));
+                return;
+            case > 4:
+                shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
+                return;
         }
-        
-        public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+
+        var message = args[0];
+        var sender = Loc.GetString("cmd-announce-sender");
+        var color = Color.Gold;
+        var sound = new SoundPathSpecifier("/Audio/Announcements/announce.ogg");
+
+        // Optional sender argument
+        if (args.Length >= 2)
+            sender = args[1];
+
+        // Optional color argument
+        if (args.Length >= 3)
         {
-            switch (args.Length)
+            try
             {
-                case 3:
-                {
-                    var list = new List<string>();
-
-                    foreach (var prototype in IoCManager.Resolve<IPrototypeManager>()
-                                .EnumeratePrototypes<AnnouncerPrototype>()
-                                .SelectMany<AnnouncerPrototype, string>(p => p.Announcements.Select(a => a.ID)))
-                    {
-                        if (!list.Contains(prototype))
-                            list.Add(prototype);
-                    }
-
-                    return CompletionResult.FromHintOptions(list, Loc.GetString("admin-announce-hint-sound"));
-                }
-                case 4:
-                {
-                    var list = new List<string>();
-
-                    foreach (var prototype in IoCManager.Resolve<IPrototypeManager>()
-                        .EnumeratePrototypes<AnnouncerPrototype>())
-                    {
-                        if (!list.Contains(prototype.ID))
-                            list.Add(prototype.ID);
-                    }
-
-                    return CompletionResult.FromHintOptions(list, Loc.GetString("admin-announce-hint-voice"));
-                }
-                default:
-                    return CompletionResult.Empty; // End Impstation Random Announcer System
+                color = Color.FromHex(args[2]);
+            }
+            catch
+            {
+                shell.WriteError(Loc.GetString("shell-invalid-color-hex"));
+                return;
             }
         }
 
