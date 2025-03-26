a46a3c6d4c1e3ddde7239b557054c9ac8397cef9
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-03-26 20:52:32 +0100

    Add super naive gunshot residue on weapons

---
+++ Content.Server/_DV/Forensics/EntitySystems/ForensicsSystem.Guns.cs
@@ -0,0 +1,42 @@
+using Content.Shared._DV.Forensics.Components;
+using Content.Shared.Weapons.Ranged.Components;
+using Content.Shared.Weapons.Ranged.Events;
+
+namespace Content.Server.Forensics;
+
+public sealed partial class ForensicsSystem
+{
+    private void InitializeGunForensics()
+    {
+        SubscribeLocalEvent<GunComponent, AmmoShotEvent>(OnAmmoShot);
+    }
+
+    private string DescribeResidue(ResidueComponent comp)
+    {
+        if (string.IsNullOrEmpty(comp.ResidueColor))
+        {
+            return Loc.GetString("forensic-residue", ("adjective", comp.ResidueAdjective));
+        }
+        else
+        {
+            return Loc.GetString("forensic-residue-colored", ("color", comp.ResidueColor), ("adjective", comp.ResidueAdjective));
+        }
+    }
+
+    private void OnAmmoShot(Entity<GunComponent> ent, ref AmmoShotEvent args)
+    {
+        // Apply Firearm residue to the weapon AND the entity firing the weapon,
+        // if one exists.
+        if (TryComp<ResidueComponent>(ent.Owner, out var residue))
+        {
+            var weaponForensics = EnsureComp<ForensicsComponent>(ent.Owner);
+            var description = DescribeResidue(residue);
+            weaponForensics.Residues.Add(description);
+            if (ent.Comp.Holder.HasValue)
+            {
+                var userForensics = EnsureComp<ForensicsComponent>(ent.Comp.Holder.Value);
+                userForensics.Residues.Add(description);
+            }
+        }
+    }
+}
---
+++ Resources/Locale/en-US/_DV/forensics/residues.ftl
@@ -0,0 +1,1 @@
+residue-gunshot = gunshot
