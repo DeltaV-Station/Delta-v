1f6d1b23772992d51908f761ca4e31ae22abd5d7
Janet Blackquill <uhhadd@gmail.com>
2025-04-03 12:25:53 -0400

    bugfixes, some timering

--- Content.Shared/_DV/CosmicCult/Components/CosmicImposingComponent.cs
+++ Content.Shared/_DV/CosmicCult/Components/CosmicImposingComponent.cs
@@ -1,4 +1,5 @@
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization;
 using Robust.Shared.Utility;
 
@@ -8,8 +9,12 @@ namespace Content.Shared._DV.CosmicCult.Components;
 /// Component for displaying Vacuous Imposition's visuals on a player.
 /// </summary>
 [NetworkedComponent, RegisterComponent]
+[AutoGenerateComponentPause]
 public sealed partial class CosmicImposingComponent : Component
 {
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
+    public TimeSpan Expiry;
+
     [DataField]
     public SpriteSpecifier Sprite = new SpriteSpecifier.Rsi(new("/Textures/_DV/CosmicCult/Effects/ability_imposition_overlay.rsi"), "vfx");
 }
