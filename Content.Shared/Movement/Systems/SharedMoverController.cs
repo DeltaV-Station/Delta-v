3410a10c949c9ef3f8864ffe6f161ce3da8578cb
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-04-05 00:33:52 +1100

    Mob collisions (#34580)
    * Conveyor optimisations
    
    - Optimise movement for moving stuff. Better flags + less resolves + slapped parallelrobustjob on it.
    - Sleeping for entities getting conveyed into walls.
    
    * Blocker version
    
    * Finish
    
    * Final
    
    * Mob collisions
    
    * impulses
    
    * Collision smoothing
    
    * Locked in
    
    * 30tps working
    
    * r
    
    * fixes
    
    * Best
    
    * Fixes + CVars
    
    * CVars in place
    
    * Pushies
    
    * Opt attempt 1
    
    * Revert "Opt attempt 1"
    
    This reverts commit 5ccd72dcbea09261a992aa1f7f05df169a1ce676.
    
    * Fix mispredicts
    
    * Ready-ish
    
    * better
    
    * Cleanup
    
    * Fix conveyor power mispredict
    
    * Forgetting to actually do deltas
    
    * Fix buckle pushes
    
    * Bagel save
    
    * Revert "Bagel save"
    
    This reverts commit 1b93fda81fb852d89b89b0beae0b80f8a61165f2.
    
    * Conveyor resave
    
    * Fix prediction
    
    * Mob movement rewrite
    
    * Bandaid
    
    * Working version
    
    * Tentatively working
    
    * Friction to fix cornering
    
    * More fixes
    
    * Revert bagel
    
    * Revert this
    
    * Bad parity
    
    * Working
    
    * Fixes
    
    * Woops
    
    * Doc comments
    
    * Pen cap cvar
    
    * StandingState cleanup and sub
    
    * Fix downed mobs
    
    * fish
    
    * client
    
    * Disable pushing on tests
    
    * More variables
    
    * Movement mods
    
    * Mass diff
    
    * 1 more tweak
    
    * Cvar

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -44,7 +44,6 @@ public abstract partial class SharedMoverController : VirtualController
     [Dependency] private   readonly SharedContainerSystem _container = default!;
     [Dependency] private   readonly SharedMapSystem _mapSystem = default!;
     [Dependency] private   readonly SharedGravitySystem _gravity = default!;
-    [Dependency] protected readonly SharedPhysicsSystem Physics = default!;
     [Dependency] private   readonly SharedTransformSystem _transform = default!;
     [Dependency] private   readonly TagSystem _tags = default!;
 
@@ -108,6 +107,14 @@ public abstract partial class SharedMoverController : VirtualController
     public override void UpdateAfterSolve(bool prediction, float frameTime)
     {
         base.UpdateAfterSolve(prediction, frameTime);
+
+        var query = AllEntityQuery<InputMoverComponent, PhysicsComponent>();
+
+        while (query.MoveNext(out var uid, out var _, out var physics))
+        {
+            //PhysicsSystem.SetLinearVelocity(uid, Vector2.Zero, body: physics);
+        }
+
         UsedMobMovement.Clear();
     }
 
