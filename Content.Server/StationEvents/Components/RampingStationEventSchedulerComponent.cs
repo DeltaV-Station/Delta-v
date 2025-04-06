539bdff52dbf202686cc1197b3739be1717599b7
nkokic <nikola1.kokic@gmail.com>
2025-04-06 20:54:21 +0200

    polishing code

--- Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
+++ Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
@@ -1,49 +1,51 @@
 using Content.Shared.EntityTable.EntitySelectors;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
-using System.Numerics;
+using System.Numerics; // DeltaV
 
 namespace Content.Server.StationEvents.Components;
 
 [RegisterComponent, Access(typeof(RampingStationEventSchedulerSystem))]
 public sealed partial class RampingStationEventSchedulerComponent : Component
 {
-    ///// <summary>
-    /////     Average ending chaos modifier for the ramping event scheduler. Higher means faster.
-    /////     Max chaos chosen for a round will deviate from this
-    ///// </summary>
-    //[DataField]
-    //public float AverageChaos = 12f;
-
-    ///// <summary>
-    /////     Average time (in minutes) for when the ramping event scheduler should stop increasing the chaos modifier.
-    /////     Close to how long you expect a round to last, so you'll probably have to tweak this on downstreams.
-    ///// </summary>
-    //[DataField]
-    //public float AverageEndTime = 90f;
-
-    //[DataField]
-    //public float EndTime;
-
-    //[DataField]
-    //public float MaxChaos;
-
-    //[DataField]
-    //public float StartingChaos;
-
-    // DeltaV code
+    /*
     /// <summary>
-    ///     Key points which determine after which minute will event period be called on average.
+    ///     Average ending chaos modifier for the ramping event scheduler. Higher means faster.
+    ///     Max chaos chosen for a round will deviate from this
+    /// </summary>
+    [DataField]
+    public float AverageChaos = 12f;
+
+    /// <summary>
+    ///     Average time (in minutes) for when the ramping event scheduler should stop increasing the chaos modifier.
+    ///     Close to how long you expect a round to last, so you'll probably have to tweak this on downstreams.
+    /// </summary>
+    [DataField]
+    public float AverageEndTime = 90f;
+
+    [DataField]
+    public float EndTime;
+
+    [DataField]
+    public float MaxChaos;
+
+    [DataField]
+    public float StartingChaos;
+    */
+
+    /// <summary>
+    ///     DeltaV - Key points which determine after which minute will event period be called on average.
     ///     X is round time in minutes, Y is time until next event in minutes.
     /// </summary>
-    [DataField("timeKeyPoints")]
-    public List<Vector2> TimeKeyPoints { get; private set; } = new()
+    [DataField(required: true)]
+    public List<Vector2> TimeKeyPoints = new()
     {
         new Vector2(0f, 1f)
     };
 
+    /// <summary>
+    ///     DeltaV - Maximum possible error when randomly offsetting time until next event.
+    /// </summary>
     [DataField]
-    public float TimeDeviation = 0;
-    // End of DeltaV code
+    public float TimeDeviation = 1;
 
     [DataField]
     public float TimeUntilNextEvent;
