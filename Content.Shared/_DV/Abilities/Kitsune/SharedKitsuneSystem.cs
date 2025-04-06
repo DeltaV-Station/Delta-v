63a86c44ffba19d4f3eed6f4238aca098182f4fb
AeraAulin <133451603+AeraAuling@users.noreply.github.com>
2025-04-06 04:22:44 -0700

    same thing for this
    Co-authored-by: Tobias Berger <toby@tobot.dev>
    Signed-off-by: AeraAulin <133451603+AeraAuling@users.noreply.github.com>

--- Content.Shared/_DV/Abilities/Kitsune/SharedKitsuneSystem.cs
+++ Content.Shared/_DV/Abilities/Kitsune/SharedKitsuneSystem.cs
@@ -66,6 +66,7 @@ public abstract class SharedKitsuneSystem : EntitySystem
 
         args.Handled = true;
     }
+
     private void OnFoxFireShutdown(Entity<FoxFireComponent> ent, ref ComponentShutdown args)
     {
         if (ent.Comp.Kitsune is { } kitsune && TryComp<KitsuneComponent>(kitsune, out var kitsuneComp))
