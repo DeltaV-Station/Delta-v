a6aa05b5b6307613adc5e11bed018f0d5023b0ff
Falcon <falcon@zigtag.dev>
2025-04-08 23:04:56 -0700

    fix: required comments for code changes upstream

--- Content.Shared/Chat/TypingIndicator/TypingIndicatorComponent.cs
+++ Content.Shared/Chat/TypingIndicator/TypingIndicatorComponent.cs
@@ -7,13 +7,13 @@ namespace Content.Shared.Chat.TypingIndicator;
 ///     Show typing indicator icon when player typing text in chat box.
 ///     Added automatically when player poses entity.
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-// [Access(typeof(SharedTypingIndicatorSystem))] SynthSystem.cs isn't in the correct namespace in order to be accessed by this function
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState] // CD - AutoGenerateComponentState fixes a bug with synth trait
+// [Access(typeof(SharedTypingIndicatorSystem))] CD - Restricted access breaks synth trait because it rewrites the speech bubble over the default race indicator
 public sealed partial class TypingIndicatorComponent : Component
 {
     /// <summary>
     ///     Prototype id that store all visual info about typing indicator.
     /// </summary>
-    [DataField("proto"), AutoNetworkedField]
+    [DataField("proto"), AutoNetworkedField] // CD - AutoNetworkedField fixes a bug in synth trait
     public ProtoId<TypingIndicatorPrototype> TypingIndicatorPrototype = "default";
 }
