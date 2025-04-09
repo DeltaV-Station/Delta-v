ba590b28f2cb2f25a0005ab5aff2e903cade2b56
Falcon <falcon@zigtag.dev>
2025-04-08 22:38:47 -0700

    Synth Trait (cosmatic-drift-14/cosmatic-drift#149 and cosmatic-drift-14/cosmatic-drift#329) from cosmatic-drift-14/cosmatic-drift
    * Initial trait implementation
    
    * Fix the trait.
    
    * Readd fields.

--- Content.Server/StationEvents/Events/IonStormRule.cs
+++ Content.Server/StationEvents/Events/IonStormRule.cs
@@ -1,14 +1,21 @@
+using Content.Server._CD.Traits;
 using Content.Server.Silicons.Laws;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Silicons.Laws.Components;
 using Content.Shared.Station.Components;
+// Used in CD's System
+using Content.Server.Chat.Managers;
+using Content.Shared.Chat;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
 {
     [Dependency] private readonly IonStormSystem _ionStorm = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!; // Used for CD's System
 
     protected override void Started(EntityUid uid, IonStormRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
@@ -16,6 +23,22 @@ public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
 
         if (!TryGetRandomStation(out var chosenStation))
             return;
+
+        // CD Change - Go through everyone with the SynthComponent and inform them a storm is happening.
+        var synthQuery = EntityQueryEnumerator<SynthComponent>();
+        while (synthQuery.MoveNext(out var ent, out var synthComp))
+        {
+            if (RobustRandom.Prob(synthComp.AlertChance))
+                continue;
+
+            if (!TryComp<ActorComponent>(ent, out var actor))
+                continue;
+
+            var msg = Loc.GetString("station-event-ion-storm-synth");
+            var wrappedMessage = Loc.GetString("chat-manager-server-wrap-message", ("message", msg));
+            _chatManager.ChatMessageToOne(ChatChannel.Server, msg, wrappedMessage, default, false, actor.PlayerSession.Channel, colorOverride: Color.Yellow);
+        }
+        // End of CD change
 
         var query = EntityQueryEnumerator<SiliconLawBoundComponent, TransformComponent, IonStormTargetComponent>();
         while (query.MoveNext(out var ent, out var lawBound, out var xform, out var target))
--- Content.Shared/Chat/TypingIndicator/TypingIndicatorComponent.cs
+++ Content.Shared/Chat/TypingIndicator/TypingIndicatorComponent.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.GameStates;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Chat.TypingIndicator;
@@ -7,13 +7,13 @@ namespace Content.Shared.Chat.TypingIndicator;
 ///     Show typing indicator icon when player typing text in chat box.
 ///     Added automatically when player poses entity.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(SharedTypingIndicatorSystem))]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+// [Access(typeof(SharedTypingIndicatorSystem))] SynthSystem.cs isn't in the correct namespace in order to be accessed by this function
 public sealed partial class TypingIndicatorComponent : Component
 {
     /// <summary>
     ///     Prototype id that store all visual info about typing indicator.
     /// </summary>
-    [DataField("proto")]
+    [DataField("proto"), AutoNetworkedField]
     public ProtoId<TypingIndicatorPrototype> TypingIndicatorPrototype = "default";
 }
