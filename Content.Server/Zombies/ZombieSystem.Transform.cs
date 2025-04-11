4f5681c8ee6231c7c425ccca2923b246560f9c59
Ethan_k <eknowles27@dtechhs.org>
2025-04-10 18:12:13 -0700

    Zombies can no longer Suicide take 2 (#36453)
    * I did it right
    
    * Update Content.Server/Zombies/ZombieSystem.Transform.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Server/Zombies/ZombieSystem.Transform.cs
+++ Content.Server/Zombies/ZombieSystem.Transform.cs
@@ -65,7 +65,7 @@ public sealed partial class ZombieSystem
     [Dependency] private readonly NameModifierSystem _nameMod = default!;
 
     private static readonly ProtoId<TagPrototype> InvalidForGlobalSpawnSpellTag = "InvalidForGlobalSpawnSpell";
-
+    private static readonly ProtoId<TagPrototype> CannotSuicideTag = "CannotSuicide";
     /// <summary>
     /// Handles an entity turning into a zombie when they die or go into crit
     /// </summary>
@@ -310,5 +310,6 @@ public sealed partial class ZombieSystem
         //Need to prevent them from getting an item, they have no hands.
         // Also prevents them from becoming a Survivor. They're undead.
         _tag.AddTag(target, InvalidForGlobalSpawnSpellTag);
+        _tag.AddTag(target, CannotSuicideTag);
     }
 }
