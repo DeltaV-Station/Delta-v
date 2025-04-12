776fa6822bf6d80fa9be180c1b2af3ea99628465
deltanedas <39013340+deltanedas@users.noreply.github.com>
2025-04-12 15:52:21 +0100

    Merge pull request #3424 from deltanedas/surgery-cleanup-2-ops
    surgery dirtiness cleanup 2: judgement day

--- Content.Shared/_DV/Surgery/SurgeryContaminableComponent.cs
+++ Content.Shared/_DV/Surgery/SurgeryContaminableComponent.cs
@@ -1,11 +1,14 @@
 using Content.Shared._Shitmed.Medical.Surgery;
+using Content.Shared.Damage.Prototypes;
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._DV.Surgery;
 
 /// <summary>
-///     Component that indicates how an entity should respond to unsanitary surgery conditions
+/// Component that indicates how an entity should respond to unsanitary surgery conditions
+/// It also causes surgery tools to become dirty/cross contaminated when operated on.
 /// </summary>
 [RegisterComponent, NetworkedComponent, Access(typeof(SharedSurgerySystem))]
 public sealed partial class SurgeryContaminableComponent : Component
@@ -21,6 +24,12 @@ public sealed partial class SurgeryContaminableComponent : Component
     /// </summary>
     [DataField]
     public FixedPoint2 DirtinessThreshold = 50.0;
+
+    /// <summary>
+    /// The damage type to deal for sepsis.
+    /// </summary>
+    [DataField]
+    public ProtoId<DamageTypePrototype> SepsisDamageType = "Poison";
 
     /// <summary>
     ///     The base amount of toxin damage to deal above the threshold
