1f70d6a19e8684628d66bab7b152d01437f1a4da
AeraAulin <133451603+AeraAuling@users.noreply.github.com>
2025-04-15 07:53:43 -0700

    a
    Co-authored-by: Tobias Berger <toby@tobot.dev>
    Signed-off-by: AeraAulin <133451603+AeraAuling@users.noreply.github.com>

--- Content.Client/_DV/Kitsune/KitsuneFoxSystem.cs
+++ Content.Client/_DV/Kitsune/KitsuneFoxSystem.cs
@@ -7,7 +7,7 @@ public sealed class KitsuneFoxSystem : VisualizerSystem<KitsuneFoxComponent>
 {
     protected override void OnAppearanceChange(EntityUid uid, KitsuneFoxComponent comp, ref AppearanceChangeEvent args)
     {
-        if (args.Sprite is not {} sprite)
+        if (args.Sprite is not { } sprite)
             return;
 
         if (!AppearanceSystem.TryGetData<Color>(uid, KitsuneColorVisuals.Color, out var color, args.Component))
