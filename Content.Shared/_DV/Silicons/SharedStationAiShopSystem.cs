55d5177b44b9d52e440b3013567d288fb7c4b528
Janet Blackquill <uhhadd@gmail.com>
2025-05-01 00:45:08 -0400

    Introduce an AI shop system

---
+++ Content.Shared/_DV/Silicons/SharedStationAiShopSystem.cs
@@ -0,0 +1,64 @@
+using Content.Shared.Actions;
+using Content.Shared.Damage;
+using Content.Shared.Light.Components;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Shared._DV.Silicons;
+
+public abstract class SharedStationAiShopSystem : EntitySystem
+{
+    [Dependency] private readonly DamageableSystem _damage = default!;
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StationAiShopComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<StationAiShopComponent, ComponentShutdown>(OnShutdown);
+
+        SubscribeLocalEvent<StationAiShopComponent, StationAiRgbLightingActionEvent>(OnRgbLighting);
+        SubscribeLocalEvent<StationAiShopComponent, StationAiPlaySoundActionEvent>(OnPlaySound);
+        SubscribeLocalEvent<StationAiShopComponent, StationAiHealthChangeActionEvent>(OnHealthChange);
+        SubscribeLocalEvent<StationAiShopComponent, StationAiSpawnEntityActionEvent>(OnHolopointer);
+    }
+
+    private void OnMapInit(Entity<StationAiShopComponent> ent, ref MapInitEvent args)
+    {
+        _actions.AddAction(ent, ref ent.Comp.ShopAction, ent.Comp.ShopActionId);
+    }
+
+    private void OnShutdown(Entity<StationAiShopComponent> ent, ref ComponentShutdown args)
+    {
+        _actions.RemoveAction(ent, ent.Comp.ShopAction);
+    }
+
+    private void OnRgbLighting(Entity<StationAiShopComponent> ent, ref StationAiRgbLightingActionEvent args)
+    {
+        if (HasComp<RgbLightControllerComponent>(args.Target))
+            RemComp<RgbLightControllerComponent>(args.Target);
+        else
+            AddComp<RgbLightControllerComponent>(args.Target);
+
+        args.Handled = true;
+    }
+
+    private void OnPlaySound(Entity<StationAiShopComponent> ent, ref StationAiPlaySoundActionEvent args)
+    {
+        _audio.PlayPredicted(args.Sound, args.Target, ent);
+        args.Handled = true;
+    }
+
+    private void OnHealthChange(Entity<StationAiShopComponent> ent, ref StationAiHealthChangeActionEvent args)
+    {
+        _damage.TryChangeDamage(args.Target, args.Damage);
+        args.Handled = true;
+    }
+
+    private void OnHolopointer(Entity<StationAiShopComponent> ent, ref StationAiSpawnEntityActionEvent args)
+    {
+        Spawn(args.Entity, args.Target);
+        args.Handled = true;
+    }
+}
