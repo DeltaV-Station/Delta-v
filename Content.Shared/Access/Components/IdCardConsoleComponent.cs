56f9589749d47ce2bf4ec9f6af5ba0704d28e635
foxcurl <kitshoffeitt@gmail.com>
2025-05-13 21:01:41 -0400

    typo

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -94,7 +94,7 @@ public sealed partial class IdCardConsoleComponent : Component
         "Clerk", // Delta V - Add Clerk access
         "Surgery", // Delta V - Add Surgery access
         "Funding", // DeltaV - Add Funding access
-        "Vault", //DeltaV - Add Vault access
+        "Vault", // DeltaV - Add Vault access
     };
 
     [Serializable, NetSerializable]
