eea7469e30cd114247efc65797616578000d1ad6
deltanedas <39013340+deltanedas@users.noreply.github.com>
2025-05-18 14:42:47 +0100

    Update Content.Server/_DV/Objectives/Components/CritAnomalyConditionComponent.cs
    Co-authored-by: Tobias Berger <toby@tobot.dev>
    Signed-off-by: deltanedas <39013340+deltanedas@users.noreply.github.com>

--- Content.Server/_DV/Objectives/Components/CritAnomalyConditionComponent.cs
+++ Content.Server/_DV/Objectives/Components/CritAnomalyConditionComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server._DV.Objectives.Components;
 
 /// <summary>
-/// Triggers <c>CodeConditionComponent</c> when an anomaly goes supercritical.
+/// Triggers <see cref="Server.Objectives.Components.CodeConditionComponent" /> when an anomaly goes supercritical.
 /// </summary>
 [RegisterComponent]
 public sealed partial class CritAnomalyConditionComponent : Component;
