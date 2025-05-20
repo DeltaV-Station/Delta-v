aae1c2285918b36dc96ccce9aa465082e86b19a6
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-05-20 15:08:55 +1000

    CompFactory updates (#37559)

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -33,14 +33,11 @@ namespace Content.Server.Weapons.Ranged.Systems;
 
 public sealed partial class GunSystem : SharedGunSystem
 {
-    [Dependency] private readonly IComponentFactory _factory = default!;
-    [Dependency] private readonly BatterySystem _battery = default!;
     [Dependency] private readonly DamageExamineSystem _damageExamine = default!;
     [Dependency] private readonly PricingSystem _pricing = default!;
     [Dependency] private readonly SharedColorFlashEffectSystem _color = default!;
     [Dependency] private readonly SharedStaminaSystem _stamina = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
-    [Dependency] private readonly PowerCellSystem _powerCell = default!;
     [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly TemperatureSystem _temperature = default!; // DeltaV Heat change system
 
