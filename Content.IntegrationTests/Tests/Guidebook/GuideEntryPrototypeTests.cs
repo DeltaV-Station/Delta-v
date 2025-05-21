d56dc7d5f2590004665b8e731082559f497de307
deltanedas <@deltanedas:kde.org>
2025-05-21 18:51:39 +0100

    evil test + kill hemo/cyanoxadone

--- Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
+++ Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
@@ -34,7 +34,7 @@ public sealed class GuideEntryPrototypeTests
             });
 
             // Avoid styleguide update limit
-            await client.WaitRunTicks(1);
+            await client.WaitRunTicks(10); // DeltaV - didn't avoid hard enough
         }
 
         await pair.CleanReturnAsync();
