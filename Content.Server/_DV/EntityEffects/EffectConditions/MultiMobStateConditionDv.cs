7406fd77477d96f73caf8b0c565e30fb8a84234e
Quanteey <61941975+Quanteey@users.noreply.github.com>
2025-05-22 20:03:24 +0200

    Synthoxadone!

---
+++ Content.Server/_DV/EntityEffects/EffectConditions/MultiMobStateConditionDv.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using Content.Shared.EntityEffects;
+using Content.Shared.Localizations;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._DV.EntityEffects.EffectConditions;
+
+/// <summary>
+/// This works like the upstream MobStateCondition, but it accepts a list of states instead of just one.
+/// Helps with de-cluttering the guidebook for stuff that does one thing for dead mobs and a different one
+/// for critical and alive mobs.
+/// </summary>
+public sealed partial class MultiMobStateConditionDv : EntityEffectCondition
+{
+    [DataField("states", required: true)]
+    public List<MobState> States { get; private set; } = [];
+
+    public override bool Condition(EntityEffectBaseArgs args)
+    {
+        if (args.EntityManager.TryGetComponent(args.TargetEntity, out MobStateComponent? mobState))
+        {
+            return States.Contains(mobState.CurrentState);
+        }
+
+        return false;
+    }
+
+    public override string GuidebookExplanation(IPrototypeManager prototype)
+    {
+        var stateStrings = States.Select(s => s.ToString().ToLower()).ToList();
+        var formattedStates = ContentLocalizationManager.FormatListToOr(stateStrings);
+
+        return Loc.GetString("reagent-effect-condition-guidebook-mob-state-condition", ("state", formattedStates));
+    }
+}
