0e935c8a720493476f549fe7e5f3c27ed2bbb395
Quanteey <61941975+Quanteey@users.noreply.github.com>
2025-05-23 10:41:04 +0200

    requested changes

--- Content.Shared/_DV/EntityEffects/EffectConditions/MultiMobStateConditionDv.cs
+++ Content.Shared/_DV/EntityEffects/EffectConditions/MultiMobStateConditionDv.cs
@@ -5,17 +5,17 @@ using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server._DV.EntityEffects.EffectConditions;
+namespace Content.Shared._DV.EntityEffects.EffectConditions;
 
 /// <summary>
 /// This works like the upstream MobStateCondition, but it accepts a list of states instead of just one.
 /// Helps with de-cluttering the guidebook for stuff that does one thing for dead mobs and a different one
 /// for critical and alive mobs.
 /// </summary>
-public sealed partial class MultiMobStateConditionDv : EntityEffectCondition
+public sealed partial class MultiMobStateCondition : EntityEffectCondition
 {
-    [DataField("states", required: true)]
-    public List<MobState> States { get; private set; } = [];
+    [DataField(required: true)]
+    public List<MobState> States = new();
 
     public override bool Condition(EntityEffectBaseArgs args)
     {
