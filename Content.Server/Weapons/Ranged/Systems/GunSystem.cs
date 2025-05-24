c47dd3e51b49615a4260c1907642538e2b82f0e5
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-05-24 19:36:38 +0200

    Move PriceCalculationEvent and EstimatedPriceCalculationEvent to shared (#37782)
    * moce PriceCalculationEvent to shared
    
    * Update Content.Shared/Cargo/PriceCalculationEvent.cs
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>
    
    ---------
    
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -5,6 +5,7 @@ using Content.Server.Power.EntitySystems;
 using Content.Server.Stunnable; // DeltaV
 using Content.Server.Temperature.Systems; // DeltaV Heat Change system
 using Content.Server.Weapons.Ranged.Components;
+using Content.Shared.Cargo;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Database;
