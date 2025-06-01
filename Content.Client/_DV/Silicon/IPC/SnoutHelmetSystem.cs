b9279b705e36f62269aa2b1245910338463b6004
Falcon <falcon@zigtag.dev>
2025-06-01 12:27:43 -0700

    refactor: Move change-able variables to private consts

--- Content.Client/_DV/Silicon/IPC/SnoutHelmetSystem.cs
+++ Content.Client/_DV/Silicon/IPC/SnoutHelmetSystem.cs
@@ -7,11 +7,14 @@ namespace Content.Client._DV.Silicon.IPC;
 
 public sealed class SnoutHelmetSystem : VisualizerSystem<SnoutHelmetComponent>
 {
+    private const MarkingCategories MarkingToQuery = MarkingCategories.Snout;
+    private const int MaximumMarkingCount = 0;
+
     protected override void OnAppearanceChange(EntityUid uid, SnoutHelmetComponent component, ref AppearanceChangeEvent args)
     {
         if (TryComp(uid, out HumanoidAppearanceComponent? humanoidAppearanceComponent) &&
-            humanoidAppearanceComponent.ClientOldMarkings.Markings.TryGetValue(MarkingCategories.Snout, out var data) &&
-            data.Count > 0)
+            humanoidAppearanceComponent.ClientOldMarkings.Markings.TryGetValue(MarkingToQuery, out var markings) &&
+            markings.Count > MaximumMarkingCount)
         {
             component.EnableAlternateHelmet = true;
         }
