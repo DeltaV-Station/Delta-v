28fcf1422057632b61cef4a1c12a81574854eddd
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-06-02 16:02:41 +0200

    Add multipart machines system (#35969)

--- Content.Server/Entry/IgnoredComponents.cs
+++ Content.Server/Entry/IgnoredComponents.cs
@@ -20,7 +20,8 @@ namespace Content.Server.Entry
             "LightFade",
             "HolidayRsiSwap",
             "OptionsVisualizer",
-            "DrawDepthVisualizer" // DeltaV
+            "DrawDepthVisualizer", // DeltaV
+            "MultipartMachineGhost"
         };
     }
 }
