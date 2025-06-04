80e1d82f4b42ae1d3da07d5b88e352d1fdbd3f07
vitopigno <103512727+VitusVeit@users.noreply.github.com>
2025-06-04 12:12:37 +0200

    Add text highlighting (#31442)
    Heavy DeltaV modications by Vanessa
    
    Co-authored-by: ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
    Co-authored-by: Hans Larsson <hanandlia@gmail.com>
    Co-authored-by: Tobias Berger <toby@tobot.dev>
    Co-authored-by: Vanessa <vanessalouwagie@gmail.com>

--- Content.Client/_DV/UserInterfaces/Systems/Chat/Controls/Widgets/ChatBox.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Client.UserInterface.Systems.Chat.Widgets;
-
-public partial class ChatBox
-{
-    private void OnNewHighlights(string highlights)
-    {
-        _controller.UpdateHighlights(highlights);
-    }
-}
