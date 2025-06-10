725e35a229387943da77e76beff7959e3d393d79
Cepelinas1 <kakelis01@gmail.com>
2025-06-11 01:01:39 +0300

    As requested, moved the announcement folder to _DV folder, which only now includes deltaV or nyanotrasen specific audio files. Please double check the license.txt in Announcements/RoundEnd to see if I got it right.

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
@@ -393,7 +393,7 @@ public sealed partial class EmergencyShuttleSystem : EntitySystem
 
         var audioFile = result.ResultType == ShuttleDockResultType.NoDock
             ? "/Audio/Misc/notice1.ogg"
-            : "/Audio/Announcements/shuttle_dock.ogg";
+            : "/Audio/_DV/Announcements/shuttle_dock.ogg"; // DeltaV - custom announcer
 
         // TODO: Need filter extensions or something don't blame me.
         _audio.PlayGlobal(audioFile, Filter.Broadcast(), true);
