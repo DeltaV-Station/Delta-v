9d5203d41d6dd350045bd3a7cd7c55041276ebd7
AlgisAlphonse <algis.posiunas@gmail.com>
2025-06-13 01:34:57 +0200

    Increases magnet time, reduces fulton time, etc. etc.

--- Content.Server/Salvage/SalvageSystem.Magnet.cs
+++ Content.Server/Salvage/SalvageSystem.Magnet.cs
@@ -107,7 +107,7 @@ public sealed partial class SalvageSystem
                 {
                     EndMagnet((uid, magnetData));
                 }
-                else if (!magnetData.Announced && (magnetData.EndTime.Value - curTime).TotalSeconds < 31)
+                else if (!magnetData.Announced && (magnetData.EndTime.Value - curTime).TotalSeconds < 59) //DeltaV: was 31 seconds. Increased to give time to actually fulton a crate out. 59 seconds picked because .Seconds only supports values up to 59.
                 {
                     var magnet = GetMagnet((uid, magnetData));
 
