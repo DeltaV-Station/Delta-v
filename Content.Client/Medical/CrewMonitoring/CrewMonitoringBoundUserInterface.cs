c3f3290fdd571bcac202724b4d4f606d6a102263
elperson <elpersonpl@gmail.com>
2025-06-18 20:27:11 +0200

    base fixes to crewmon, committing so that i can revert later if needed

--- Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
+++ Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
@@ -1,5 +1,10 @@
 using Content.Shared.Medical.CrewMonitoring;
 using Robust.Client.UserInterface;
+using Content.Client.Station;
+using System.Diagnostics;
+using System.Linq;
+using Content.Shared.Station.Components;
+using Robust.Shared.Log;
 
 namespace Content.Client.Medical.CrewMonitoring;
 
@@ -15,11 +20,18 @@ public sealed class CrewMonitoringBoundUserInterface : BoundUserInterface
     protected override void Open()
     {
         base.Open();
-
+        var debug = Logger.GetSawmill("crewmon-ui");
         EntityUid? gridUid = null;
         var stationName = string.Empty;
-
-        if (EntMan.TryGetComponent<TransformComponent>(Owner, out var xform))
+        // Delta-v start of crew monitor display correction
+        var station = EntMan.EntitySysManager.GetEntitySystem<StationSystem>().Stations;
+        var station0 = station.FirstOrDefault();
+        
+        debug.Debug($"Stations: {station.Count} Station 0 name: {station0.Name}");
+        var station0_uid = EntMan.GetEntity(station0.Entity);
+        debug.Debug($"Station 0 ENT-UID: {station0_uid}, station 0 netid: {station0.Entity.Id}");
+        
+        if (EntMan.TryGetComponent<TransformComponent>(station0_uid, out var xform))
         {
             gridUid = xform.GridUid;
 
@@ -28,6 +40,7 @@ public sealed class CrewMonitoringBoundUserInterface : BoundUserInterface
                 stationName = metaData.EntityName;
             }
         }
+        
 
         _menu = this.CreateWindow<CrewMonitoringWindow>();
         _menu.Set(stationName, gridUid);
