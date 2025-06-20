0d25681d561f82bdafc520616609b3ef538dbc76
beck <163376292+widgetbeck@users.noreply.github.com>
2025-06-20 01:44:14 -0400

    Replicator Balance (#2804)
    * stack point calc, nest preserves mechs, t3 aac vocab rework
    
    * readjust replicator stun prod
    
    * trailing whitespace
    
    * re-up stamina damage
    
    * rep balance tweaks
    
    * modify struct damage coefficients
    
    * detankify t3s slightly
    
    * add visual indicator for which replicator is the queen
    
    * add ZombieImmune component to Replicators

--- Content.Server/_Impstation/Replicator/ReplicatorSystem.cs
+++ Content.Server/_Impstation/Replicator/ReplicatorSystem.cs
@@ -2,6 +2,7 @@
 // all credit for the core gameplay concepts and a lot of the core functionality of the code goes to the folks over at Goob, but I re-wrote enough of it to justify putting it in our filestructure.
 // the original Bingle PR can be found here: https://github.com/Goob-Station/Goob-Station/pull/1519
 
+using Content.Server._Impstation.Administration.Components;
 using Content.Server.Actions;
 using Content.Server.Emp;
 using Content.Server.Ghost.Roles.Events;
