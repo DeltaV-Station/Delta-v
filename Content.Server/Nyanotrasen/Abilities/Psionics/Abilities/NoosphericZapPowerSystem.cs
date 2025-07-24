0864159585745ce35abb0d50e1693b4cd159889a
kotobdev <abzik7890@gmail.com>
2025-07-23 22:32:55 -0700

    no this is way cooler

--- Content.Server/Nyanotrasen/Abilities/Psionics/Abilities/NoosphericZapPowerSystem.cs
+++ Content.Server/Nyanotrasen/Abilities/Psionics/Abilities/NoosphericZapPowerSystem.cs
@@ -5,28 +5,19 @@ using Content.Shared.StatusEffect;
 using Content.Server.Stunnable;
 using Content.Server.Beam;
 using Content.Server.Inventory;
+using Content.Server.Lightning;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using Content.Server.Mind;
 using Content.Shared.Actions.Events;
-using Content.Shared.Electrocution;
-using Content.Shared.Lightning;
 
 namespace Content.Server.Abilities.Psionics
 {
     public sealed class NoosphericZapPowerSystem : EntitySystem
     {
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-        [Dependency] private readonly ServerInventorySystem _inventory = default!;
         [Dependency] private readonly SharedActionsSystem _actions = default!;
-        [Dependency] private readonly SharedLightningSystem _lightning = default!;
+        [Dependency] private readonly LightningSystem _lightning = default!;
         [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
-        [Dependency] private readonly StunSystem _stunSystem = default!;
-        [Dependency] private readonly StatusEffectsSystem _statusEffectsSystem = default!;
-        [Dependency] private readonly IGameTiming _gameTiming = default!;
-        [Dependency] private readonly BeamSystem _beam = default!;
-        [Dependency] private readonly MindSystem _mindSystem = default!;
-
 
         public override void Initialize()
         {
@@ -61,22 +52,14 @@ namespace Content.Server.Abilities.Psionics
         private void OnPowerUsed(NoosphericZapPowerActionEvent args)
         {
             _psionics.LogPowerUsed(args.Performer, "noospheric zap");
-
-            _beam.TryCreateBeam(args.Performer, args.Target, "LightningNoospheric");
-
-            if (!HasComp<PotentialPsionicComponent>(args.Target))
+            
+            if (HasComp<PotentialPsionicComponent>(args.Target))
                 return;
 
-            if (HasComp<PsionicInsulationComponent>(args.Target))
+            if (!HasComp<PsionicInsulationComponent>(args.Target))
                 return;
 
-            if (_inventory.TryGetSlotEntity(args.Target, "gloves", out var gloves)
-                && TryComp<InsulatedComponent>(gloves, out var insulation)
-                && insulation.Coefficient == 0)
-                return;
-
-            _stunSystem.TryParalyze(args.Target, TimeSpan.FromSeconds(5), false);
-            _statusEffectsSystem.TryAddStatusEffect(args.Target, "Stutter", TimeSpan.FromSeconds(10), false, "StutteringAccent");
+            _lightning.ShootLightning(args.Performer, args.Target);
 
             args.Handled = true;
         }
