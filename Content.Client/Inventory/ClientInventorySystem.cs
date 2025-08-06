c80d74cc666fab9cae4a27b701061f2eb070da26
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-08-06 22:14:28 +0200

    Fix inventory flickering and missing InventoryTemplateUpdated event (#39379)
    * fix flickering
    
    * move InitClothing
    
    * fix this

--- Content.Client/Inventory/ClientInventorySystem.cs
+++ Content.Client/Inventory/ClientInventorySystem.cs
@@ -116,6 +116,13 @@ namespace Content.Client.Inventory
             OnLinkInventorySlots?.Invoke(uid, component);
         }
 
+        protected override void OnInit(Entity<InventoryComponent> ent, ref ComponentInit args)
+        {
+            base.OnInit(ent, ref args);
+
+            _clothingVisualsSystem.InitClothing(ent.Owner, ent.Comp);
+        }
+
         public override void Shutdown()
         {
             CommandBinds.Unregister<ClientInventorySystem>();
@@ -262,7 +269,6 @@ namespace Content.Client.Inventory
                     TryAddSlotData((ent.Owner, inventorySlots), (SlotData)slot);
             }
 
-            _clothingVisualsSystem.InitClothing(ent, ent.Comp);
             if (ent.Owner == _playerManager.LocalEntity)
                 ReloadInventory(inventorySlots);
         }
