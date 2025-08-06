233478726c259c79080b86270b1b4885c40da98b
Quanteey <61941975+Quanteey@users.noreply.github.com>
2025-08-07 00:09:55 +0200

    import me pls

--- Content.Shared/Rootable/SharedRootableSystem.cs
+++ Content.Shared/Rootable/SharedRootableSystem.cs
@@ -1,4 +1,5 @@
-﻿using Content.Shared.Damage.Components;
+﻿using Content.Server.Damage.Components;
+using Content.Shared.Damage.Components;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Components;
 using Content.Shared.Alert;
