57bf35aa8dc81d517885c73cbe02fa8e171ae5d3
kotobdev <abzik7890@gmail.com>
2025-08-06 12:53:08 -0700

    toggleable + minor cleanup

--- Content.Client/_DV/Overlays/GlimmerOverlaySystem.cs
+++ Content.Client/_DV/Overlays/GlimmerOverlaySystem.cs
@@ -1,11 +1,14 @@
+using Content.Shared._DV.CCVars;
 using Content.Shared.Psionics.Glimmer;
 using Robust.Client.Graphics;
+using Robust.Shared.Configuration;
 
 namespace Content.Client._DV.Overlays;
 
 public sealed partial class GlimmerOverlaySystem : EntitySystem
 {
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
 
     private GlimmerOverlay _overlay = default!;
 
@@ -13,12 +16,16 @@ public sealed partial class GlimmerOverlaySystem : EntitySystem
     {
         base.Initialize();
 
-        _overlayMan.AddOverlay(_overlay = new());
+        _overlay = new GlimmerOverlay();
         SubscribeNetworkEvent<GlimmerChangedEvent>(OnGlimmerChanged);
+        Subs.CVar(_cfg, DCCVars.DisableGlimmerShader, OnDisableGlimmerShaderChanged);
     }
 
     private void OnGlimmerChanged(GlimmerChangedEvent eventArgs)
     {
+        if(_cfg.GetCVar(DCCVars.DisableGlimmerShader))
+            return;
+
         if(eventArgs.Glimmer > 700)
         {
             _overlay.ActualGlimmerLevel = eventArgs.Glimmer;
@@ -41,6 +48,14 @@ public sealed partial class GlimmerOverlaySystem : EntitySystem
     {
         base.Shutdown();
         _overlayMan.RemoveOverlay<GlimmerOverlay>();
+    }
+
+    private void OnDisableGlimmerShaderChanged(bool enabled)
+    {
+        if (enabled)
+            _overlayMan.RemoveOverlay(_overlay);
+        else if (_overlay.ActualGlimmerLevel > 700)
+            _overlayMan.AddOverlay(_overlay);
     }
 
 }
