f5290d770212cd01ef5eb40dfc3f607e8506dd12
No Elka <no.elka.the.god@gmail.com>
2025-08-12 13:28:22 +0300

    Random rift spawn

---
+++ Content.Server/_DV/StationEvents/Components/RandomMultipleSpawnRuleComponent.cs
@@ -0,0 +1,30 @@
+﻿using Content.Server.StationEvents.Events;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server._DV.StationEvents.Components;
+
+/// <summary>
+/// Spawns a random amount of entities at a random tile on a station using TryGetRandomTile.
+/// </summary>
+[RegisterComponent, Access(typeof(RandomSpawnRule))]
+public sealed partial class RandomMultipleSpawnRuleComponent : Component
+{
+    /// <summary>
+    /// The entity to be spawned.
+    /// </summary>
+    [DataField("prototype", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string Prototype = string.Empty;
+
+    /// <summary>
+    /// The minimum amount of entities to be spawned.
+    /// </summary>
+    [DataField]
+    public int MinAmount = 1;
+
+    /// <summary>
+    /// The maximum amount of entities to be spawned.
+    /// </summary>
+    [DataField]
+    public int MaxAmount = 1;
+}
