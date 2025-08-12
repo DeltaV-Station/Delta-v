f0d4c263e0ceee4a9c22d776790184650f6c486d
No Elka <no.elka.the.god@gmail.com>
2025-08-12 13:52:37 +0300

    Tweak timers

--- Content.Shared/_DV/CCVars/DCCVars.cs
+++ Content.Shared/_DV/CCVars/DCCVars.cs
@@ -233,16 +233,16 @@ public sealed partial class DCCVars
         CVarDef.Create("cosmiccult.steward_vote_delay", 25, CVar.SERVER);
 
     /// <summary>
-    /// The delay between the monument getting upgraded to tier 2 and the crew learning of that fact. the monument cannot be upgraded again in this time.
+    /// The delay between the monument getting upgraded to tier 2 and rifts starting to appear. the monument cannot be upgraded again in this time.
     /// </summary>
     public static readonly CVarDef<int> CosmicCultT2RevealDelaySeconds =
-        CVarDef.Create("cosmiccult.t2_reveal_delay_seconds", 180, CVar.SERVER);
+        CVarDef.Create("cosmiccult.t2_reveal_delay_seconds", 30, CVar.SERVER);
 
     /// <summary>
     /// The delay between the monument getting upgraded to tier 3 and the crew learning of that fact. the monument cannot be upgraded again in this time.
     /// </summary>
     public static readonly CVarDef<int> CosmicCultT3RevealDelaySeconds =
-        CVarDef.Create("cosmiccult.t3_reveal_delay_seconds", 100, CVar.SERVER);
+        CVarDef.Create("cosmiccult.t3_reveal_delay_seconds", 180, CVar.SERVER);
 
     /// <summary>
     /// The delay between the monument getting upgraded to tier 3 and the finale starting.
