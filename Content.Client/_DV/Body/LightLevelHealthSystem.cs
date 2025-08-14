1b455a5cbcc92ab2752f0c6fa77a3a829f991b2a
William Lemon <William.Lemon2@gmail.com>
2025-08-14 13:13:27 +1000

    Added some missing Protos

--- Content.Client/_DV/Body/LightLevelHealthSystem.cs
+++ Content.Client/_DV/Body/LightLevelHealthSystem.cs
@@ -13,15 +13,10 @@ public sealed class LightLevelHealthSystem : SharedLightLevelHealthSystem
     [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly LightReactiveSystem _lightReactive = default!;
 
-    [ValidatePrototypeId<AlertPrototype>]
-    private const string LightLevelDarkIcon = "LightLevelDarkIcon";
-    [ValidatePrototypeId<AlertPrototype>]
-    private const string LightLevelNeutralIcon = "LightLevelNeutralIcon";
-    [ValidatePrototypeId<AlertPrototype>]
-    private const string LightLevelBrightIcon = "LightLevelBrightIcon";
-
-    [ValidatePrototypeId<AlertCategoryPrototype>]
-    private const string LightAlertCategory = "Light";
+    private static ProtoId<AlertPrototype> _lightLevelDarkIcon = "LightLevelDarkIcon";
+    private static ProtoId<AlertPrototype> _lightLevelNeutralIcon = "LightLevelNeutralIcon";
+    private static ProtoId<AlertPrototype> _lightLevelBrightIcon = "LightLevelBrightIcon";
+    private static ProtoId<AlertCategoryPrototype> _lightAlertCategory = "Light";
 
     private int _lastThreshold = 0;
 
@@ -30,9 +25,8 @@ public sealed class LightLevelHealthSystem : SharedLightLevelHealthSystem
         base.Update(frameTime);
 
         // If we're a LightLevelHealthComponent client, update our alerts.
-        if (_player.LocalSession?.AttachedEntity is null)
+        if (_player.LocalSession?.AttachedEntity is not { } ent)
             return;
-        var ent = _player.LocalSession.AttachedEntity.Value;
 
         if (!TryComp<LightLevelHealthComponent>(ent, out var lightLevelHealth))
             return;
@@ -40,22 +34,20 @@ public sealed class LightLevelHealthSystem : SharedLightLevelHealthSystem
         var currentThreshold = CurrentThreshold(_lightReactive.GetLightLevelForPoint(ent), lightLevelHealth);
         var alertIcon = currentThreshold switch
         {
-            -1 => LightLevelDarkIcon,
-            1 => LightLevelBrightIcon,
-            _ => LightLevelNeutralIcon,
+            -1 => _lightLevelDarkIcon,
+            1 => _lightLevelBrightIcon,
+            _ => _lightLevelNeutralIcon,
         };
 
         if (currentThreshold != _lastThreshold)
         {
-            if (_prototype.TryIndex<AlertCategoryPrototype>(LightAlertCategory, out var alertCategory))
+            if(_prototype.Index(_lightAlertCategory) is { } alertCategory)
                 _alerts.ClearAlertCategory(ent, alertCategory);
         }
         _lastThreshold = currentThreshold;
 
-        if (!_prototype.TryIndex<AlertPrototype>(alertIcon, out var alertProto))
-        {
+        if (_prototype.Index(alertIcon) is not { } alertProto)
             return;
-        }
 
         _alerts.ShowAlert(ent, alertProto);
     }
