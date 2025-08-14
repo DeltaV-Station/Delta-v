bac514fc943fd710aa473885ec371e489b09914b
No Elka <125199100+NoElkaTheGod@users.noreply.github.com>
2025-08-15 02:51:17 +0300

    Multispectral inhibitor, new cosmic cult structure (#4181)
    * JammerSystem additions
    
    * Temporary sprite
    
    * Prototypes
    
    * Locale and guidebook changes
    
    * Revert "JammerSystem additions"
    
    This reverts commit b4019131b2411df2be90c016f7f3c41c85f42bf8.
    
    * All my homies hate RadioJammerComponent
    
    * Lower the range a tad
    
    * Add actual sprite
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix stuff
    
    * Kill SpriteFade while I'm at it
    
    * Increase range
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- Content.Server/_DV/CosmicCult/CosmicCultSystem.cs
+++ Content.Server/_DV/CosmicCult/CosmicCultSystem.cs
@@ -6,6 +6,7 @@ using Content.Server.Chat.Systems;
 using Content.Server.GameTicking.Events;
 using Content.Server.Pinpointer;
 using Content.Server.Popups;
+using Content.Server.Radio;
 using Content.Server.Station.Systems;
 using Content.Shared._DV.CosmicCult.Components;
 using Content.Shared._DV.CosmicCult;
@@ -57,6 +58,9 @@ public sealed partial class CosmicCultSystem : SharedCosmicCultSystem
     [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
     [Dependency] private readonly StationSystem _station = default!;
     [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly AmbientSoundSystem _ambient = default!;
+    [Dependency] private readonly SharedPointLightSystem _lights = default!;
 
     private readonly ResPath _mapPath = new("Maps/_DV/Nonstations/cosmicvoid.yml");
 
@@ -90,6 +94,9 @@ public sealed partial class CosmicCultSystem : SharedCosmicCultSystem
 
         SubscribeLocalEvent<CosmicSubtleMarkComponent, ExaminedEvent>(OnSubtleMarkExamined);
         SubscribeLocalEvent<CosmicCultComponent, EncryptionChannelsChangedEvent>(OnTransmitterChannelsChangedCult, after: new[] { typeof(IntrinsicRadioKeySystem) });
+
+        SubscribeLocalEvent<RadioSendAttemptEvent>(OnRadioSendAttempt);
+        SubscribeLocalEvent<CosmicJammerComponent, AnchorStateChangedEvent>(OnJammerAnchorStateChange);
 
         SubscribeFinale(); //Hook up the cosmic cult finale system
     }
@@ -237,4 +244,27 @@ public sealed partial class CosmicCultSystem : SharedCosmicCultSystem
 
     }
 
+    private void OnJammerAnchorStateChange(Entity<CosmicJammerComponent> ent, ref AnchorStateChangedEvent args)
+    {
+        ent.Comp.Active = args.Anchored;
+        _ambient.SetAmbience(ent, args.Anchored);
+        _lights.SetEnabled(ent, args.Anchored);
+    }
+
+    private void OnRadioSendAttempt(ref RadioSendAttemptEvent args)
+    {
+        if (args.Channel == CosmicRadio) return; // Cult can still communicate within range of a jammer.
+
+        var source = Transform(args.RadioSource).Coordinates;
+        var query = EntityQueryEnumerator<CosmicJammerComponent, TransformComponent>();
+
+        while (query.MoveNext(out var uid, out var jammer, out var transform))
+        {
+            if (_transform.InRange(source, transform.Coordinates, jammer.Range) && jammer.Active)
+            {
+                args.Cancelled = true;
+                return;
+            }
+        }
+    }
 }
