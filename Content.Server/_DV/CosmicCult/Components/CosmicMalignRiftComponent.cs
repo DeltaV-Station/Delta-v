f30c42601afc5fc4d4a8d753931b5159d23fc2e7
No Elka <no.elka.the.god@gmail.com>
2025-08-17 21:12:29 +0300

    Our cult so cosmic

--- Content.Server/_DV/CosmicCult/Components/CosmicMalignRiftComponent.cs
+++ Content.Server/_DV/CosmicCult/Components/CosmicMalignRiftComponent.cs
@@ -1,5 +1,6 @@
 using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Server._DV.CosmicCult.Components;
 
@@ -29,4 +30,18 @@ public sealed partial class CosmicMalignRiftComponent : Component
 
     [DataField]
     public TimeSpan AbsorbTime = TimeSpan.FromSeconds(35);
+
+    [DataField]
+    public TimeSpan MinPulseTime = TimeSpan.FromSeconds(20);
+
+    [DataField]
+    public TimeSpan MaxPulseTime = TimeSpan.FromSeconds(80);
+
+    [DataField]
+    public float PulseRange = 15f;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
+    public TimeSpan NextPulseTime = default!;
+
+    [DataField] public EntProtoId PulseVFX = "CosmicGenericVFX";
 }
