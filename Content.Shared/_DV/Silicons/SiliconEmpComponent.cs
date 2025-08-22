6409965cbecc5353d355032844c720e2d4a17b91
No Elka <no.elka.the.god@gmail.com>
2025-08-22 20:27:00 +0300

    Why was that even here

--- Content.Shared/_DV/Silicons/SiliconEmpComponent.cs
+++ Content.Shared/_DV/Silicons/SiliconEmpComponent.cs
@@ -1,7 +1,5 @@
-using Content.Server.Emp;
-using Content.Shared.Damage;
+namespace Content.Shared._DV.Silicons;
 
-namespace Content.Server._DV.Silicons;
 ///<summary>
 ///This components prevents the normal EMP effects on an entity, and makes it take ion damage instead
 ///</summary>
