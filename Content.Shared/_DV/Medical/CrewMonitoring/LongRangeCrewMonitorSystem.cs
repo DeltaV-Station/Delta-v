e5a80229cb1e66886d1af4d39b642a25df215ee5
elperson <elpersonpl@gmail.com>
2025-08-26 22:58:41 +0200

    Simple fix

--- Content.Shared/_DV/Medical/CrewMonitoring/LongRangeCrewMonitorSystem.cs
+++ Content.Shared/_DV/Medical/CrewMonitoring/LongRangeCrewMonitorSystem.cs
@@ -22,4 +22,21 @@ public sealed class LongRangeCrewMonitorSystem : EntitySystem
 
         return null;
     }
+    /// <summary>
+    /// Finds the largest (presumably the main station) grid on the same map as the argument.
+    /// </summary>
+    /// <param name="map"></param>
+    /// <returns>Returns null if not found</returns>
+    public EntityUid? FindLargestStationGridInMap(MapId map)
+    {
+        // also requiring MapGrid incase StationMember gets used for non-grids in the future
+        (EntityUid?, int) biggest_grid = (null, 0);
+        var query = EntityQueryEnumerator<StationMemberComponent, MapGridComponent>();
+        while (query.MoveNext(out var grid, out _, out var mapgrid))
+        {
+            if (Transform(grid).MapID == map && mapgrid.ChunkCount > biggest_grid.Item2)
+                biggest_grid = (grid, mapgrid.ChunkCount);
+        }
+        return biggest_grid.Item1;
+    }
 }
