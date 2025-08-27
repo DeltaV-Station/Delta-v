8b7aa3f73e29f0cf04ef6e46d52e2b5958c9acd4
elperson <elpersonpl@gmail.com>
2025-08-27 14:38:45 +0200

    maintainer requested changes

--- Content.Shared/_DV/Medical/CrewMonitoring/LongRangeCrewMonitorSystem.cs
+++ Content.Shared/_DV/Medical/CrewMonitoring/LongRangeCrewMonitorSystem.cs
@@ -7,22 +7,6 @@ namespace Content.Shared._DV.Medical.CrewMonitoring;
 public sealed class LongRangeCrewMonitorSystem : EntitySystem
 {
     /// <summary>
-    /// Finds an arbitrary station grid on the same map as the argument.
-    /// Returns null if no grid was found.
-    /// </summary>
-    public EntityUid? FindStationGridInMap(MapId map)
-    {
-        // also requiring MapGrid incase StationMember gets used for non-grids in the future
-        var query = EntityQueryEnumerator<StationMemberComponent, MapGridComponent>();
-        while (query.MoveNext(out var grid, out _, out _))
-        {
-            if (Transform(grid).MapID == map)
-                return grid;
-        }
-
-        return null;
-    }
-    /// <summary>
     /// Finds the largest (presumably the main station) grid on the same map as the argument.
     /// </summary>
     /// <param name="map"></param>
