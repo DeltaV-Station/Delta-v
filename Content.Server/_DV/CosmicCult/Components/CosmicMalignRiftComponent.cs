0f00e68bf7f1c01e0590a7ba7dceefd06d44582c
No Elka <no.elka.the.god@gmail.com>
2025-08-28 15:13:54 +0300

    Change stuff

--- Content.Server/_DV/CosmicCult/Components/CosmicMalignRiftComponent.cs
+++ Content.Server/_DV/CosmicCult/Components/CosmicMalignRiftComponent.cs
@@ -14,29 +14,17 @@ public sealed partial class CosmicMalignRiftComponent : Component
     [DataField]
     public bool Occupied;
 
-    [DataField]
-    public EntProtoId PurgeVFX = "CleanseEffectVFX";
-
-    [DataField]
-    public SoundSpecifier PurgeSound = new SoundPathSpecifier("/Audio/_DV/CosmicCult/cleanse_deconversion.ogg");
-
     // [DataField]
     // public EntProtoId GrailID = "NullRodGrail"; // Not implemented at this time
 
     [DataField]
-    public TimeSpan BibleTime = TimeSpan.FromSeconds(35);
+    public TimeSpan AbsorbTime = TimeSpan.FromSeconds(20);
 
     [DataField]
-    public TimeSpan ChaplainTime = TimeSpan.FromSeconds(20);
+    public TimeSpan MinPulseTime = TimeSpan.FromSeconds(10);
 
     [DataField]
-    public TimeSpan AbsorbTime = TimeSpan.FromSeconds(35);
-
-    [DataField]
-    public TimeSpan MinPulseTime = TimeSpan.FromSeconds(20);
-
-    [DataField]
-    public TimeSpan MaxPulseTime = TimeSpan.FromSeconds(60);
+    public TimeSpan MaxPulseTime = TimeSpan.FromSeconds(30);
 
     [DataField]
     public float PulseRange = 15f;
