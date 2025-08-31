c36679fbd8f903232829652110736df4d200c980
kotobdev <59124164+kotobdev@users.noreply.github.com>
2025-08-30 20:56:14 -0700

    make it awesome
    uncopy code steamhappy

--- Content.Server/_DV/AkashicFold/AkashicFoldSpawnerSystem.cs
+++ Content.Server/_DV/AkashicFold/AkashicFoldSpawnerSystem.cs
@@ -1,33 +1,34 @@
-using Content.Server._DV.AkashicFold.Components;
 using Content.Server._DV.Planet;
+using Content.Server.GameTicking.Events;
+using Content.Shared._DV.Planet;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Server._DV.AkashicFold;
 
-// okay look this is entirely copy-pasted from StationPlanetSpawnerSystem but every class is sealed so :shrug: what am i supposed to do
-// i am doing this as more of a proof-of-concept i will ask smarter people then me the Correct way to do this later
+// entity component system? more like entity nothing system amirite
+// stealing coscult code basically
 // todo: delete these comments so i don't seem like a complete nerd
-public sealed class AkashicFoldSpawnerSystem : EntitySystem
+public sealed class AkashicFoldSystem : EntitySystem
 {
     [Dependency] private readonly PlanetSystem _planet = default!;
+
+    public static readonly ProtoId<PlanetPrototype> FoldPlanet = "AkashicFold";
+    private readonly ResPath _baseGridPath = new("Maps/_DV/Nonstations/glacier_surface_outpost.yml");
+    public static EntityUid? Map;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<AkashicFoldSpawnerComponent, MapInitEvent>(OnMapInit);
-        SubscribeLocalEvent<AkashicFoldSpawnerComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<RoundStartingEvent>(OnRoundStart);
+
+        //SubscribeLocalEvent<AkashicFoldSpawnerComponent, MapInitEvent>(OnMapInit);
+        //SubscribeLocalEvent<AkashicFoldSpawnerComponent, ComponentShutdown>(OnShutdown);
     }
 
-    private void OnMapInit(Entity<AkashicFoldSpawnerComponent> ent, ref MapInitEvent args)
+    private void OnRoundStart(RoundStartingEvent ev)
     {
-        if (ent.Comp.GridPath is not {} path)
-            return;
-
-        ent.Comp.Map = _planet.LoadPlanet(ent.Comp.Planet, path);
-    }
-
-    private void OnShutdown(Entity<AkashicFoldSpawnerComponent> ent, ref ComponentShutdown args)
-    {
-        QueueDel(ent.Comp.Map);
+        Map = _planet.LoadPlanet(FoldPlanet, _baseGridPath);
     }
 }
