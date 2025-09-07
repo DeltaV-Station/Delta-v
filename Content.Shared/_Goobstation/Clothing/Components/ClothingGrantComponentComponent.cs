bff43706b84768a49f57429a1e9bbf89d4a4bcf4
kotobdev <59124164+kotobdev@users.noreply.github.com>
2025-09-06 23:00:53 -0700

    no attribution allowed here no siree

--- Content.Shared/_Goobstation/Clothing/Components/ClothingGrantComponentComponent.cs
+++ Content.Shared/_Goobstation/Clothing/Components/ClothingGrantComponentComponent.cs
@@ -1,14 +1,3 @@
-// SPDX-FileCopyrightText: 2024 Aidenkrz <aiden@djkraz.com>
-// SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
-// SPDX-FileCopyrightText: 2024 VMSolidus <evilexecutive@gmail.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
-// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
-// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
-// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Goobstation.Clothing.Components
