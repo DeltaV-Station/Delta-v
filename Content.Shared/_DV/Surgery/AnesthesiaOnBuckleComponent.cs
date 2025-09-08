4a9331bd1b6abcf463d75312e799eb7cf33e6ed9
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-09-08 12:19:07 +0200

    GoodCode:tm:

--- Content.Shared/_DV/Surgery/AnesthesiaOnBuckleComponent.cs
+++ Content.Shared/_DV/Surgery/AnesthesiaOnBuckleComponent.cs
@@ -6,4 +6,11 @@ namespace Content.Shared._DV.Surgery;
 ///     Exists for strap entities to apply surgical anesthesia to the patient upon being buckled.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class AnesthesiaOnBuckleComponent : Component;
+public sealed partial class AnesthesiaOnBuckleComponent : Component
+{
+    /// <summary>
+    /// Check whether the buckled entity was anesthetized already.
+    /// </summary>
+    [DataField]
+    public bool HadAnesthesia;
+}
--- Content.Shared/_DV/Surgery/SharedAnesthesiaOnBuckleSystem.cs
+++ Content.Shared/_DV/Surgery/SharedAnesthesiaOnBuckleSystem.cs
@@ -1,35 +1,29 @@
 using Content.Shared.Buckle.Components;
 using Content.Shared.Examine;
 
-namespace Content.Shared._DV.Surgery
+namespace Content.Shared._DV.Surgery;
+
+public sealed class SharedAnesthesiaOnBuckleSystem : EntitySystem
 {
-    public sealed class SharedAnesthesiaOnBuckleSystem : EntitySystem
+    public override void Initialize()
     {
-        public override void Initialize()
-        {
-            base.Initialize();
-            SubscribeLocalEvent<AnesthesiaOnBuckleComponent, StrappedEvent>(OnStrapped);
-            SubscribeLocalEvent<AnesthesiaOnBuckleComponent, UnstrappedEvent>(OnUnstrapped);
-            SubscribeLocalEvent<AnesthesiaOnBuckleComponent, ExaminedEvent>(OnExamine);
-        }
-
-        private void OnStrapped(Entity<AnesthesiaOnBuckleComponent> bed, ref StrappedEvent args)
-        {
-            EnsureComp<AnesthesiaComponent>(args.Buckle.Owner);
-        }
-
-        private void OnUnstrapped(Entity<AnesthesiaOnBuckleComponent> bed, ref UnstrappedEvent args)
-        {
+        base.Initialize();
+        SubscribeLocalEvent<AnesthesiaOnBuckleComponent, StrappedEvent>(OnStrapped);
+        SubscribeLocalEvent<AnesthesiaOnBuckleComponent, UnstrappedEvent>(OnUnstrapped);
+        SubscribeLocalEvent<AnesthesiaOnBuckleComponent, ExaminedEvent>(OnExamine);
+    }
+    private void OnStrapped(Entity<AnesthesiaOnBuckleComponent> strap, ref StrappedEvent args)
+    {
+        strap.Comp.HadAnesthesia = HasComp<AnesthesiaComponent>(args.Buckle.Owner);
+        EnsureComp<AnesthesiaComponent>(args.Buckle.Owner);
+    }
+    private void OnUnstrapped(Entity<AnesthesiaOnBuckleComponent> strap, ref UnstrappedEvent args)
+    {
+        if (!strap.Comp.HadAnesthesia)
             RemComp<AnesthesiaComponent>(args.Buckle.Owner);
-        }
-
-        private void OnExamine(Entity<AnesthesiaOnBuckleComponent> ent, ref ExaminedEvent args)
-        {
-            if (!HasComp<StrapComponent>(ent))
-            {
-                return;
-            }
-            args.PushMarkup(Loc.GetString("anesthesia-on-buckle"));
-        }
+    }
+    private void OnExamine(Entity<AnesthesiaOnBuckleComponent> ent, ref ExaminedEvent args)
+    {
+        args.PushMarkup(Loc.GetString("anesthesia-on-buckle"));
     }
 }
