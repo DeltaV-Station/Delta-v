fc865c2a9183b85fc4063029ff8d4a296188cd94
Cepelinas1 <kakelis01@gmail.com>
2025-09-10 18:02:23 +0300

    With massive Quanteey's help and guidance, code cleanup once again.

--- Content.Server/_DV/StationEvents/Events/EpsilonEventRule.cs
+++ Content.Server/_DV/StationEvents/Events/EpsilonEventRule.cs
@@ -7,58 +7,72 @@ using Content.Shared.Station.Components;
 using JetBrains.Annotations;
 using Robust.Shared.Player;
 using Content.Server.AlertLevel;
+using Content.Server.Audio;
+using Robust.Shared.Audio.Systems;
 using Timer = Robust.Shared.Timing.Timer;
 
 namespace Content.Server.StationEvents.Events
 {
-    [UsedImplicitly]
-    public sealed class EpsilonEventRule : StationEventSystem<EpsilonEventRuleComponent>
+[UsedImplicitly]
+public sealed class EpsilonEventRule : StationEventSystem<EpsilonEventRuleComponent>
+{
+    [Dependency] private readonly ApcSystem _apcSystem = default!;
+    [Dependency] private readonly AlertLevelSystem _alertLevelSystem = default!;
+    [Dependency] private readonly ServerGlobalSoundSystem _sound = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    protected override void Started(EntityUid uid, EpsilonEventRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
-        [Dependency] private readonly ApcSystem _apcSystem = default!;
-        [Dependency] private readonly AlertLevelSystem _alertLevelSystem = default!;
+        base.Started(uid, component, gameRule, args);
 
-        protected override void Started(EntityUid uid, EpsilonEventRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+        // This should be checked before playing the sound
+        if (!TryGetRandomStation(out var chosenStation))
+            return;
+        component.AffectedStation = chosenStation.Value;
+
+        //Plays the power off sound to the station.
+        _sound.PlayGlobalOnStation(component.AffectedStation, _audio.ResolveSound(component.PowerOffSound));
+
+        var query = AllEntityQuery<ApcComponent, TransformComponent>();
+        while (query.MoveNext(out var apcUid, out var apc, out var transform))
         {
-            base.Started(uid, component, gameRule, args);
 
-            component.AnnounceCancelToken?.Cancel();
-            component.AnnounceCancelToken = new CancellationTokenSource();
-            Timer.Spawn(10, () =>
+            if (CompOrNull<StationMemberComponent>(transform.GridUid)?.Station != chosenStation)
+                continue;
+
+            //Turns off APCs during the event. Basic power-outage event.
+            if (apc.MainBreakerEnabled)
             {
-                Audio.PlayGlobal(component.PowerOffSound, Filter.Broadcast(), true);
-            }, component.AnnounceCancelToken.Token);
-
-            if (!TryGetRandomStation(out var chosenStation))
-                return;
-
-            component.AffectedStation = chosenStation.Value;
-
-            var query = AllEntityQuery<ApcComponent, TransformComponent>();
-            while (query.MoveNext(out var apcUid, out var apc, out var transform))
-            {
-                // Toggle all APCs on the station off, store for later
-                if (apc.MainBreakerEnabled && CompOrNull<StationMemberComponent>(transform.GridUid)?.Station == chosenStation)
-                {
-                    _apcSystem.ApcToggleBreaker(apcUid, apc);
-                    component.ToggledAPCs.Add((apcUid, apc));
-                }
+                _apcSystem.ApcToggleBreaker(apcUid, apc);
+                component.ToggledAPCs.Add((apcUid, apc));
             }
-        }
-
-        protected override void Ended(EntityUid uid, EpsilonEventRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
-        {
-            if (TryComp(component.AffectedStation, out AlertLevelComponent? _))
-            {
-                _alertLevelSystem.SetLevel(component.AffectedStation, "epsilon", true, true, true);
-            }
-
-            // Toggle all disabled APCs back on
-            foreach (var apc in component.ToggledAPCs)
-            {
-                if (!Deleted(apc) && !apc.Comp.MainBreakerEnabled)
-                    _apcSystem.ApcToggleBreaker(apc, apc);
-            }
-            component.ToggledAPCs.Clear();
         }
     }
+
+    protected override void Ended(EntityUid uid, EpsilonEventRuleComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
+    {
+        base.Ended(uid, component, gameRule, args);
+
+        // Checks if station exists.
+        if (!Exists(component.AffectedStation))
+            return;
+
+        // Restores the power to the station.
+        foreach (var apc in component.ToggledAPCs)
+        {
+            if (Deleted(apc))
+                continue;
+
+            if (!apc.Comp.MainBreakerEnabled)
+                _apcSystem.ApcToggleBreaker(apc, apc);
+        }
+        component.ToggledAPCs.Clear();
+
+        // Final effect, sets the code to Epsilon
+        if (!HasComp<AlertLevelComponent>(component.AffectedStation))
+            return;
+
+        _alertLevelSystem.SetLevel(component.AffectedStation, "epsilon", true, true, true);
+    }
+}
 }
