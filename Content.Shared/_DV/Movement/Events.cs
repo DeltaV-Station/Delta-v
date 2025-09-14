e8ca9967579ba7f719061e07836c37ccec35a36c
kotobdev <59124164+kotobdev@users.noreply.github.com>
2025-09-13 19:05:30 -0700

    Revert "almost 50% functional"
    This reverts commit 0b26d11779b650315551645289b4377fa49f3d93.

--- Content.Shared/_DV/Movement/Events.cs
+++
@@ -1,6 +0,0 @@
-using Content.Shared.Actions;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._DV.Movement;
-
-public sealed partial class CursorOffsetActionEvent : InstantActionEvent;
