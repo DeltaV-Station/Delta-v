562a15efb3deb4db44f6559ed3df03a3a34c414b
kotobdev <59124164+kotobdev@users.noreply.github.com>
2025-09-14 23:11:40 -0700

    epic merge fail

---
+++ Content.Shared/_DV/Movement/CursorOffsetActionComponent.cs
@@ -0,0 +1,20 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._DV.Movement;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class CursorOffsetActionComponent : Component
+{
+    [DataField("cursorOffsetActionId",
+        customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>),
+        required: true)]
+    public string? CursorOffsetActionId;
+
+    [DataField("cursorOffsetActionEntity")]
+    public EntityUid? CursorOffsetActionEntity;
+
+    [DataField, AutoNetworkedField]
+    public bool Active = false;
+}
