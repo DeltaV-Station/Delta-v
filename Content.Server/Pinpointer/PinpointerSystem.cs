c92501839203450886b69dfebb389e69154cb0ff
AeraAulin <133451603+AeraAuling@users.noreply.github.com>
2025-10-02 16:46:38 -0700

    remove unused import thing

--- Content.Server/Pinpointer/PinpointerSystem.cs
+++ Content.Server/Pinpointer/PinpointerSystem.cs
@@ -5,7 +5,6 @@ using System.Numerics;
 using Robust.Shared.Utility;
 using Content.Server.Shuttles.Events;
 using Content.Shared.IdentityManagement;
-using Content.Server.Bible.Components; // #IMP
 namespace Content.Server.Pinpointer;
 
 public sealed class PinpointerSystem : SharedPinpointerSystem
