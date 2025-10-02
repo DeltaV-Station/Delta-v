e8827a8213e75316a7d22ac606c4a3e14d4a9761
Kara <mirrorcult@pm.me>
2025-10-02 06:15:00 +0000

    Fix deny/emag animation (EphemeralSpace/ephemeral-space#181)
    Co-signed-by: DisposableCrewmember42 <disposablecrewmember42@proton.me>

--- Content.Client/Doors/DoorSystem.cs
+++ Content.Client/Doors/DoorSystem.cs
@@ -191,6 +191,9 @@ public sealed class DoorSystem : SharedDoorSystem
             case DoorState.Denying:
                 // ES START
                 // AnimationKey -> DenyKey
+                if (_animationSystem.HasRunningAnimation(entity, DoorComponent.DenyKey))
+                    return;
+
                 _animationSystem.Play(entity, (Animation)entity.Comp.DenyingAnimation, DoorComponent.DenyKey);
                 // ES END
 
@@ -198,6 +201,9 @@ public sealed class DoorSystem : SharedDoorSystem
             case DoorState.Emagging:
                 // ES START
                 // AnimationKey -> DenyKey
+                if (_animationSystem.HasRunningAnimation(entity, DoorComponent.EmagKey))
+                    return;
+
                 if (_sprite.TryGetLayer(entity.Owner, DoorVisualLayers.BaseEmagging, out var _, false))
                     _animationSystem.Play(entity, (Animation)entity.Comp.EmaggingAnimation, DoorComponent.EmagKey);
                 // ES END
