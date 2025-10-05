d2342015aea59eebbaaf684b79744a504846507c
mq <113324899+mqole@users.noreply.github.com>
2025-10-06 07:58:46 +1100

    fix edge case typing indicator error (#40708)
    * cleanup rsi error in typingindicatorvisualizersystem
    
    * sprite fix

--- Content.Client/Chat/TypingIndicator/TypingIndicatorVisualizerSystem.cs
+++ Content.Client/Chat/TypingIndicator/TypingIndicatorVisualizerSystem.cs
@@ -45,7 +45,7 @@ public sealed class TypingIndicatorVisualizerSystem : VisualizerSystem<TypingInd
         if (!layerExists)
             layer = SpriteSystem.LayerMapReserve((uid, args.Sprite), TypingIndicatorLayers.Base);
 
-        SpriteSystem.LayerSetRsi((uid, args.Sprite), layer, proto.SpritePath, proto.TypingState); // DeltaV - Combine path and state calls to avoid missing state error on alternate indicators
+        SpriteSystem.LayerSetRsi((uid, args.Sprite), layer, proto.SpritePath, proto.TypingState);
         args.Sprite.LayerSetShader(layer, proto.Shader);
         SpriteSystem.LayerSetOffset((uid, args.Sprite), layer, proto.Offset);
 
