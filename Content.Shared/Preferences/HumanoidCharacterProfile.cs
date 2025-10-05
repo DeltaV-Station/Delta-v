6a5c0d2befd5dc0861a2433ba0d84ea92e444609
Absotively <jen@jenpollock.ca>
2025-10-05 14:53:58 -0600

    Fix species default skin tones (#40707)
    fix so that all default Urist entities don't spawn with the same default human skin tone (cursed skin tone Urist McSlime)
    
    (cherry picked from commit 8d9564c23856c342bc3a35834c462e659f5ebd5b)
    
    Co-authored-by: Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -234,6 +234,7 @@ namespace Content.Shared.Preferences
             return new()
             {
                 Species = species,
+                Appearance = HumanoidCharacterAppearance.DefaultWithSpecies(species),
             };
         }
 
