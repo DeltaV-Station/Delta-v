24be79d4ce4059a1c43731c809b1bd43c4c4050b
Vapetastic-Gaming <76235372+Vapetastic-Gaming@users.noreply.github.com>
2025-10-18 11:17:55 -0400

    Changes requested

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -79,7 +79,7 @@ namespace Content.IntegrationTests.Tests
             "Chibi", // DeltaV
             "Elegance", // DeltaV
             "Division", // DeltaV
-            "ArenaMedieval", //DeltaV
+            "ArenaMedieval" //DeltaV
             "Submarine", //DeltaV
         };
 
