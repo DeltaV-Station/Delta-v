97c2fd5edab7d9d53307854c6bbe4b28595ab7f6
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-10-19 19:20:34 +0200

    Add missing client side remote control system

---
+++ Content.Client/_DV/RemoteControl/EntitySystems/RemoteControlSystem.cs
@@ -0,0 +1,8 @@
+using Content.Shared._DV.RemoteControl.EntitySystems;
+
+namespace Content.Client._DV.RemoteControl.EntitySystems;
+
+/// <summary>
+/// Client side handling for remote controls.
+/// </summary>
+public sealed class RemoteControlSystem : SharedRemoteControlSystem;
