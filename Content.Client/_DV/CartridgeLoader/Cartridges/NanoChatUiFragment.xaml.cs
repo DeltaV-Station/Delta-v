bd2245eee374eb398607007d44995cbfbb48c3e9
funkystationbot <funky@funkystation.org>
2025-10-31 16:34:20 +0000

    chore: Automatically update REUSE headers
    (cherry picked from commit 84d2cc3a1f31878e5d18ebde63928d63f3b4dd95)

--- Content.Client/_DV/CartridgeLoader/Cartridges/NanoChatUiFragment.xaml.cs
+++ Content.Client/_DV/CartridgeLoader/Cartridges/NanoChatUiFragment.xaml.cs
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: 2024 Skubman <ba.fallaria@gmail.com>
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Evaisa <evagiacosa1@gmail.com>
+// SPDX-FileCopyrightText: 2025 EvaisaDev <mail@evaisa.dev>
 // SPDX-FileCopyrightText: 2025 Icepick <122653407+Icepicked@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025 EvaisaDev <mail@evaisa.dev>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 
