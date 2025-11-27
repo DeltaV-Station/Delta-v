b04c33ba5dae2a0f49969151d1fe8d21ca2d9d07
pootslap <211648338+pootslap@users.noreply.github.com>
2025-11-27 16:27:33 +0000

    Update SpeciesPrototype.cs

--- Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
@@ -108,7 +108,7 @@ public sealed partial class SpeciesPrototype : IPrototype
     ///     Characters younger than this appear young.
     /// </summary>
     [DataField]
-    public int YoungAge = 40;
+    public int YoungAge = 39;
 
     /// <summary>
     ///     Characters older than this appear old. Characters in between young and old age appear middle aged.
