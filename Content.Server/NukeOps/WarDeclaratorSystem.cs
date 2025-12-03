7af9e9c465373657800f75458b49dafcaff03076
kotobdev <59124164+kotobdev@users.noreply.github.com>
2025-12-02 18:03:56 -0800

    evil stationcode

--- Content.Server/NukeOps/WarDeclaratorSystem.cs
+++ Content.Server/NukeOps/WarDeclaratorSystem.cs
@@ -44,10 +44,10 @@ public sealed class WarDeclaratorSystem : EntitySystem
     {
         // Begin DeltaV - fancy default message
         var issuer = _random.Pick(_prototypeManager.Index(ent.Comp.WarIssuers).Values);
-        var stationName = _station.GetStationNames()[0].Name;
+        var stationNames = _station.GetStationNames();
         var presetMessage = Loc.GetString("nuke-ops-war-declaration-message",
             ("company", Loc.GetString(issuer)),
-            ("station", stationName));
+            ("station", stationNames.Count > 0 ? stationNames[0].Name : Loc.GetString("nuke-ops-war-declaration-default-station")));
         ent.Comp.Message = presetMessage;
         // End DeltaV - fancy default message
 
--- Resources/Locale/en-US/_DV/nukie-operations/declaration.ftl
+++ Resources/Locale/en-US/_DV/nukie-operations/declaration.ftl
@@ -1,1 +1,2 @@
 nuke-ops-war-declaration-message = The Gorlex Marauders, on behalf of {$company}, have declared war on {$station}.
+nuke-ops-war-declaration-default-station = the station
