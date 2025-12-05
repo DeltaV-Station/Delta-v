24509b078de5677b5127ac65b9457ab7911ced67
Charlie Morley <cmorley191@gmail.com>
2025-12-04 17:03:55 -0700

    prevent anomalies from spawning duplicate entities or tiles on a tile

--- Content.Shared/Anomaly/SharedAnomalySystem.cs
+++ Content.Shared/Anomaly/SharedAnomalySystem.cs
@@ -425,6 +425,9 @@ public abstract class SharedAnomalySystem : EntitySystem
 
             if (!settings.CanSpawnOnEntities)
             {
+                // If it can't spawn on entities, ensure that maximum one entity will be spawned here this pulse.
+                tilerefs.Remove(tileref);
+
                 var valid = true;
                 foreach (var ent in _map.GetAnchoredEntities(xform.GridUid.Value, grid, tileref.GridIndices))
                 {
@@ -441,7 +444,6 @@ public abstract class SharedAnomalySystem : EntitySystem
                 }
                 if (!valid)
                 {
-                    tilerefs.Remove(tileref);
                     continue;
                 }
             }
