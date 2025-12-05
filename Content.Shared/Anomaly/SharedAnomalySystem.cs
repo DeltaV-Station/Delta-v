ecd16eeb97f35734bbbb1801a05cebb268d487f2
Charlie Morley <cmorley191@gmail.com>
2025-12-04 17:13:44 -0700

    deltav comments

--- Content.Shared/Anomaly/SharedAnomalySystem.cs
+++ Content.Shared/Anomaly/SharedAnomalySystem.cs
@@ -425,8 +425,10 @@ public abstract class SharedAnomalySystem : EntitySystem
 
             if (!settings.CanSpawnOnEntities)
             {
+                // DeltaV - start of duplicate spawn fix (borrowed from upstream #37833)
                 // If it can't spawn on entities, ensure that maximum one entity will be spawned here this pulse.
                 tilerefs.Remove(tileref);
+                // DeltaV - end of duplicate spawn fix (borrowed from upstream #37833)
 
                 var valid = true;
                 foreach (var ent in _map.GetAnchoredEntities(xform.GridUid.Value, grid, tileref.GridIndices))
@@ -444,6 +446,7 @@ public abstract class SharedAnomalySystem : EntitySystem
                 }
                 if (!valid)
                 {
+                    // DeltaV - duplicate spawn fix removed: tilerefs.Remove(tileref);
                     continue;
                 }
             }
