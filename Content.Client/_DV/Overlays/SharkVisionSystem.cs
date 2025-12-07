564eff8522457672580894892500a3ca2ac2b900
Vanessa <vanessalouwagie@gmail.com>
2025-12-07 07:09:35 -0600

    More clean-up

--- Content.Client/_DV/Overlays/SharkVisionSystem.cs
+++ Content.Client/_DV/Overlays/SharkVisionSystem.cs
@@ -1,13 +1,4 @@
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
-// SPDX-FileCopyrightText: 2025 Spatison <137375981+Spatison@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using Content.Client.Overlays;
-using Content.Shared._DV.Overlays;
 using Content.Shared._DV.Overlays.Components;
 using Content.Shared._Goobstation.Overlays;
 using Content.Shared.Inventory;
