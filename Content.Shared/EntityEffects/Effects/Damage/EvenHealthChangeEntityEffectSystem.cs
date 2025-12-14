29aceb4e03825595b006acf177fa798ddc050da4
Samuka <47865393+Samuka-C@users.noreply.github.com>
2025-12-14 19:31:15 -0300

    Move logic from EvenHealthChangeEntityEffectSystem to the damage system API (#41684)
    * add two methods
    
    * move stuff to damage system api
    
    * use TryIndex
    
    * simplify
    
    * minor fix
    
    * add helper functions
    
    * fix
    
    * remove random new line
    
    * simplify
    
    * remove unnecessary lines
    
    * rename to GetDamage
    
    * Got it working
    
    * make more clear
    
    * why backwards
    
    * value should be the amount to heal
    
    * fix
    
    * fix all dumb fixedpoint2 edge cases I hope
    
    * One more thing
    
    * fix
    
    * make it more simple
    
    * ops it was backwards
    
    * valueHeal can't be more than remaining
    
    * add all keys beforehand and no need to check and add them inside the loop
    
    * break for loop in case remaining is zero
    
    * comment was wrong
    
    * optimized, works
    
    * remove random spaces
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Shared/EntityEffects/Effects/Damage/EvenHealthChangeEntityEffectSystem.cs
+++ Content.Shared/EntityEffects/Effects/Damage/EvenHealthChangeEntityEffectSystem.cs
@@ -1,102 +1,48 @@
-﻿// DeltaV Start - Fix EvenHealing with Limbs.
-using System.Linq;
+// DeltaV Start - Fix EvenHealing with Limbs.
 using Content.Shared._Shitmed.Targeting;
 using Content.Shared.Body.Systems;
 // DeltaV End - Fix EvenHealing with Limbs.
-using Content.Shared.Damage;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.Damage.Systems;
 using Content.Shared.FixedPoint;
 using Content.Shared.Localizations;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Utility;
 
-namespace Content.Shared.EntityEffects.Effects;
+namespace Content.Shared.EntityEffects.Effects.Damage;
 
 /// <summary>
-/// Evenly adjust the damage types in a damage group by up to a specified total on this entity.
+/// Evenly heal the damage types in a damage group by up to a specified total on this entity.
 /// Total adjustment is modified by scale.
 /// </summary>
 /// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
 public sealed partial class EvenHealthChangeEntityEffectSystem : EntityEffectSystem<DamageableComponent, EvenHealthChange>
 {
     [Dependency] private readonly DamageableSystem _damageable = default!;
-    [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly SharedBodySystem _body = default!; // DeltaV
-    [Dependency] private readonly EntityManager _ent = default!; // DeltaV
 
     protected override void Effect(Entity<DamageableComponent> entity, ref EntityEffectEvent<EvenHealthChange> args)
     {
-        // DeltaV - Even Healing with Limbs
-        var damageSpec = GetDamageSpec(entity.Owner, ref args); // DeltaV - Basically moved this to a private method
-
-        damageSpec *= args.Scale;
-
-
-        _damageable.TryChangeDamage(
-            entity.AsNullable(),
-            damageSpec,
-            args.Effect.IgnoreResistances,
-            interruptsDoAfters: false,
-            doPartDamage: false); // DeltaV - Even Healing with Limbs
-
-        var bodyParts = SharedTargetingSystem.GetValidParts();
-        foreach (var bodyPart in bodyParts)
-        {
-            var (targetType, targetSymmetry) = _body.ConvertTargetBodyPart(bodyPart);
-            if (_body.GetBodyChildrenOfType(entity, targetType, symmetry: targetSymmetry) is { } part)
-            {
-                var dspec = GetDamageSpec(part.FirstOrDefault().Id, ref args);
-
-                if (dspec.GetTotal() == 0)
-                    continue;
-
-                _damageable.TryChangeDamage(
-                    entity.AsNullable(),
-                    dspec * args.Scale,
-                    args.Effect.IgnoreResistances,
-                    interruptsDoAfters: false,
-                    targetPart: bodyPart,
-                    onlyDamageParts: true,
-                    canSever: false);
-            }
-        }
-        // END DeltaV
-    }
-
-    /// <summary>
-    /// DeltaV - Returns a damage spec for a specific entity with DamageableComponent.
-    /// </summary>
-    /// <param name="entity"></param>
-    /// <param name="args"></param>
-    /// <returns></returns>
-    private DamageSpecifier GetDamageSpec(Entity<DamageableComponent?> entity, ref EntityEffectEvent<EvenHealthChange> args)
-    {
-        var damageSpec = new DamageSpecifier();
-        if (!_ent.TryGetComponent<DamageableComponent>(entity, out var damageable))
-            return damageSpec;
-
         foreach (var (group, amount) in args.Effect.Damage)
         {
-            var groupProto = _proto.Index(group);
-            var groupDamage = new Dictionary<string, FixedPoint2>();
-            foreach (var damageId in groupProto.DamageTypes)
-            {
-                var damageAmount = damageable.Damage.DamageDict.GetValueOrDefault(damageId);
-                if (damageAmount != FixedPoint2.Zero)
-                    groupDamage.Add(damageId, damageAmount);
-            }
+            // Begin DeltaV Additions - Limb even healing
+            _damageable.HealEvenly(entity.AsNullable(), amount * args.Scale, group, doPartDamage: false);
 
-            var sum = groupDamage.Values.Sum();
-            foreach (var (damageId, damageAmount) in groupDamage)
+            var bodyParts = SharedTargetingSystem.GetValidParts();
+            foreach (var bodyPart in bodyParts)
             {
-                var existing = damageSpec.DamageDict.GetOrNew(damageId);
-                damageSpec.DamageDict[damageId] = existing + damageAmount / sum * amount;
+                var (targetType, targetSymmetry) = _body.ConvertTargetBodyPart(bodyPart);
+                if (_body.GetBodyChildrenOfType(entity, targetType, symmetry: targetSymmetry) is { } part)
+                {
+                    _damageable.HealEvenly(
+                        entity.AsNullable(),
+                        amount * args.Scale,
+                        targetPart: bodyPart,
+                        onlyDamageParts: true);
+                }
             }
+            // End DeltaV Additions - Limb even healing
         }
-
-        return damageSpec;
     }
 }
 
