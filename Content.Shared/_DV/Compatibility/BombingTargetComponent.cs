c345039934e459049dca00c237ee44e6d69d6f53
Vanessa <vanessalouwagie@gmail.com>
2025-12-25 14:51:31 -0600

    Added a dummy component for BombingTarget for mapping compatibility.

---
+++ Content.Shared/_DV/Compatibility/BombingTargetComponent.cs
@@ -0,0 +1,5 @@
+namespace Content.Shared._DV.Compatibility;
+
+[Obsolete("Do not use. See SpiderChargeSystem on how to handle bombing targets. This will be removed in the future.")]
+[RegisterComponent]
+public sealed partial class BombingTargetComponent : Component;
