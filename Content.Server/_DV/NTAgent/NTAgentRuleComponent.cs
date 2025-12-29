8387196e5c2d014cc8646b5ac098a5421a6b8199
Cepelinas1 <kakelis01@gmail.com>
2025-12-29 19:35:19 +0200

    test for now

---
+++ Content.Server/_DV/NTAgent/NTAgentRuleComponent.cs
@@ -0,0 +1,8 @@
+using Content.Shared.Random;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._DV.NTAgent;
+
+[RegisterComponent, Access(typeof(NTAgentRuleSystem))]
+public sealed partial class NTAgentRuleComponent : Component;
---
+++ Content.Server/_DV/NTAgent/preset-NTAgent.ftl
@@ -0,0 +1,5 @@
+NTAgent-role-greeting-human =
+    You're an Internals Agent for NT.
+    Perform tasks delivered by your handler.
+    Try to minimize damage to crew and property.
+    If you're caught, Central Commmand will deny everything.
