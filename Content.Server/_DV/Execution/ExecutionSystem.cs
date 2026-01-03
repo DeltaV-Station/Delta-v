4f3c50e0ac9d6f34efa2e0149a753902e42709d7
BarryNorfolk <barrynorfolkman@protonmail.com>
2026-01-03 16:28:02 +0100

    Remove unused dependencies [CS0414]

--- Content.Server/_DV/Execution/ExecutionSystem.cs
+++ Content.Server/_DV/Execution/ExecutionSystem.cs
@@ -40,7 +40,6 @@ public sealed class ExecutionSystem : EntitySystem
     [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
-    [Dependency] private readonly InteractionSystem _interactionSystem = default!;
     [Dependency] private readonly ActionBlockerSystem _actionBlockerSystem = default!;
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
