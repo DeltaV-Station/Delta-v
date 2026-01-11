eb2fdc8d4c04d62a59eb811746266851ff82e815
faintspeaker <joe@faintspeaker.com>
2026-01-11 17:59:29 -0500

    fix:  i didnt make these entity effects correctly

--- Content.Server/_DV/EntityEffects/Effects/Silicon/IonStormEffectSystem.cs
+++ Content.Server/_DV/EntityEffects/Effects/Silicon/IonStormEffectSystem.cs
@@ -1,28 +1,18 @@
 ﻿using Content.Server.Silicons.Laws;
+using Content.Shared._DV.EntityEffects.Effects.Silicon;
 using Content.Shared.EntityEffects;
 using Content.Shared.Silicons.Laws.Components;
-using Content.Shared.StatusEffect;
-using Robust.Shared.Prototypes;
 namespace Content.Server._DV.EntityEffects.Effects.Silicon;
 
-public sealed class IonStormEffectSystem : EntityEffectSystem<StatusEffectsComponent, IonStorm>
+public sealed class IonStormEffectSystem : EntityEffectSystem<IonStormTargetComponent, IonStorm>
 {
     [Dependency] private readonly IonStormSystem _ionStorm = default!;
 
-    protected override void Effect(Entity<StatusEffectsComponent> entity, ref EntityEffectEvent<IonStorm> args)
+    protected override void Effect(Entity<IonStormTargetComponent> entity, ref EntityEffectEvent<IonStorm> args)
     {
         if (!TryComp<SiliconLawBoundComponent>(entity, out var laws))
             return;
         
-        if (!TryComp<IonStormTargetComponent>(entity, out var target))
-            return;
-        
-        _ionStorm.IonStormTarget((entity.Owner, laws, target));
+        _ionStorm.IonStormTarget((entity.Owner, laws, entity.Comp));
     }
-}
-
-public sealed partial class IonStorm : EntityEffectBase<IonStorm>
-{
-    public override string? EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => Loc.GetString("entity-effect-guidebook-ion-storm", ("chance", Probability));
 }
--- Content.Server/_DV/EntityEffects/Effects/Thaven/ThavenRefreshMoodsEffectSystem.cs
+++ Content.Server/_DV/EntityEffects/Effects/Thaven/ThavenRefreshMoodsEffectSystem.cs
@@ -1,26 +1,16 @@
 ﻿using Content.Server._Impstation.Thaven;
+using Content.Shared._DV.EntityEffects.Effects.Thaven;
 using Content.Shared._Impstation.Thaven.Components;
 using Content.Shared.EntityEffects;
-using Content.Shared.StatusEffect;
-using Robust.Shared.Prototypes;
 
 namespace Content.Server._DV.EntityEffects.Effects.Thaven;
 
-public sealed partial class ThavenRefreshMoodsEffectSystem : EntityEffectSystem<StatusEffectsComponent, ThavenRefreshMoods>
+public sealed partial class ThavenRefreshMoodsEffectSystem : EntityEffectSystem<ThavenMoodsComponent, ThavenRefreshMoods>
 {
     [Dependency] private readonly ThavenMoodsSystem _moods = default!;
 
-    protected override void Effect(Entity<StatusEffectsComponent> entity, ref EntityEffectEvent<ThavenRefreshMoods> args)
+    protected override void Effect(Entity<ThavenMoodsComponent> entity, ref EntityEffectEvent<ThavenRefreshMoods> args)
     {
-        if (!TryComp<ThavenMoodsComponent>(entity, out var moods))
-            return;
-        
-        _moods.RefreshMoods(entity, moods);
+        _moods.RefreshMoods(entity, entity.Comp);
     }
-}
-
-public sealed partial class ThavenRefreshMoods : EntityEffectBase<ThavenRefreshMoods>
-{
-    public override string? EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys) =>
-        Loc.GetString("entity-effect-guidebook-thaven-refresh-moods", ("chance", Probability));
 }
