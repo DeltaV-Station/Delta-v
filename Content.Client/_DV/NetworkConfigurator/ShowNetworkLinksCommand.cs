69230b8dc058c8c4e228fc609b0ee50df2f427fa
BarryNorfolk <barrynorfolkman@protonmail.com>
2026-01-12 18:51:38 +0100

    Localise description

--- Content.Client/_DV/NetworkConfigurator/ShowNetworkLinksCommand.cs
+++ Content.Client/_DV/NetworkConfigurator/ShowNetworkLinksCommand.cs
@@ -8,7 +8,7 @@ public sealed class ToggleNetworkLinksCommand : LocalizedEntityCommands
     [Dependency] private readonly IOverlayManager _overlay = default!;
 
     public override string Command => "togglenetworklinks";
-    public override string Description => "Toggles seeing network configurator links.";
+    public override string Description => Loc.GetString("cmd-togglenetworklinks-desc");
 
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
