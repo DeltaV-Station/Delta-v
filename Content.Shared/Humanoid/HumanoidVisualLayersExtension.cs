243dd9d831e14854dfe7ee33ec12fe13e5735cf9
pathetic meowmeow <uhhadd@gmail.com>
2026-01-16 19:39:35 -0500

    Nubody (#42419)
    * Nubody
    
    * fix test fails
    
    * gibbing
    
    * lung test returns
    
    * doc comment
    
    * hand organ test
    
    * giblet test
    
    * yaml formatting
    
    * returning
    
    * relocate
    
    * trimming
    
    * re-smite
    
    * oops thusd tweak
    
    * arachnids have slower metabolism i guess
    
    * never mind the old behaviour is bad actually
    
    * rider whyyy
    
    * style changes and allat
    
    * fix collision
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Shared/Humanoid/HumanoidVisualLayersExtension.cs
+++ Content.Shared/Humanoid/HumanoidVisualLayersExtension.cs
@@ -1,6 +1,3 @@
-using Content.Shared.Body.Components;
-using Content.Shared.Body.Part;
-
 namespace Content.Shared.Humanoid
 {
     public static class HumanoidVisualLayersExtension
@@ -68,97 +65,9 @@ namespace Content.Shared.Humanoid
                     yield return HumanoidVisualLayers.Chest;
                     yield return HumanoidVisualLayers.Tail;
                     break;
-                // Shitmed Change Start
-                case HumanoidVisualLayers.LHand:
-                    yield return HumanoidVisualLayers.LHand;
-                    break;
-                case HumanoidVisualLayers.RHand:
-                    yield return HumanoidVisualLayers.RHand;
-                    break;
-                case HumanoidVisualLayers.LFoot:
-                    yield return HumanoidVisualLayers.LFoot;
-                    break;
-                case HumanoidVisualLayers.RFoot:
-                    yield return HumanoidVisualLayers.RFoot;
-                    break;
-                // Shitmed Change End
                 default:
                     yield break;
             }
         }
-
-        // Begin DeltaV - We want this data w/o needing a BodyPartComponnet
-        public static HumanoidVisualLayers? ToHumanoidLayers(this BodyPartComponent part)
-        {
-            return part.PartType.ToHumanoidLayers(part.Symmetry);
-        }
-
-        public static HumanoidVisualLayers? ToHumanoidLayers(this BodyPartType partType, BodyPartSymmetry partSymmetry)
-        {
-            switch (partType)
-            {
-                case BodyPartType.Other:
-                    break;
-                case BodyPartType.Torso:
-                    return HumanoidVisualLayers.Chest;
-                case BodyPartType.Tail:
-                    return HumanoidVisualLayers.Tail;
-                case BodyPartType.Head:
-                    // use the Sublayers method to hide the rest of the parts,
-                    // if that's what you're looking for
-                    return HumanoidVisualLayers.Head;
-                case BodyPartType.Arm:
-                    switch (partSymmetry)
-                    {
-                        case BodyPartSymmetry.None:
-                            break;
-                        case BodyPartSymmetry.Left:
-                            return HumanoidVisualLayers.LArm;
-                        case BodyPartSymmetry.Right:
-                            return HumanoidVisualLayers.RArm;
-                    }
-
-                    break;
-                case BodyPartType.Hand:
-                    switch (partSymmetry)
-                    {
-                        case BodyPartSymmetry.None:
-                            break;
-                        case BodyPartSymmetry.Left:
-                            return HumanoidVisualLayers.LHand;
-                        case BodyPartSymmetry.Right:
-                            return HumanoidVisualLayers.RHand;
-                    }
-
-                    break;
-                case BodyPartType.Leg:
-                    switch (partSymmetry)
-                    {
-                        case BodyPartSymmetry.None:
-                            break;
-                        case BodyPartSymmetry.Left:
-                            return HumanoidVisualLayers.LLeg;
-                        case BodyPartSymmetry.Right:
-                            return HumanoidVisualLayers.RLeg;
-                    }
-
-                    break;
-                case BodyPartType.Foot:
-                    switch (partSymmetry)
-                    {
-                        case BodyPartSymmetry.None:
-                            break;
-                        case BodyPartSymmetry.Left:
-                            return HumanoidVisualLayers.LFoot;
-                        case BodyPartSymmetry.Right:
-                            return HumanoidVisualLayers.RFoot;
-                    }
-
-                    break;
-            }
-
-            return null;
-        }
-        // End DeltaV - We want this data w/o needing a BodyPartComponnet
     }
 }
