26db1e8f36d3d51884afdf2daaa5fd42a03deaee
Vanessa <908648+ShepardToTheStars@users.noreply.github.com>
2026-01-30 15:11:55 -0600

    Health Analyzer Uncloneable Refactor (#5330)
    * Partial revert of #5310
    
    * Refactored uncloneable out of the HealthAnalyzerScannedUserMessage BUI.

--- Content.Server/Medical/CryoPodSystem.cs
+++ Content.Server/Medical/CryoPodSystem.cs
@@ -60,7 +60,6 @@ public sealed partial class CryoPodSystem : SharedCryoPodSystem
             null,
             null,
             null,
-            null, // DeltaV - Uncloneable
             null // Shitmed Change
         ));
     }
--- Content.Shared/_DV/Medical/UncloneableSystem.cs
+++ Content.Shared/_DV/Medical/UncloneableSystem.cs
@@ -1,10 +1,20 @@
 using Content.Shared._DV.Traits.Assorted;
 using Content.Shared.Cloning.Events;
+using JetBrains.Annotations;
 
 namespace Content.Shared._DV.Medical;
 
 public sealed class UncloneableSystem : EntitySystem
 {
+    [PublicAPI]
+    public bool IsUncloneable(Entity<UncloneableComponent?> entity)
+    {
+        if (!Resolve(entity, ref entity.Comp, false))
+            return false;
+
+        return true;
+    }
+
     public override void Initialize()
     {
         base.Initialize();
