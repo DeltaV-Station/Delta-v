980f1f125866f34186a1155379d978a0199d1b6c
pathetic meowmeow <uhhadd@gmail.com>
2026-02-02 22:13:26 -0500

    Clean up Marking data structure, add tests for Zombie transformation (#42756)
    * Clean up Marking data structure, add tests for Zombie transformation
    
    * empty
    
    * AAAAAAAAAAAAAAAA
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Server/Database/ServerDbBase.cs
+++ Content.Server/Database/ServerDbBase.cs
@@ -235,7 +235,7 @@ namespace Content.Server.Database
             var legacyMarkings = appearance.Markings
                 .SelectMany(organ => organ.Value.Values)
                 .SelectMany(i => i)
-                .Select(marking => marking.ToString())
+                .Select(marking => marking.ToLegacyDbString())
                 .ToList();
             var flattenedMarkings = appearance.Markings.SelectMany(it => it.Value)
                 .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
