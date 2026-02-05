aa8442032541f21db0dcd545dc39b92eab69e00b
William Lemon <William.Lemon2@gmail.com>
2026-02-06 09:01:40 +1100

    Modified CVar to be a single variable, fixed some errors

--- Content.Shared/_DV/Species/SpeciesHiderSystem.cs
+++ Content.Shared/_DV/Species/SpeciesHiderSystem.cs
@@ -1,34 +1,26 @@
-using Content.Shared.Humanoid.Prototypes;
+using Content.Shared._DV.CCVars;
 using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
+using System.Linq;
 
 namespace Content.Shared._DV.Species;
 
 public sealed partial class SpeciesHiderSystem : EntitySystem
 {
     [Dependency] private readonly IConfigurationManager _cfg = default!;
-    [Dependency] private readonly IPrototypeManager _proto = default!;
 
-    private static Dictionary<string, bool>? _hiddenSpecies;
+    private static HashSet<string>? _hiddenSpecies = [];
 
     public override void Initialize()
     {
         base.Initialize();
-        if (_hiddenSpecies != null) return; // Prevent duplicate initialization, as this is a static list.
-        _hiddenSpecies = [];
-        foreach (var species in _proto.EnumeratePrototypes<SpeciesPrototype>())
-        {
-            Log.Info($"Creating CVAR: species.hide.{species.ID}");
-            var cvar = CVarDef.Create($"species.hide.{species.ID}", species.DefaultHidden, CVar.SERVER | CVar.REPLICATED);
-            _cfg.RegisterCVar(cvar.Name, species.DefaultHidden, CVar.SERVER | CVar.REPLICATED);
-            Subs.CVar(_cfg, cvar, val => _hiddenSpecies[species.ID] = val, true);
-        }
+        Subs.CVar(_cfg, DCCVars.HiddenSpecies, val => _hiddenSpecies = new(val.Split(",").Select(c => c.Trim())), true);
     }
 
     public static bool IsHidden(string speciesId)
     {
         // If _hiddenSpecies is somehow not yet initialized, probably an invalid state.
         if (_hiddenSpecies is null) return false;
-        return _hiddenSpecies.ContainsKey(speciesId) && _hiddenSpecies[speciesId];
+        return _hiddenSpecies.Contains(speciesId);
     }
 }
