23fe945a4849c4e0406a64536babf8073ac0de8e
Pharaz4 <143268074+Pharaz4@users.noreply.github.com>
2026-02-19 01:40:04 +1100

    engi, tesla, and trash tweaks

--- Content.Shared/Materials/OreSilo/OreSiloComponent.cs
+++ Content.Shared/Materials/OreSilo/OreSiloComponent.cs
@@ -23,7 +23,7 @@ public sealed partial class OreSiloComponent : Component
     /// Default value should be big enough to span a single large department.
     /// </remarks>
     [DataField, AutoNetworkedField]
-    public float Range = 20f;
+    public float Range = 10f; // Delta V - Reduced to 10
 }
 
 [Serializable, NetSerializable]
