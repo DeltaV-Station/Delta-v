26576176dee5bc991bc0d85a5c730189f6f3aa6c
DisposableCrewmember42 <disposablecrewmember42@proton.me>
2026-03-12 22:39:45 +0100

    docs: add summary to puddlefootprintscomp

--- Content.Server/_DV/Footprints/Components/PuddleFootPrintsComponent.cs
+++ Content.Server/_DV/Footprints/Components/PuddleFootPrintsComponent.cs
@@ -1,5 +1,10 @@
 namespace Content.Server._DV.Footprints.Components;
 
+/// <summary>
+/// Component added to entities with PuddleComponent to enable entities leaving footprints after colliding with it.
+/// System requires the following other components: AppearanceComponent, PuddleComponent, SolutionContainerManagerComponent
+/// Colliding entity requires the FootPrintsComponent
+/// </summary>
 [RegisterComponent]
 public sealed partial class PuddleFootPrintsComponent : Component
 {
