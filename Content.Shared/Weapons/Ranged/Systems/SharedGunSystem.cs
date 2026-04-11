4ecb9222d139d7c30ea9ade8a5adde017b7e83a1
DisposableCrewmember42 <disposablecrewmember42@proton.me>
2026-04-11 21:58:17 +0200

    fix: upmerge compatibility

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -429,7 +429,7 @@ public abstract partial class SharedGunSystem : EntitySystem
 
         // ES START
         // this is a suspicious place to do this but whatever.
-        var gunShakeRotation = new ESScreenshakeParameters() { Trauma = 0.085f * gun.CameraRecoilScalarModified, DecayRate = 1.2f, Frequency = 0.008f};
+        var gunShakeRotation = new ESScreenshakeParameters() { Trauma = 0.085f * gun.Comp.CameraRecoilScalarModified, DecayRate = 1.2f, Frequency = 0.008f};
         _shake.Screenshake(user, null, gunShakeRotation);
         // ES END
 
