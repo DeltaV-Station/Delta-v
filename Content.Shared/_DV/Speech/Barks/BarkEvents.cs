9057e9660238d9dca198f3c50bad9b8fbfdef7a1
uwuKemonoChan <woodroyalty@gmail.com>
2026-04-16 14:04:50 -0500

    Add BarkEvents.cs
    Network/client events for messenger communication

---
+++ Content.Shared/_DV/Speech/Barks/BarkEvents.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._DV.Speech.Barks;
+
+///<summary>
+/// Server to client indicating a trigger for bark audio playback
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class PlayBarkEvent : EntityEventArgs
+{
+    public NetEntity Speaker;
+    public bool IsWhisper;
+}
+
+///<summary>
+/// Local previews of a Speech Bark voice within the character editor
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class PreviewBarkEvent : EntityEventArgs;
