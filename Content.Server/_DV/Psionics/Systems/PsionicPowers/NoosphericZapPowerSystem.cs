a90eb7395a46fd38339a4af570275fb4c1627f1e
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2026-05-11 22:07:15 +0200

    Rewrite with Events

--- Content.Server/_DV/Psionics/Systems/PsionicPowers/NoosphericZapPowerSystem.cs
+++ Content.Server/_DV/Psionics/Systems/PsionicPowers/NoosphericZapPowerSystem.cs
@@ -2,12 +2,14 @@ using Content.Server.Electrocution;
 using Content.Server.Lightning;
 using Content.Server.Power.EntitySystems;
 using Content.Shared._DV.Psionics.Components.PsionicPowers;
+using Content.Shared._DV.Psionics.Events;
 using Content.Shared._DV.Psionics.Events.PowerActionEvents;
 using Content.Shared._DV.Psionics.Systems.PsionicPowers;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Popups;
 using Content.Shared.Power.Components;
 using Content.Shared.PowerCell;
+using Content.Shared.PowerCell.Components;
 
 namespace Content.Server._DV.Psionics.Systems.PsionicPowers;
 
@@ -18,25 +20,49 @@ public sealed class NoosphericZapPowerSystem : SharedNoosphericZapPowerSystem
     [Dependency] private readonly LightningSystem _lightning = default!;
     [Dependency] private readonly PowerCellSystem _powerCell = default!;
 
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PowerCellSlotComponent, NoosphericallyZappedEvent>(OnBatterySlotZapped);
+        SubscribeLocalEvent<BatteryComponent, NoosphericallyZappedEvent>(OnBatteryZapped);
+    }
+
     protected override void OnPowerUsed(Entity<NoosphericZapPowerComponent> psionic, ref NoosphericZapPowerActionEvent args)
     {
         // As this can target batteries, it doesn't require the target to be psionic.
-        if (!Psionic.CanBeTargeted(args.Target, hasAggressor: args.Performer, ignorePsionicRequirement: true))
-            return;
-        // This checks for IPCS and Borgs too.
-        if (_powerCell.TryGetBatteryFromEntityOrSlot(args.Target, out var battery))
-        {
-            ChargeBattery(battery.Value.AsNullable(), psionic.Comp.AddedBatteryCharge, args.Target);
-            ShootLightning(args.Performer, args.Target, psionic.Comp);
-            AfterPowerUsed(psionic, args.Performer);
-            return;
-        }
-        // If there is no battery, the target needs to be potentially psionic.
-        if (!Psionic.CanBeTargeted(args.Target, showPopup: false))
+        if (!Psionic.CanBeTargeted(args.Target, ignorePsionicRequirement: true, hasAggressor: args.Performer))
             return;
 
-        ShootLightning(args.Performer, args.Target, psionic.Comp);
+        var ev = new NoosphericallyZappedEvent(psionic.Comp.AddedBatteryCharge, args.Performer);
+        RaiseLocalEvent(args.Target, ref ev);
+
+        // If they don't have anything else, we check if they're a potential psionic.
+        if (!ev.CanZap && !Psionic.CanBeTargeted(args.Target, hasAggressor: args.Performer))
+            return;
+
+        var message = Loc.GetString("psionic-power-noospheric-zap-user", ("user", Identity.Entity(args.Performer, EntityManager)));
+        Popup.PopupEntity(message, args.Performer, PopupType.LargeCaution);
+
+        _lightning.ShootLightning(args.Performer, args.Target, psionic.Comp.LightningPrototpyeId);
+        _electrocution.TryDoElectrocution(args.Target, args.Performer, psionic.Comp.ShockDamage, psionic.Comp.StunDuration, true);
+
         AfterPowerUsed(psionic, args.Performer);
+    }
+
+    private void OnBatterySlotZapped(Entity<PowerCellSlotComponent> batterySlot, ref NoosphericallyZappedEvent args)
+    {
+        if (!_powerCell.TryGetBatteryFromEntityOrSlot(batterySlot.Owner, out var battery))
+            return;
+
+        ChargeBattery(battery.Value.AsNullable(), args.RechargeAmount, batterySlot);
+        args.CanZap = true;
+    }
+
+    private void OnBatteryZapped(Entity<BatteryComponent> battery, ref NoosphericallyZappedEvent args)
+    {
+        ChargeBattery(battery.AsNullable(), args.RechargeAmount, battery);
+        args.CanZap = true;
     }
 
     private void ChargeBattery(Entity<BatteryComponent?> battery, float amount, EntityUid container)
@@ -44,13 +70,5 @@ public sealed class NoosphericZapPowerSystem : SharedNoosphericZapPowerSystem
         var message = Loc.GetString("psionic-power-noospheric-zap-battery", ("battery", Identity.Entity(container, EntityManager)));
         Popup.PopupEntity(message, battery, PopupType.Medium);
         _battery.ChangeCharge(battery, amount);
-    }
-
-    private void ShootLightning(EntityUid shooter, EntityUid target, NoosphericZapPowerComponent comp)
-    {
-        var message = Loc.GetString("psionic-power-noospheric-zap-user", ("user", Identity.Entity(shooter, EntityManager)));
-        Popup.PopupEntity(message, shooter, PopupType.LargeCaution);
-        _lightning.ShootLightning(shooter, target, comp.LightningPrototpyeId);
-        _electrocution.TryDoElectrocution(target, shooter, comp.ShockDamage, comp.StunDuration, true);
     }
 }
