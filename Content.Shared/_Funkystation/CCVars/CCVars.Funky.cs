2297f0935d644842458052bb606dc21976e496ae
KOTOB <59124164+kotobdev@users.noreply.github.com>
2026-06-02 14:43:12 -0700

    fixes and adjustments

--- Content.Shared/_Funkystation/CCVars/CCVars.Funky.cs
+++ Content.Shared/_Funkystation/CCVars/CCVars.Funky.cs
@@ -9,11 +9,19 @@ namespace Content.Shared._Funkystation.CCVars;
 [CVarDefs]
 public sealed class CCVars_Funky
 {
+    // imp start. Automatically disable content warnings in debug builds. // DeltaV - and tools, too
+#if DEBUG || TOOLS
+    private static bool _cwOnBuildType;
+#else
+    private static bool _cwOnBuildType = true;
+#endif
+    // imp end
+
     /// <summary>
     /// If the content warning should be displayed.
     /// </summary>
     public static readonly CVarDef<bool> ContentWarningDisplay =
-        CVarDef.Create("cw.display", true, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("cw.display", _cwOnBuildType, CVar.SERVER | CVar.REPLICATED); // imp. Automatically disable content warnings in debug builds.
 
     /// <summary>
     /// If ignoring the content warning should kick you from the server.
