60fdfbb19168fd4d5fea37286bfbabdf13886dc9
DisposableCrewmember42 <disposablecrewmember42@proton.me>
2026-06-06 20:54:27 +0200

    chore: remove unused import

--- Content.Shared/SSDIndicator/SSDIndicatorComponent.cs
+++ Content.Shared/SSDIndicator/SSDIndicatorComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.CCVar;
 using Content.Shared.StatusIcon;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared.SSDIndicator;
