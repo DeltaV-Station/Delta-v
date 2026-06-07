RMC-14/RMC-14

Add Discord account linking, patron perks (#2666)
Co-authored-by: Whisper <121047731+QuietlyWhisper@users.noreply.github.com>

Content.Client/_RMC14/LinkAccount/LinkAccountWindow.xaml.cs
@@ -1,0 +1,15 @@
+﻿using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client._RMC14.LinkAccount;
+
+[GenerateTypedNameReferences]
+public sealed partial class LinkAccountWindow : DefaultWindow
+{
+    public LinkAccountWindow()
+    {
+        RobustXamlLoader.Load(this);
+    }
+}

Content.Server.Database/Migrations/Postgres/20240628081332_RMCPatrons.cs
@@ -1,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class RMCPatrons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rmc_discord_accounts",
+                columns: table => new
+                {
+                    rmc_discord_accounts_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rmc_discord_accounts", x => x.rmc_discord_accounts_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rmc_patron_tiers",
+                columns: table => new
+                {
+                    rmc_patron_tiers_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    show_on_credits = table.Column<bool>(type: "boolean", nullable: false),
+                    named_items = table.Column<bool>(type: "boolean", nullable: false),
+                    figurines = table.Column<bool>(type: "boolean", nullable: false),
+                    lobby_message = table.Column<bool>(type: "boolean", nullable: false),
+                    round_end_shoutout = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rmc_patron_tiers", x => x.rmc_patron_tiers_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rmc_linked_accounts",
+                columns: table => new
+                {
+                    player_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    discord_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rmc_linked_accounts", x => x.player_id);
+                    table.ForeignKey(
+                        name: "FK_rmc_linked_accounts_player_player_id",
+                        column: x => x.player_id,
+                        principalTable: "player",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_rmc_linked_accounts_rmc_discord_accounts_discord_id",
+                        column: x => x.discord_id,
+                        principalTable: "rmc_discord_accounts",
+                        principalColumn: "rmc_discord_accounts_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rmc_patrons",
+                columns: table => new
+                {
+                    player_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tier_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rmc_patrons", x => x.player_id);
+                    table.ForeignKey(
+                        name: "FK_rmc_patrons_player_player_id",
+                        column: x => x.player_id,
+                        principalTable: "player",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_rmc_patrons_rmc_patron_tiers_tier_id",
+                        column: x => x.tier_id,
+                        principalTable: "rmc_patron_tiers",
+                        principalColumn: "rmc_patron_tiers_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rmc_linked_accounts_discord_id",
+                table: "rmc_linked_accounts",
+                column: "discord_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rmc_patrons_tier_id",
+                table: "rmc_patrons",
+                column: "tier_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rmc_linked_accounts");
+
+            migrationBuilder.DropTable(
+                name: "rmc_patrons");
+
+            migrationBuilder.DropTable(
+                name: "rmc_discord_accounts");
+
+            migrationBuilder.DropTable(
+                name: "rmc_patron_tiers");
+        }
+    }
+}

Content.Server.Database/Migrations/Postgres/20240628092907_RMCPatronsRolePriority.cs
@@ -1,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class RMCPatronsRolePriority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "discord_role",
+                table: "rmc_patron_tiers",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "rmc_patron_tiers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "priority",
+                table: "rmc_patron_tiers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rmc_patron_tiers_discord_role",
+                table: "rmc_patron_tiers",
+                column: "discord_role",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_rmc_patron_tiers_discord_role",
+                table: "rmc_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "discord_role",
+                table: "rmc_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "rmc_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "priority",
+                table: "rmc_patron_tiers");
+        }
+    }
+}

Content.Server.Database/Migrations/Sqlite/20240628214811_RMCPatronsCodeDate.cs
@@ -1,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class RMCPatronsCodeDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

Content.Shared/_RMC14/LinkAccount/RMCPatronListMsg.cs
@@ -1,0 +1,34 @@
+﻿using Lidgren.Network;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._RMC14.LinkAccount;
+
+public sealed class RMCPatronListMsg : NetMessage
+{
+    public override MsgGroups MsgGroup => MsgGroups.Core;
+
+    public List<SharedRMCPatron> Patrons = default!;
+
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
+    {
+        var count = buffer.ReadVariableInt32();
+        Patrons = new List<SharedRMCPatron>(count);
+        for (var i = 0; i < count; i++)
+        {
+            var name = buffer.ReadString();
+            var tier = buffer.ReadString();
+            Patrons.Add(new SharedRMCPatron(name, tier));
+        }
+    }
+
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
+    {
+        buffer.WriteVariableInt32(Patrons.Count);
+        foreach (var patron in Patrons)
+        {
+            buffer.Write(patron.Name);
+            buffer.Write(patron.Tier);
+        }
+    }
+}

Content.Shared/_RMC14/LinkAccount/SharedRMCPatron.cs
@@ -1,0 +1,10 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._RMC14.LinkAccount;
+
+[Serializable, NetSerializable]
+public sealed class SharedRMCPatron(string name, string tier)
+{
+    public readonly string Name = name;
+    public readonly string Tier = tier;
+}

DeltaV-Station/Delta-v

Authored-by: Vanessa <vanessalouwagie@gmail.com>

Content.Client/_DV/AccountLinking/LinkAccountWindow.xaml.cs
@@ -1,0 +1,15 @@
+﻿using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client._DV.AccountLinking;
+
+[GenerateTypedNameReferences]
+public sealed partial class LinkAccountWindow : DefaultWindow
+{
+    public LinkAccountWindow()
+    {
+        RobustXamlLoader.Load(this);
+    }
+}

Content.Server.Database/Migrations/Postgres/20240628081332_RMCPatrons.cs
@@ -1,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class DeltaVPatrons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "discord_accounts",
+                columns: table => new
+                {
+                    discord_accounts_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_discord_accounts", x => x.discord_accounts_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "discord_patron_tiers",
+                columns: table => new
+                {
+                    discord_patron_tiers_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    show_on_credits = table.Column<bool>(type: "boolean", nullable: false),
+                    named_items = table.Column<bool>(type: "boolean", nullable: false),
+                    figurines = table.Column<bool>(type: "boolean", nullable: false),
+                    lobby_message = table.Column<bool>(type: "boolean", nullable: false),
+                    round_end_shoutout = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_discord_patron_tiers", x => x.discord_patron_tiers_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "discord_linked_accounts",
+                columns: table => new
+                {
+                    player_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    discord_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_discord_linked_accounts", x => x.player_id);
+                    table.ForeignKey(
+                        name: "FK_discord_linked_accounts_player_player_id",
+                        column: x => x.player_id,
+                        principalTable: "player",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_discord_linked_accounts_discord_accounts_discord_id",
+                        column: x => x.discord_id,
+                        principalTable: "discord_accounts",
+                        principalColumn: "discord_accounts_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "discord_patrons",
+                columns: table => new
+                {
+                    player_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tier_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_discord_patrons", x => x.player_id);
+                    table.ForeignKey(
+                        name: "FK_discord_patrons_player_player_id",
+                        column: x => x.player_id,
+                        principalTable: "player",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_discord_patrons_discord_patron_tiers_tier_id",
+                        column: x => x.tier_id,
+                        principalTable: "discord_patron_tiers",
+                        principalColumn: "discord_patron_tiers_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_discord_linked_accounts_discord_id",
+                table: "discord_linked_accounts",
+                column: "discord_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_discord_patrons_tier_id",
+                table: "discord_patrons",
+                column: "tier_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "discord_linked_accounts");
+
+            migrationBuilder.DropTable(
+                name: "discord_patrons");
+
+            migrationBuilder.DropTable(
+                name: "discord_accounts");
+
+            migrationBuilder.DropTable(
+                name: "discord_patron_tiers");
+        }
+    }
+}

Content.Server.Database/Migrations/Postgres/20240628092907_RMCPatronsRolePriority.cs
@@ -1,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class DeltaVPatronsRolePriority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "discord_role",
+                table: "discord_patron_tiers",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "discord_patron_tiers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "priority",
+                table: "discord_patron_tiers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_discord_patron_tiers_discord_role",
+                table: "discord_patron_tiers",
+                column: "discord_role",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_discord_patron_tiers_discord_role",
+                table: "discord_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "discord_role",
+                table: "discord_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "discord_patron_tiers");
+
+            migrationBuilder.DropColumn(
+                name: "priority",
+                table: "discord_patron_tiers");
+        }
+    }
+}

Content.Server.Database/Migrations/Sqlite/20240628214811_RMCPatronsCodeDate.cs
@@ -1,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class DeltaVPatronsCodeDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

Content.Shared/_DV/AccountLinking/RMCPatronListMsg.cs
@@ -1,0 +1,34 @@
+﻿using Lidgren.Network;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._DV.AccountLinking;
+
+public sealed class DeltaVPatronListMsg : NetMessage
+{
+    public override MsgGroups MsgGroup => MsgGroups.Core;
+
+    public List<SharedDeltaVPatron> Patrons = default!;
+
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
+    {
+        var count = buffer.ReadVariableInt32();
+        Patrons = new List<SharedDeltaVPatron>(count);
+        for (var i = 0; i < count; i++)
+        {
+            var name = buffer.ReadString();
+            var tier = buffer.ReadString();
+            Patrons.Add(new SharedDeltaVPatron(name, tier));
+        }
+    }
+
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
+    {
+        buffer.WriteVariableInt32(Patrons.Count);
+        foreach (var patron in Patrons)
+        {
+            buffer.Write(patron.Name);
+            buffer.Write(patron.Tier);
+        }
+    }
+}

Content.Shared/_DV/AccountLinking/SharedRMCPatron.cs
@@ -1,0 +1,10 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._DV.AccountLinking;
+
+[Serializable, NetSerializable]
+public sealed class SharedDeltaVPatron(string name, string tier)
+{
+    public readonly string Name = name;
+    public readonly string Tier = tier;
+}