7e50dbe591ba5c71ee38e781d8209e52b9e0032b
DisposableCrewmember42 <disposablecrewmember42@proton.me>
2026-06-08 17:13:36 +0200

    fix: don't display examine text on dead people or untaken ghost roles

--- Content.Shared/SSDIndicator/SSDIndicatorSystem.cs
+++ Content.Shared/SSDIndicator/SSDIndicatorSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared._DV.CCVars; // DeltaV - SSD Recency
 using Content.Shared.CCVar;
 using Content.Shared.Examine; // DeltaV - SSD Recency
+using Content.Shared.Mobs.Systems; // DeltaV - SSD Recency
 using Content.Shared.StatusEffectNew;
 using Robust.Shared.Configuration;
 using Robust.Shared.Player;
@@ -19,12 +20,13 @@ public sealed class SSDIndicatorSystem : EntitySystem
     [Dependency] private readonly IConfigurationManager _cfg = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!; // DeltaV - SSD Recency
 
     private bool _icSsdSleep;
     private float _icSsdSleepTime;
 
-    private TimeSpan _cryoableSsdSeconds; // DeltaV
-    private TimeSpan _recentSsdSeconds; // DeltaV
+    private TimeSpan _cryoableSsdSeconds; // DeltaV - SSD Recency
+    private TimeSpan _recentSsdSeconds; // DeltaV - SSD Recency
 
     public override void Initialize()
     {
@@ -43,12 +45,15 @@ public sealed class SSDIndicatorSystem : EntitySystem
     // DeltaV - SSD Recency START
     private void OnExamine(Entity<SSDIndicatorComponent> ent, ref ExaminedEvent args)
     {
-        if (!ent.Comp.IsSSD)
+        if (!ent.Comp.IsSSD || ent.Comp.SsdSince is not { } ssdSince)
+            return;
+
+        if (_mobState.IsDead(ent))
             return;
 
         using (args.PushGroup(nameof(SSDIndicatorComponent)))
         {
-            var timestamp = (_timing.CurTime - ent.Comp.SsdSince).ToString("%hh':'mm':'ss");
+            var timestamp = (_timing.CurTime - ssdSince).ToString("%hh':'mm':'ss");
             args.PushMarkup(Loc.GetString("ssd-examine-duration", ("time", timestamp)));
             args.PushMarkup(Loc.GetString($"ssd-examine-{GetStage(ent).ToString().ToLower()}"));
         }
