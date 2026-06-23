f33e2c1640a68c03f52ba62e5dfea29f0980d923
weskified <seanderscore@gmail.com>
2026-06-23 17:55:24 +0800

    important summary comments to give more important info when someone's too lazy to check the rule

--- Content.Server/StationEvents/Components/IonStormRuleComponent.cs
+++ Content.Server/StationEvents/Components/IonStormRuleComponent.cs
@@ -14,31 +14,31 @@ public sealed partial class IonStormRuleComponent : Component
     public float SynthElectrocutionChance = 0.6f;
 
     /// <summary>
-    /// The minimum delay before a synth affected by the ion storm gets electrocuted.
+    /// The minimum delay before a synth affected by the ion storm gets electrocuted. (in seconds)
     /// </summary>
     [DataField]
     public int SynthElectrocutionDelayMin = 3;
 
     /// <summary>
-    /// The maximum delay before a synth affected by the ion storm gets electrocuted.
+    /// The maximum delay before a synth affected by the ion storm gets electrocuted. (in seconds)
     /// </summary>
     [DataField]
     public int SynthElectrocutionDelayMax = 10;
 
     /// <summary>
-    /// The minimum damage dealt by the synth electrocution.
+    /// The minimum damage dealt by the synth electrocution. (shock damage)
     /// </summary>
     [DataField]
     public int SynthElectrocutionDamageMin = 5;
 
     /// <summary>
-    /// The maximum damage dealt by the synth electrocution.
+    /// The maximum damage dealt by the synth electrocution. (shock damage)
     /// </summary>8
     [DataField]
     public int SynthElectrocutionDamageMax = 10;
 
     /// <summary>
-    /// How long the synth is stunned for by the electrocution.
+    /// How long the synth is stunned for by the electrocution. (in seconds)
     /// </summary>
     [DataField]
     public int SynthElectrocutionStunDuration = 3;
