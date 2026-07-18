6b9d386653e7ffd5fd9b632a21ae7ecc0ae3a400
BarryNorfolk <barrynorfolkman@protonmail.com>
2026-07-18 14:19:36 +0200

    Remove need for sending RoleType

--- Content.Client/_DV/Objectives/Eui/ObjectiveEditorEui.cs
+++ Content.Client/_DV/Objectives/Eui/ObjectiveEditorEui.cs
@@ -1,17 +1,21 @@
 using Content.Client.Eui;
 using Content.Shared._DV.Objectives.Eui;
 using Content.Shared.Eui;
+using Content.Shared.Mind;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client._DV.Objectives.Eui;
 
 public sealed class ObjectiveEditorEui : BaseEui
 {
+    [Dependency] private readonly EntityManager _entityManager = default!;
     private readonly ObjectiveEditorUi _editorUi;
-    private NetEntity _targetMind;
+    private Entity<MindComponent> _targetMind;
 
     public ObjectiveEditorEui()
     {
+        IoCManager.InjectDependencies(this);
+
         _editorUi = new ObjectiveEditorUi();
         _editorUi.SaveAction += SaveObjectives;
         _editorUi.CreateAction += CreateObjective;
@@ -28,8 +32,16 @@ public sealed class ObjectiveEditorEui : BaseEui
         if (state is not ObjectiveEditorEUIState s)
             return;
 
-        _targetMind = s.TargetMind;
-        _editorUi.SetRoleDescription(s.Role, s.Subtype);
+        var mind = _entityManager.GetEntity(s.TargetMind);
+        if (!_entityManager.TryGetComponent<MindComponent>(mind, out var comp))
+        {
+            // TODO: Log
+            return;
+        }
+
+        _targetMind = (mind, comp);
+
+        _editorUi.SetRoleDescription(comp.RoleType, s.Subtype);
         _editorUi.SetObjectives(s.Objectives);
     }
 
@@ -58,7 +70,7 @@ public sealed class ObjectiveEditorEui : BaseEui
 
     private void SaveObjectives()
     {
-        SendMessage(new ObjectiveEditorSaveMessage(_editorUi.GetObjectives(), _targetMind));
+        SendMessage(new ObjectiveEditorSaveMessage(_editorUi.GetObjectives(), _entityManager.GetNetEntity(_targetMind)));
     }
 
     private void CreateObjective(EntProtoId? proto)
