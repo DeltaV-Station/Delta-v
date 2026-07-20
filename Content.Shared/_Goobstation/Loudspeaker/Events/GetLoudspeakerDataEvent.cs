madeline-celeste/Maidstation

Over-ear loudspeakers and handheld megaphones [BOUNTY] (#4635)
* loudspeaker

* wowza

Content.Goobstation.Shared/Loudspeaker/Events/GetLoudspeakerDataEvent.cs
@@ -1,0 +1,12 @@
+using Content.Shared.Speech;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Shared.Loudspeaker.Events;
+
+[ByRefEvent]
+public record struct GetLoudspeakerDataEvent(
+    bool IsActive = false,
+    int? FontSize = null,
+    bool AffectRadio = false,
+    bool AffectChat = false,
+    ProtoId<SpeechSoundsPrototype>? SpeechSounds = null);

DeltaV-Station/Delta-v

Content.Shared/_Goobstation/Loudspeaker/Events/GetLoudspeakerDataEvent.cs
@@ -1,0 +1,12 @@
+using Content.Shared.Speech;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Goobstation.Loudspeaker.Events;
+
+[ByRefEvent]
+public record struct GetLoudspeakerDataEvent(
+    bool IsActive = false,
+    int? FontSize = null,
+    bool AffectRadio = false,
+    bool AffectChat = false,
+    ProtoId<SpeechSoundsPrototype>? SpeechSounds = null);