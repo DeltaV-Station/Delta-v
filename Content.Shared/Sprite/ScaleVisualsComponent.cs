1f09c717f3fb9f7e635b94b0e36805cba93fa037
Vanessa <908648+ShepardToTheStars@users.noreply.github.com>
2026-08-07 11:51:24 -0500

    Fix Antags Being Crazy Smol (#6398)
    * Fixed antags being crazy smol
    
    * Update Content.Shared/Sprite/SharedScaleVisualsSystem.cs
    
    Signed-off-by: Sir Warock <67167466+SirWarock@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sir Warock <67167466+SirWarock@users.noreply.github.com>
    Co-authored-by: Sir Warock <67167466+SirWarock@users.noreply.github.com>

--- Content.Shared/Sprite/ScaleVisualsComponent.cs
+++ Content.Shared/Sprite/ScaleVisualsComponent.cs
@@ -24,4 +24,28 @@ public sealed partial class ScaleVisualsComponent : Component
     [DataField]
     [ViewVariables]
     public Vector2? OriginalScale;
+
+    /// <summary>
+    /// DeltaV - Contains the species scale. Set dynamically by
+    /// baseScale in the Species prototype.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    [ViewVariables]
+    public Vector2 SpeciesScale = new(1f, 1f);
+
+    /// <summary>
+    /// DeltaV - Contains the user-defined scale from the character creation
+    /// screen.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    [ViewVariables]
+    public Vector2 ProfileScale = new(1f, 1f);
+
+    /// <summary>
+    /// DeltaV - Contains the computer scale from applying Scale, SpeciesScale, and ProfileScale.
+    /// This will contain the actual scale after all modifiers are applied.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    [ViewVariables]
+    public Vector2 ComputedScale;
 }
