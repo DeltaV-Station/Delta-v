DeltaV-Station/Acceleration

handhelds get separate microphone and speaker toggles!

Content.Client/Radio/EntitySystems/RadioDeviceSystem.cs
@@ -13,12 +13,34 @@ public sealed partial class RadioDeviceSystem : SharedRadioDeviceSystem
     /// <inheritdoc/>
     public override void Initialize()
     {
+        // BEGIN DeltaV - Update intercom UI on RadioMicrophone/Speaker component statechange
+        base.Initialize();
         SubscribeLocalEvent<IntercomComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        SubscribeLocalEvent<RadioMicrophoneComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        SubscribeLocalEvent<RadioSpeakerComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        // END DeltaV
     }
 
-    private void OnAfterHandleState(Entity<IntercomComponent> ent, ref AfterAutoHandleStateEvent args)
+    /// <summary>
+    /// DeltaV - Update intercom UI on RadioMicrophone/Speaker component statechange.
+    /// </summary>
+    private void OnAfterHandleState<TComp>(EntityUid uid, TComp component, AfterAutoHandleStateEvent args) where TComp : IComponent
+    {
+        IntercomComponent? intercom = null;
+        if (!Resolve(uid, ref intercom, false))
+            return;
+
+        var entity = new Entity<IntercomComponent>(uid, intercom);
+
+        if (_ui.TryGetOpenUi<IntercomBoundUserInterface>(entity.Owner, IntercomUiKey.Key, out var bui))
+            bui.Update(entity);
+    }
+
+    /* DeltaV - Replaced by OnAfterHandleState
+    private void OpenAndUpdateUI(Entity<IntercomComponent> ent)
     {
         if (_ui.TryGetOpenUi<IntercomBoundUserInterface>(ent.Owner, IntercomUiKey.Key, out var bui))
             bui.Update(ent);
     }
+    */
 }

Content.Client/Radio/Ui/IntercomMenu.xaml.cs
@@ -1,7 +1,10 @@
-using System.Threading.Channels;
+using System.Linq; // DeltaV - Used for `.First()` on channel sets.
+// using System.Threading.Channels; // DeltaV - Unused.
 using Content.Client.UserInterface.Controls;
+using Content.Shared._DVA.Radio.Components; // DeltaV - To use DVRadioToggleable type
 using Content.Shared.Radio.Components;
 using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls; // DeltaV - To use Button type
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
@@ -28,31 +31,60 @@ public sealed partial class IntercomMenu : FancyWindow
         SpeakerButton.OnPressed += args => OnSpeakerPressed?.Invoke(args.Button.Pressed);
     }
 
-    public void Update(Entity<IntercomComponent> entity)
+    /// <summary>
+    /// DeltaV - Sets button's state based on component's presence and enabled-status.
+    /// </summary>
+    /// <param name="button">The button which state depends on the component</param>
+    /// <param name="component">The component which defines the button's state</param>
+    private void UpdateToggleButton(Button button, DVRadioToggleable? component)
     {
-        MicButton.Pressed = entity.Comp.MicrophoneEnabled;
-        SpeakerButton.Pressed = entity.Comp.SpeakerEnabled;
+        if (component == null)
+        {
+            button.Disabled = true;
+            return;
+        }
 
-        MicButton.Disabled = entity.Comp.SupportedChannels.Count == 0;
-        SpeakerButton.Disabled = entity.Comp.SupportedChannels.Count == 0;
-        ChannelOptions.Disabled = entity.Comp.SupportedChannels.Count == 0;
+        button.Disabled = false;
+        button.Pressed = component.Enabled;
+    }
+
+    // BEGIN DeltaV - Add microphone and speaker component parameters to Update
+    public void Update(EntityUid uid, IntercomComponent intercom, RadioMicrophoneComponent? microphone, RadioSpeakerComponent? speaker)
+    {
+        if (intercom.SupportedChannels.Count == 0)
+        {
+            MicButton.Disabled = true;
+            SpeakerButton.Disabled = true;
+            ChannelOptions.Disabled = true;
+        }
+        else
+        {
+            UpdateToggleButton(MicButton, microphone);
+            UpdateToggleButton(SpeakerButton, speaker);
+        }
+
+        var currentChannel =
+            microphone?.BroadcastChannel ??
+            speaker?.Channels.First() ??
+            intercom.SupportedChannels.First();
+        // END DeltaV
 
         ChannelOptions.Clear();
         _channels.Clear();
-        for (var i = 0; i < entity.Comp.SupportedChannels.Count; i++)
+        for (var i = 0; i < intercom.SupportedChannels.Count; i++) // DeltaV - Add microphone and speaker component parameters to Update
         {
-            var channel = entity.Comp.SupportedChannels[i];
+            var channel = intercom.SupportedChannels[i]; // DeltaV - Add microphone and speaker component parameters to Update
             if (!_prototype.Resolve(channel, out var prototype))
                 continue;
 
             _channels.Add(channel);
             ChannelOptions.AddItem(Loc.GetString(prototype.Name), i);
 
-            if (channel == entity.Comp.CurrentChannel)
+            if (channel == currentChannel) // DeltaV - Add microphone and speaker component parameters to Update
                 ChannelOptions.Select(i);
         }
 
-        if (entity.Comp.SupportedChannels.Count == 0)
+        if (intercom.SupportedChannels.Count == 0) // DeltaV - Add microphone and speaker component parameters to Update
         {
             ChannelOptions.AddItem(Loc.GetString("intercom-options-none"), 0);
             ChannelOptions.Select(0);

Content.Shared/Radio/Components/RadioMicrophoneComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared._DVA.Radio.Components; // DeltaV - Add DVRadioToggleable class
 using Content.Shared.Radio.EntitySystems;
 using Content.Shared.Chat;
 using Robust.Shared.GameStates;
@@ -8,28 +9,30 @@ namespace Content.Shared.Radio.Components;
 /// <summary>
 ///     Listens for local chat messages and relays them to some radio frequency
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)] // DeltaV - Add AutoGenerateComponentState for prediction
 [Access(typeof(SharedRadioDeviceSystem))]
-public sealed partial class RadioMicrophoneComponent : Component
+public sealed partial class RadioMicrophoneComponent : DVRadioToggleable // DeltaV - Set base class to DVRadioToggleable
 {
-    [DataField]
+    [DataField, AutoNetworkedField] // DeltaV - Add AutoNetworkedField for prediction
     public ProtoId<RadioChannelPrototype> BroadcastChannel = SharedChatSystem.CommonChannel;
 
     [DataField]
     public int ListenRange = 4;
 
-    [DataField]
-    public bool Enabled = false;
+    // DeltaV - Defined in base class
+    // [DataField]
+    // public bool Enabled = false;
 
     [DataField]
     public bool PowerRequired = false;
 
-    /// <summary>
-    /// Whether or not interacting with this entity
-    /// toggles it on or off.
-    /// </summary>
-    [DataField]
-    public bool ToggleOnInteract = true;
+    // DeltaV - Defined in base class
+    // /// <summary>
+    // /// Whether or not interacting with this entity
+    // /// toggles it on or off.
+    // /// </summary>
+    // [DataField]
+    // public bool ToggleOnInteract = true;
 
     /// <summary>
     /// Whether or not the speaker must have an
@@ -37,4 +40,7 @@ public sealed partial class RadioMicrophoneComponent : Component
     /// </summary>
     [DataField]
     public bool UnobstructedRequired = false;
+
+    public override LocId EnableVerbText { get; set; } = "radio-microphone-component-verb-enable"; // DeltaV - Override verb text!
+    public override LocId DisableVerbText { get; set; } = "radio-microphone-component-verb-disable"; // DeltaV - Override verb text!
 }

Content.Shared/_DVA/Radio/Components/DVRadioToggleable.cs
@@ -1,0 +1,40 @@
+﻿namespace Content.Shared._DVA.Radio.Components;
+
+/// <summary>
+/// Parent class for RadioMicrophoneComponent and RadioSpeakerComponent,
+/// because they're basically have all the same fields...
+/// </summary>
+public abstract partial class DVRadioToggleable : Component
+{
+    /// <summary>
+    /// Whether it's on or off :)
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool Enabled = false;
+
+    /// <summary>
+    /// Whether clicking the entity should toggle the Enable field.
+    /// </summary>
+    [DataField]
+    public bool ToggleOnInteract = false;
+
+    /// <summary>
+    /// Whether or not this entity has a verb
+    /// for toggling this component on or off.
+    /// </summary>
+    [DataField]
+    public bool Toggleable = true;
+
+    /// <summary>
+    /// Text to show in the verb menu for the "Enable" action.
+    /// </summary>
+    [DataField]
+    public abstract LocId EnableVerbText { get; set; }
+
+    /// <summary>
+    /// Text to show in the verb menu for the "Disable" action.
+    /// </summary>
+    [DataField]
+    public abstract LocId DisableVerbText { get; set; }
+}
+

DeltaV-Station/Delta-v

Content.Client/Radio/EntitySystems/RadioDeviceSystem.cs
@@ -13,11 +13,25 @@ public sealed class RadioDeviceSystem : SharedRadioDeviceSystem
     /// <inheritdoc/>
     public override void Initialize()
     {
+        // BEGIN DeltaV - Update intercom UI on RadioMicrophone/Speaker component statechange
+        base.Initialize();
+        SubscribeLocalEvent<RadioMicrophoneComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        SubscribeLocalEvent<RadioSpeakerComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
+        // END DeltaV
         SubscribeLocalEvent<IntercomComponent, AfterAutoHandleStateEvent>(OnAfterHandleState);
     }
 
-    private void OnAfterHandleState(Entity<IntercomComponent> ent, ref AfterAutoHandleStateEvent args)
+    // DeltaV - Made generic. `component` unused, but required for subscriptions.
+    private void OnAfterHandleState<TComp>(EntityUid uid, TComp component, AfterAutoHandleStateEvent args) where TComp : IComponent
     {
+        // BEGIN DeltaV
+        IntercomComponent? intercom = null;
+        if (!Resolve(uid, ref intercom))
+            return;
+
+        var ent = new Entity<IntercomComponent>(uid, intercom);
+        // END DeltaV
+
         if (_ui.TryGetOpenUi<IntercomBoundUserInterface>(ent.Owner, IntercomUiKey.Key, out var bui))
             bui.Update(ent);
     }

Content.Client/Radio/Ui/IntercomMenu.xaml.cs
@@ -1,7 +1,10 @@
-using System.Threading.Channels;
+using System.Linq; // DeltaV - Used for `.First()` on channel sets.
+// using System.Threading.Channels; // DeltaV - Unused.
 using Content.Client.UserInterface.Controls;
+using Content.Shared._DV.Radio.Components; // DeltaV - To use DVRadioToggleable type
 using Content.Shared.Radio.Components;
 using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls; // DeltaV - To use Button type
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
@@ -28,31 +31,60 @@ public sealed partial class IntercomMenu : FancyWindow
         SpeakerButton.OnPressed += args => OnSpeakerPressed?.Invoke(args.Button.Pressed);
     }
 
-    public void Update(Entity<IntercomComponent> entity)
+    /// <summary>
+    /// DeltaV - Sets button's state based on component's presence and enabled-status.
+    /// </summary>
+    /// <param name="button">The button which state depends on the component</param>
+    /// <param name="component">The component which defines the button's state</param>
+    private void UpdateToggleButton(Button button, DVRadioToggleable? component)
     {
-        MicButton.Pressed = entity.Comp.MicrophoneEnabled;
-        SpeakerButton.Pressed = entity.Comp.SpeakerEnabled;
+        if (component == null)
+        {
+            button.Disabled = true;
+            return;
+        }
 
-        MicButton.Disabled = entity.Comp.SupportedChannels.Count == 0;
-        SpeakerButton.Disabled = entity.Comp.SupportedChannels.Count == 0;
-        ChannelOptions.Disabled = entity.Comp.SupportedChannels.Count == 0;
+        button.Disabled = false;
+        button.Pressed = component.Enabled;
+    }
+
+    // BEGIN DeltaV - Add microphone and speaker component parameters to Update
+    public void Update(EntityUid uid, IntercomComponent intercom, RadioMicrophoneComponent? microphone, RadioSpeakerComponent? speaker)
+    {
+        if (intercom.SupportedChannels.Count == 0)
+        {
+            MicButton.Disabled = true;
+            SpeakerButton.Disabled = true;
+            ChannelOptions.Disabled = true;
+        }
+        else
+        {
+            UpdateToggleButton(MicButton, microphone);
+            UpdateToggleButton(SpeakerButton, speaker);
+        }
+
+        var currentChannel =
+            microphone?.BroadcastChannel ??
+            speaker?.Channels.First() ??
+            intercom.SupportedChannels.First();
+        // END DeltaV
 
         ChannelOptions.Clear();
         _channels.Clear();
-        for (var i = 0; i < entity.Comp.SupportedChannels.Count; i++)
+        for (var i = 0; i < intercom.SupportedChannels.Count; i++) // DeltaV - Add microphone and speaker component parameters to Update
         {
-            var channel = entity.Comp.SupportedChannels[i];
+            var channel = intercom.SupportedChannels[i]; // DeltaV - Add microphone and speaker component parameters to Update
             if (!_prototype.Resolve(channel, out var prototype))
                 continue;
 
             _channels.Add(channel);
             ChannelOptions.AddItem(Loc.GetString(prototype.Name), i);
 
-            if (channel == entity.Comp.CurrentChannel)
+            if (channel == currentChannel) // DeltaV - Add microphone and speaker component parameters to Update
                 ChannelOptions.Select(i);
         }
 
-        if (entity.Comp.SupportedChannels.Count == 0)
+        if (intercom.SupportedChannels.Count == 0) // DeltaV - Add microphone and speaker component parameters to Update
         {
             ChannelOptions.AddItem(Loc.GetString("intercom-options-none"), 0);
             ChannelOptions.Select(0);

Content.Shared/Radio/Components/RadioMicrophoneComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared._DV.Radio.Components; // DeltaV - Add DVRadioToggleable class
 using Content.Shared.Radio.EntitySystems;
 using Content.Shared.Chat;
 using Robust.Shared.GameStates;
@@ -8,28 +9,30 @@ namespace Content.Shared.Radio.Components;
 /// <summary>
 ///     Listens for local chat messages and relays them to some radio frequency
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)] // DeltaV - Add AutoGenerateComponentState for prediction
 [Access(typeof(SharedRadioDeviceSystem))]
-public sealed partial class RadioMicrophoneComponent : Component
+public sealed partial class RadioMicrophoneComponent : DVRadioToggleable // DeltaV - Set base class to DVRadioToggleable
 {
-    [DataField]
+    [DataField, AutoNetworkedField] // DeltaV - Add AutoNetworkedField for prediction
     public ProtoId<RadioChannelPrototype> BroadcastChannel = SharedChatSystem.CommonChannel;
 
     [DataField]
     public int ListenRange = 4;
 
-    [DataField]
-    public bool Enabled = false;
+    // DeltaV - Defined in base class
+    // [DataField]
+    // public bool Enabled = false;
 
     [DataField]
     public bool PowerRequired = false;
 
-    /// <summary>
-    /// Whether or not interacting with this entity
-    /// toggles it on or off.
-    /// </summary>
-    [DataField]
-    public bool ToggleOnInteract = true;
+    // DeltaV - Defined in base class
+    // /// <summary>
+    // /// Whether or not interacting with this entity
+    // /// toggles it on or off.
+    // /// </summary>
+    // [DataField]
+    // public bool ToggleOnInteract = true;
 
     /// <summary>
     /// Whether or not the speaker must have an
@@ -37,4 +40,7 @@ public sealed partial class RadioMicrophoneComponent : Component
     /// </summary>
     [DataField]
     public bool UnobstructedRequired = false;
+
+    public override LocId EnableVerbText { get; set; } = "radio-microphone-component-verb-enable"; // DeltaV - Override verb text!
+    public override LocId DisableVerbText { get; set; } = "radio-microphone-component-verb-disable"; // DeltaV - Override verb text!
 }

Content.Shared/_DV/Radio/Components/DVRadioToggleable.cs
@@ -1,0 +1,40 @@
+﻿namespace Content.Shared._DV.Radio.Components;
+
+/// <summary>
+/// Parent class for RadioMicrophoneComponent and RadioSpeakerComponent,
+/// because they're basically have all the same fields...
+/// </summary>
+public abstract partial class DVRadioToggleable : Component
+{
+    /// <summary>
+    /// Whether it's on or off :)
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool Enabled = false;
+
+    /// <summary>
+    /// Whether clicking the entity should toggle the Enable field.
+    /// </summary>
+    [DataField]
+    public bool ToggleOnInteract = false;
+
+    /// <summary>
+    /// Whether or not this entity has a verb
+    /// for toggling this component on or off.
+    /// </summary>
+    [DataField]
+    public bool Toggleable = true;
+
+    /// <summary>
+    /// Text to show in the verb menu for the "Enable" action.
+    /// </summary>
+    [DataField]
+    public abstract LocId EnableVerbText { get; set; }
+
+    /// <summary>
+    /// Text to show in the verb menu for the "Disable" action.
+    /// </summary>
+    [DataField]
+    public abstract LocId DisableVerbText { get; set; }
+}
+