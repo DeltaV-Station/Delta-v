5cf8329664a86b12e6731f7317b480fb06047f81
Carol <292806487+CarolCuchara@users.noreply.github.com>
2026-08-31 19:52:22 +0200

    Taser only stamina stun damage to heavily protected targets

--- Content.Server/Stunnable/Systems/StunOnCollideSystem.cs
+++ Content.Server/Stunnable/Systems/StunOnCollideSystem.cs
@@ -1,4 +1,7 @@
 using Content.Server.Stunnable.Components;
+using Content.Shared.Damage;
+using Content.Shared.Damage.Events;
+using Content.Shared.Damage.Systems;
 using Content.Shared.Movement.Systems;
 using JetBrains.Annotations;
 using Content.Shared.Throwing;
@@ -12,7 +15,11 @@ internal sealed class StunOnCollideSystem : EntitySystem
 {
     [Dependency] private readonly StunSystem _stunSystem = default!;
     [Dependency] private readonly MovementModStatusSystem _movementMod = default!;
-    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!; // DeltaV
+
+    // Begin DeltaV
+    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private readonly SharedStaminaSystem _sharedStaminaSystem = default!;
+    // End DeltaV
 
     public override void Initialize()
     {
@@ -27,6 +34,16 @@ internal sealed class StunOnCollideSystem : EntitySystem
         // Begin DeltaV Additions
         if (!_whitelist.IsWhitelistFailOrNull(ent.Comp.Blacklist, target))
             return;
+
+        var beforeStaminaDamageEvent = new BeforeStaminaDamageEvent(100f, FromMelee: false);
+        RaiseLocalEvent(target, ref beforeStaminaDamageEvent);
+
+        // If the target is heavily protected, just deal stamina damage, no knockdown etc.
+        if (beforeStaminaDamageEvent.Value < ent.Comp.ProtectionThreshold)
+        {
+            _sharedStaminaSystem.TakeStaminaDamage(target, ent.Comp.StaminaDamage);
+            return;
+        }
         // End DeltaV Additions
 
         _stunSystem.TryKnockdown(target, ent.Comp.KnockdownAmount, ent.Comp.Refresh, ent.Comp.AutoStand, ent.Comp.Drop, true);
