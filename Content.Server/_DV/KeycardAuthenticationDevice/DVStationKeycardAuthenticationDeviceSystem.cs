7d37f30887cd057faa1b773e7e92d38083cf5525
TNE <tne@garudalinux.org>
2026-09-08 04:32:09 +0200

    fix(DVStationKeycardAuthenticationDeviceSystem): Do not allow zeta to be called twice
    Prevents the station lights from going darker and darker and darker and darker...

--- Content.Server/_DV/KeycardAuthenticationDevice/DVStationKeycardAuthenticationDeviceSystem.cs
+++ Content.Server/_DV/KeycardAuthenticationDevice/DVStationKeycardAuthenticationDeviceSystem.cs
@@ -28,6 +28,9 @@ public sealed class DVStationKeycardAuthenticationDeviceSystem : SharedDVStation
     {
         base.Mayday(station);
 
+        if (_alertLevel.GetLevel(station) == "zeta")
+            return;
+
         _alertLevel.SetLevel(station, "zeta", true, true, true, true);
         var alertLevel = Comp<AlertLevelComponent>(station);
         var level = _prototype.Index<AlertLevelPrototype>(alertLevel.AlertLevelPrototype).Levels[alertLevel.CurrentLevel];
