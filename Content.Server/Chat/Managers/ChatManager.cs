9a673dacb8b51f5fca4cb4c60f717903b16a8c2d
BriBrooo <45323530+Bribrooo@users.noreply.github.com>
2026-09-09 22:27:05 +0200

    Update Content.Server/Chat/Managers/ChatManager.cs
    Co-authored-by: DisposableCrewmember42 <disposablecrewmember42@proton.me>
    Signed-off-by: BriBrooo <45323530+Bribrooo@users.noreply.github.com>

--- Content.Server/Chat/Managers/ChatManager.cs
+++ Content.Server/Chat/Managers/ChatManager.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization; // DeltaV - OOC color parsing
 using System.Linq;
 using System.Runtime.InteropServices;
 using Content.Server.Administration.Logs;
